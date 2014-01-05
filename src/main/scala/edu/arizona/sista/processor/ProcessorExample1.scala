8a19d9651fa0694b312a8528c351d2122b01fd0e
Mihai Surdeanu <surdeanu@gmail.com>
2014-01-04 19:16:42 -0700

    Added a simple example.

---
+++ src/main/scala/edu/arizona/sista/processor/ProcessorExample1.scala
@@ -0,0 +1,23 @@
+package edu.arizona.sista.processor
+
+import edu.arizona.sista.processor.corenlp.CoreNLPProcessor
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
+
+/**
+ * A simple example for CoreNLPProcessor usage. Assumes each line in a file is a standalone document.
+ * User: mihais
+ * Date: 9/14/13
+ */
+object ProcessorExample1 {
+  def main(args:Array[String]) {
+    val proc = new CoreNLPProcessor()
+    val lines = Source.fromFile(args(0)).getLines.toList
+    var count = 0
+    for(l <- lines) {
+      val doc = proc.annotate(l)
+      count += 1
+      println(s"Processed line $count")
+    }
+  }
+}
