227f015c8f8a71541018d31663182f9bbb80262d
Mihai Surdeanu <surdeanu@gmail.com>
2014-05-26 14:23:26 -0700

    minor cleanup of install script

--- scripts/install_jar_from_github.sh
+++ scripts/install_jar_from_github.sh
@@ -8,8 +8,7 @@
 POM_VERSION=2.1
 INSTALL_AS_VERSION=2.2
 
-mvn clean package
-./scripts/mk_model_jar $POM_VERSION
+mvn clean package; ./scripts/mk_model_jar $POM_VERSION
 
 mvn install:install-file --quiet -DartifactId=processors -DgroupId=edu.arizona.sista -Dversion=$INSTALL_AS_VERSION -Dpackaging=jar -Dfile=target/processors-$POM_VERSION-models.jar -Dclassifier=models
 mvn install:install-file --quiet -DartifactId=processors -DgroupId=edu.arizona.sista -Dversion=$INSTALL_AS_VERSION -Dpackaging=jar -Dfile=target/processors-$POM_VERSION.jar
