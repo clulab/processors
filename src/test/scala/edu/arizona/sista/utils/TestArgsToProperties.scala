0fe40b02f94a86bfd32098eeec9d63168f181576
Mihai Surdeanu <surdeanu@gmail.com>
2014-07-10 18:08:06 +0300

    Added unit test for utils.

---
+++ src/test/scala/edu/arizona/sista/utils/TestArgsToProperties.scala
@@ -0,0 +1,29 @@
+package edu.arizona.sista.utils
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Test
+import junit.framework.Assert
+
+/**
+ * 
+ * User: mihais
+ * Date: 4/3/13
+ */
+class TestArgsToProperties extends AssertionsForJUnit {
+  @Test def testArgsToProperties() {
+    val props = StringUtils.argsToProperties(List("-props", "src/main/resources/edu/arizona/sista/utils/test.properties").toArray)
+
+    val p1 = props.getProperty("p1")
+    val p2 = props.getProperty("p2")
+
+    Assert.assertTrue(p1 == "/some/path")
+    Assert.assertTrue(p2 == "/some/path/subdir/123")
+
+    val p4 = props.getProperty("p4")
+    Assert.assertTrue(
+      p4 == "shell is /bin/bash" ||
+      p4 == "shell is /bin/sh" ||
+      p4 == "shell is /bin/csh" ||
+      p4 == "shell is /bin/tcsh")
+  }
+}
