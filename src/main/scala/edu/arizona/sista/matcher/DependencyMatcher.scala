c0994ac3d0b607e19805b0f25470e868a7bbbcea
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-10-17 10:47:25 -0700

    moved TestDependencyMatcher to another file

--- src/main/scala/edu/arizona/sista/matcher/DependencyMatcher.scala
+++ src/main/scala/edu/arizona/sista/matcher/DependencyMatcher.scala
@@ -1,8 +1,7 @@
 package edu.arizona.sista.matcher
 
 import scala.util.parsing.combinator._
-import edu.arizona.sista.processors.{Document, Sentence}
-import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
+import edu.arizona.sista.processors.Sentence
 
 
 /*
@@ -91,50 +90,6 @@ class DependencyMatcher(val pattern: String) {
 
 }
 
-object TestDependencyMatcher extends App {
-
-  lazy val processor = new FastNLPProcessor()
-  val examplesWithPatterns = Seq(
-    ("My dog ate my homework.",
-     """
-     trigger: ate
-     agent: nsubj
-     patient: dobj
-     """),
-
-    ("My dog drove my mom to the store.",
-     """
-     trigger: drove
-     agent: nsubj
-     patient: dobj
-     posstest: dobj > poss
-     destination: prep
-     """) // Unfortunately, FastNLP doesn't return collapsed deps
-  )
-
-  def processExamples(testPairs: Seq[(String, String)]) {
-    for ((text, rule) <- testPairs) {
-      val doc = processor.annotate(text)
-      val s = doc.sentences(0)
-      val dm = new DependencyMatcher(rule)
-
-      // Our sentence...
-      println(s"\n\n${{0 until s.words.length}.mkString("\t")}\n${s.words.mkString("\t")}")
-      // Find any matches (currently all arguments are optional)
-      val myMap = dm.findAllIn(s)
-
-      // What do we get?
-      println("\nOutgoing deps:")
-      val deps = s.dependencies.get
-      deps.outgoingEdges.zipWithIndex foreach { case (edges, i) => println(s"$i:\t${edges.mkString(" ")}") }
-      println("\nAssigned labels:")
-      myMap.flatten.foreach(pair => println(s"\t${pair._1} -> ${pair._2}"))
-      println(s"\nRule:$rule\n")
-    }
-  }
-
-  processExamples(examplesWithPatterns)
-}
 
 object Parser extends RegexParsers {
   def parse(input: String): Matcher = parseAll(matcher, input).get
---
+++ src/main/scala/edu/arizona/sista/matcher/TestDependencyMatcher.scala
@@ -0,0 +1,49 @@
+package edu.arizona.sista.matcher
+
+import edu.arizona.sista.processors.Document
+import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
+
+object TestDependencyMatcher extends App {
+
+  lazy val processor = new FastNLPProcessor()
+  val examplesWithPatterns = Seq(
+    ("My dog ate my homework.",
+     """
+     trigger: ate
+     agent: nsubj
+     patient: dobj
+     """),
+
+    ("My dog drove my mom to the store.",
+     """
+     trigger: drove
+     agent: nsubj
+     patient: dobj
+     posstest: dobj > poss
+     destination: prep
+     """) // Unfortunately, FastNLP doesn't return collapsed deps
+  )
+
+  def processExamples(testPairs: Seq[(String, String)]) {
+    for ((text, rule) <- testPairs) {
+      val doc = processor.annotate(text)
+      val s = doc.sentences(0)
+      val dm = new DependencyMatcher(rule)
+
+      // Our sentence...
+      println(s"\n\n${{0 until s.words.length}.mkString("\t")}\n${s.words.mkString("\t")}")
+      // Find any matches (currently all arguments are optional)
+      val myMap = dm.findAllIn(s)
+
+      // What do we get?
+      println("\nOutgoing deps:")
+      val deps = s.dependencies.get
+      deps.outgoingEdges.zipWithIndex foreach { case (edges, i) => println(s"$i:\t${edges.mkString(" ")}") }
+      println("\nAssigned labels:")
+      myMap.flatten.foreach(pair => println(s"\t${pair._1} -> ${pair._2}"))
+      println(s"\nRule:$rule\n")
+    }
+  }
+
+  processExamples(examplesWithPatterns)
+}
