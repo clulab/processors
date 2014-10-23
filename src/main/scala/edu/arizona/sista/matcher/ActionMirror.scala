b906360bc52dd89a29daaaae68ccad1efc850bc2
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-10-23 14:31:48 -0700

    changed method signature

--- src/main/scala/edu/arizona/sista/matcher/ActionMirror.scala
+++ src/main/scala/edu/arizona/sista/matcher/ActionMirror.scala
@@ -1,13 +1,14 @@
 package edu.arizona.sista.matcher
 
 import scala.reflect.runtime.universe._
+import edu.arizona.sista.processors.Document
 
 class ActionMirror(obj: AnyRef) {
   private val instanceMirror = runtimeMirror(obj.getClass.getClassLoader).reflect(obj)
 
-  def invoke(name: String, args: Int): Int = {
+  def invoke(name: String, document: Document, sentence: Int, mentions: Map[String, Seq[Mention]]): Seq[Mention] = {
     val methodSymbol = instanceMirror.symbol.typeSignature.member(newTermName(name)).asMethod
     val methodMirror = instanceMirror.reflectMethod(methodSymbol)
-    methodMirror(args).asInstanceOf[Int]
+    methodMirror(document, sentence, mentions).asInstanceOf[Seq[Mention]]
   }
 }
