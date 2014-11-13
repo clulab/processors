179f0f67c8c9a5797d970d15c3ee15f73594910a
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-11-12 22:44:28 -0700

    move DependencyExtractor to top of file

--- src/main/scala/edu/arizona/sista/matcher/dependencies/DependencyExtractor.scala
+++ src/main/scala/edu/arizona/sista/matcher/dependencies/DependencyExtractor.scala
@@ -7,6 +7,158 @@ import edu.arizona.sista.processors.Sentence
 
 
 
+class DependencyExtractor(trigger: TriggerFinder, arguments: Seq[ArgumentExtractor])
+extends Extractor {
+  private val required = arguments filter (_.required == true)
+  private val optional = arguments filter (_.required == false)
+
+  def findAllIn(sentence: Sentence, state: State, ruleName: String): Seq[Map[String, Seq[Int]]] =
+    trigger.findAllIn(sentence, state, ruleName) flatMap (t => extractArgs(sentence, state, t))
+
+  private def extractArgs(sent: Sentence, state: State, tok: Int) = {
+    val req = extract(required, sent, state, tok)
+    if (req.exists(_._2.isEmpty)) None
+    else Some(req ++ extract(optional, sent, state, tok) + ("trigger" -> Seq(tok)))
+  }
+
+  private def extract(args: Seq[ArgumentExtractor], sent: Sentence, state: State, i: Int) =
+    args.map(a => (a.name -> a.findAllIn(sent, state, i))).toMap
+}
+
+object DependencyExtractor {
+  def apply(input: String): DependencyExtractor = Parser.parse(input)
+}
+
+
+
+object Parser extends RegexParsers {
+
+  // remove comment lines and trim text
+  def cleanInput(input:String): String = input.replaceAll("""(?m)^\s*#.*$""", "").trim
+
+  def parse(input: String): DependencyExtractor = parseAll(depExtractor, cleanInput(input)) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => sys.error(failure.msg)
+  }
+
+  override val whiteSpace = """[ \t\x0B\f\r]+""".r
+  val eol = "\n"
+
+  def ident: Parser[String] =
+    """\p{javaJavaIdentifierStart}\p{javaJavaIdentifierPart}*""".r
+
+  // single- or double-quote delimited string literal
+  def stringLiteral: Parser[String] =
+    """"[^\\"]*(?:\\.[^\\"]*)*"|'[^\\']*(?:\\.[^\\']*)*'""".r ^^ {
+      case s => """\\(.)""".r.replaceAllIn(s.drop(1).dropRight(1), m => m.group(1))
+    }
+
+  def exactLiteral: Parser[String] = ident | stringLiteral
+
+  // match a perl style "/" delimited regular expression
+  // "\" is the escape character, so "\/" becomes "/"
+  def regexLiteral: Parser[Regex] = """/[^\\/]*(?:\\.[^\\/]*)*/""".r ^^ {
+    case s => s.drop(1).dropRight(1).replaceAll("""\\/""", "/").r
+  }
+
+  def exactMatcher: Parser[MatcherNode] = exactLiteral ^^ {
+    case string => new ExactMatcher(string)
+  }
+
+  def regexMatcher: Parser[MatcherNode] = regexLiteral ^^ {
+    case regex => new RegexMatcher(regex)
+  }
+
+  def stringMatcher: Parser[MatcherNode] = exactMatcher | regexMatcher
+
+  def outgoingExtractor: Parser[ExtractorNode] = opt(">") ~> stringMatcher ^^ {
+    case matcher => new OutgoingExtractor(matcher)
+  }
+
+  def incomingExtractor: Parser[ExtractorNode] = "<" ~> stringMatcher ^^ {
+    case matcher => new IncomingExtractor(matcher)
+  }
+
+  def atomExtractor: Parser[ExtractorNode] =
+    outgoingExtractor | incomingExtractor | "(" ~> orExtractor <~ ")"
+
+  def quantifiedExtractor: Parser[ExtractorNode] = atomExtractor ~ opt("?"|"*"|"+") ^^ {
+    case extractor ~ None => extractor
+    case extractor ~ Some("?") => new OptionalExtractor(extractor)
+    case extractor ~ Some("*") => new KleeneExtractor(extractor)
+    case extractor ~ Some("+") => new ConcatExtractor(extractor, new KleeneExtractor(extractor))
+  }
+
+  def filteredExtractor: Parser[ExtractorNode] = quantifiedExtractor ~ opt(tokenFilter) ^^ {
+    case extractor ~ None => extractor
+    case extractor ~ Some(filter) => new FilteredExtractor(extractor, filter)
+  }
+
+  def concatExtractor: Parser[ExtractorNode] = filteredExtractor ~ rep(filteredExtractor) ^^ {
+    case first ~ rest => (first /: rest) {
+      case (lhs, rhs) => new ConcatExtractor(lhs, rhs)
+    }
+  }
+
+  def orExtractor: Parser[ExtractorNode] = concatExtractor ~ rep("|" ~> concatExtractor) ^^ {
+    case first ~ rest => (first /: rest) {
+      case (lhs, rhs) => new OrExtractor(lhs, rhs)
+    }
+  }
+
+  def filterValue: Parser[FilterNode] = ident ~ "=" ~ stringMatcher ^^ {
+    case "word" ~ _ ~ matcher => new WordFilter(matcher)
+    case "lemma" ~ _ ~ matcher => new LemmaFilter(matcher)
+    case "tag" ~ _ ~ matcher => new TagFilter(matcher)
+    case "entity" ~ _ ~ matcher => new EntityFilter(matcher)
+    case "chunk" ~ _ ~ matcher => new ChunkFilter(matcher)
+    case "mention" ~ _ ~ matcher => new MentionFilter(matcher)
+    case "incoming" ~ _ ~ matcher => new IncomingFilter(matcher)
+    case "outgoing" ~ _ ~ matcher => new OutgoingFilter(matcher)
+    case _ => sys.error("unrecognized filter")
+  }
+
+  def filterAtom: Parser[FilterNode] = filterValue | "(" ~> orFilter <~ ")"
+
+  def notFilter: Parser[FilterNode] = opt("!") ~ filterAtom ^^ {
+    case None ~ filter => filter
+    case Some(_) ~ filter => new NotFilter(filter)
+  }
+
+  def andFilter: Parser[FilterNode] = notFilter ~ rep("&" ~> notFilter) ^^ {
+    case first ~ rest => (first /: rest) {
+      case (lhs, rhs) => new AndFilter(lhs, rhs)
+    }
+  }
+
+  def orFilter: Parser[FilterNode] = andFilter ~ rep("|" ~> andFilter) ^^ {
+    case first ~ rest => (first /: rest) {
+      case (lhs, rhs) => new OrFilter(lhs, rhs)
+    }
+  }
+
+  def tokenFilter: Parser[FilterNode] = "[" ~> orFilter <~ "]"
+
+  def comment: Parser[String] = "#.*".r
+
+  def triggerFinder: Parser[TriggerFinder] = "trigger" ~> ":" ~> tokenFilter <~ opt(comment) ^^ {
+    case filter => new TriggerFinder(filter)
+  }
+
+  def argExtractor: Parser[ArgumentExtractor] = ident ~ opt("?") ~ ":" ~ orExtractor <~ opt(comment) ^^ {
+    case "trigger" ~ _ ~ _ ~ _ => sys.error("`trigger` is not a valid argument name")
+    case name ~ None ~ _ ~ extractor => new ArgumentExtractor(name, true, extractor)
+    case name ~ Some(_) ~ _ ~ extractor => new ArgumentExtractor(name, false, extractor)
+  }
+
+  def depExtractor: Parser[DependencyExtractor] =
+    triggerFinder ~ rep1(eol) ~ repsep(argExtractor, rep1(eol)) ^^ {
+      case trigger ~ _ ~ arguments  => new DependencyExtractor(trigger, arguments)
+    }
+}
+
+
+
 sealed trait Values {
   def values(tokens: Seq[Int], strings: Seq[String]): Seq[(Int, String)] =
     tokens map (i => (i, strings(i)))
@@ -191,156 +343,4 @@ class TriggerFinder(filter: FilterNode) {
       t => state.mentionsForRule(s, t, ruleName).isEmpty
     }
   }
-}
-
-
-
-class DependencyExtractor(trigger: TriggerFinder, arguments: Seq[ArgumentExtractor])
-extends Extractor {
-  private val required = arguments filter (_.required == true)
-  private val optional = arguments filter (_.required == false)
-
-  def findAllIn(sentence: Sentence, state: State, ruleName: String): Seq[Map[String, Seq[Int]]] =
-    trigger.findAllIn(sentence, state, ruleName) flatMap (t => extractArgs(sentence, state, t))
-
-  private def extractArgs(sent: Sentence, state: State, tok: Int) = {
-    val req = extract(required, sent, state, tok)
-    if (req.exists(_._2.isEmpty)) None
-    else Some(req ++ extract(optional, sent, state, tok) + ("trigger" -> Seq(tok)))
-  }
-
-  private def extract(args: Seq[ArgumentExtractor], sent: Sentence, state: State, i: Int) =
-    args.map(a => (a.name -> a.findAllIn(sent, state, i))).toMap
-}
-
-object DependencyExtractor {
-  def apply(input: String): DependencyExtractor = Parser.parse(input)
-}
-
-
-
-object Parser extends RegexParsers {
-
-  // remove comment lines and trim text
-  def cleanInput(input:String): String = input.replaceAll("""(?m)^\s*#.*$""", "").trim
-
-  def parse(input: String): DependencyExtractor = parseAll(depExtractor, cleanInput(input)) match {
-    case Success(result, _) => result
-    case failure: NoSuccess => sys.error(failure.msg)
-  }
-
-  override val whiteSpace = """[ \t\x0B\f\r]+""".r
-  val eol = "\n"
-
-  def ident: Parser[String] =
-    """\p{javaJavaIdentifierStart}\p{javaJavaIdentifierPart}*""".r
-
-  // single- or double-quote delimited string literal
-  def stringLiteral: Parser[String] =
-    """"[^\\"]*(?:\\.[^\\"]*)*"|'[^\\']*(?:\\.[^\\']*)*'""".r ^^ {
-      case s => """\\(.)""".r.replaceAllIn(s.drop(1).dropRight(1), m => m.group(1))
-    }
-
-  def exactLiteral: Parser[String] = ident | stringLiteral
-
-  // match a perl style "/" delimited regular expression
-  // "\" is the escape character, so "\/" becomes "/"
-  def regexLiteral: Parser[Regex] = """/[^\\/]*(?:\\.[^\\/]*)*/""".r ^^ {
-    case s => s.drop(1).dropRight(1).replaceAll("""\\/""", "/").r
-  }
-
-  def exactMatcher: Parser[MatcherNode] = exactLiteral ^^ {
-    case string => new ExactMatcher(string)
-  }
-
-  def regexMatcher: Parser[MatcherNode] = regexLiteral ^^ {
-    case regex => new RegexMatcher(regex)
-  }
-
-  def stringMatcher: Parser[MatcherNode] = exactMatcher | regexMatcher
-
-  def outgoingExtractor: Parser[ExtractorNode] = opt(">") ~> stringMatcher ^^ {
-    case matcher => new OutgoingExtractor(matcher)
-  }
-
-  def incomingExtractor: Parser[ExtractorNode] = "<" ~> stringMatcher ^^ {
-    case matcher => new IncomingExtractor(matcher)
-  }
-
-  def atomExtractor: Parser[ExtractorNode] =
-    outgoingExtractor | incomingExtractor | "(" ~> orExtractor <~ ")"
-
-  def quantifiedExtractor: Parser[ExtractorNode] = atomExtractor ~ opt("?"|"*"|"+") ^^ {
-    case extractor ~ None => extractor
-    case extractor ~ Some("?") => new OptionalExtractor(extractor)
-    case extractor ~ Some("*") => new KleeneExtractor(extractor)
-    case extractor ~ Some("+") => new ConcatExtractor(extractor, new KleeneExtractor(extractor))
-  }
-
-  def filteredExtractor: Parser[ExtractorNode] = quantifiedExtractor ~ opt(tokenFilter) ^^ {
-    case extractor ~ None => extractor
-    case extractor ~ Some(filter) => new FilteredExtractor(extractor, filter)
-  }
-
-  def concatExtractor: Parser[ExtractorNode] = filteredExtractor ~ rep(filteredExtractor) ^^ {
-    case first ~ rest => (first /: rest) {
-      case (lhs, rhs) => new ConcatExtractor(lhs, rhs)
-    }
-  }
-
-  def orExtractor: Parser[ExtractorNode] = concatExtractor ~ rep("|" ~> concatExtractor) ^^ {
-    case first ~ rest => (first /: rest) {
-      case (lhs, rhs) => new OrExtractor(lhs, rhs)
-    }
-  }
-
-  def filterValue: Parser[FilterNode] = ident ~ "=" ~ stringMatcher ^^ {
-    case "word" ~ _ ~ matcher => new WordFilter(matcher)
-    case "lemma" ~ _ ~ matcher => new LemmaFilter(matcher)
-    case "tag" ~ _ ~ matcher => new TagFilter(matcher)
-    case "entity" ~ _ ~ matcher => new EntityFilter(matcher)
-    case "chunk" ~ _ ~ matcher => new ChunkFilter(matcher)
-    case "mention" ~ _ ~ matcher => new MentionFilter(matcher)
-    case "incoming" ~ _ ~ matcher => new IncomingFilter(matcher)
-    case "outgoing" ~ _ ~ matcher => new OutgoingFilter(matcher)
-    case _ => sys.error("unrecognized filter")
-  }
-
-  def filterAtom: Parser[FilterNode] = filterValue | "(" ~> orFilter <~ ")"
-
-  def notFilter: Parser[FilterNode] = opt("!") ~ filterAtom ^^ {
-    case None ~ filter => filter
-    case Some(_) ~ filter => new NotFilter(filter)
-  }
-
-  def andFilter: Parser[FilterNode] = notFilter ~ rep("&" ~> notFilter) ^^ {
-    case first ~ rest => (first /: rest) {
-      case (lhs, rhs) => new AndFilter(lhs, rhs)
-    }
-  }
-
-  def orFilter: Parser[FilterNode] = andFilter ~ rep("|" ~> andFilter) ^^ {
-    case first ~ rest => (first /: rest) {
-      case (lhs, rhs) => new OrFilter(lhs, rhs)
-    }
-  }
-
-  def tokenFilter: Parser[FilterNode] = "[" ~> orFilter <~ "]"
-
-  def comment: Parser[String] = "#.*".r
-
-  def triggerFinder: Parser[TriggerFinder] = "trigger" ~> ":" ~> tokenFilter <~ opt(comment) ^^ {
-    case filter => new TriggerFinder(filter)
-  }
-
-  def argExtractor: Parser[ArgumentExtractor] = ident ~ opt("?") ~ ":" ~ orExtractor <~ opt(comment) ^^ {
-    case "trigger" ~ _ ~ _ ~ _ => sys.error("`trigger` is not a valid argument name")
-    case name ~ None ~ _ ~ extractor => new ArgumentExtractor(name, true, extractor)
-    case name ~ Some(_) ~ _ ~ extractor => new ArgumentExtractor(name, false, extractor)
-  }
-
-  def depExtractor: Parser[DependencyExtractor] =
-    triggerFinder ~ rep1(eol) ~ repsep(argExtractor, rep1(eol)) ^^ {
-      case trigger ~ _ ~ arguments  => new DependencyExtractor(trigger, arguments)
-    }
 }
