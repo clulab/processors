0b1ae08ac79578f7c6ac538f84c267b623db84a5
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-03-26 22:35:15 -0700

    deleted unused imports

--- src/main/scala/edu/arizona/sista/odin/impl/ActionMirror.scala
+++ src/main/scala/edu/arizona/sista/odin/impl/ActionMirror.scala
@@ -2,8 +2,6 @@ package edu.arizona.sista.odin.impl
 
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
-import edu.arizona.sista.struct.Interval
-import edu.arizona.sista.processors.Document
 import edu.arizona.sista.odin._
 
 class ActionMirror[A <: Actions : ClassTag](actions: A) {
