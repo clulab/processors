5716345ac5eb66272445f4cd5d710f3ba9b2175f
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-03-31 19:08:35 -0700

    added comment

--- src/main/scala/edu/arizona/sista/odin/Actions.scala
+++ src/main/scala/edu/arizona/sista/odin/Actions.scala
@@ -1,5 +1,13 @@
 package edu.arizona.sista.odin
 
+/** Inherit from this class to implement your custom actions.
+  *
+  * An action is a method of the form:
+  * {{{
+  * def customAction(mentions: Seq[Mention], state: State): Seq[Mention]
+  * }}}
+  */
 class Actions {
+  /** Returns the mentions untransformed */
   def identity(mentions: Seq[Mention], state: State): Seq[Mention] = mentions
 }
