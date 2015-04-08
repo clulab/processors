c7dbd700dd6dec5addd3f4565fd09e08aa944f0d
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-04-08 12:20:17 -0700

    added comments to Mention fields

--- src/main/scala/edu/arizona/sista/odin/Mention.scala
+++ src/main/scala/edu/arizona/sista/odin/Mention.scala
@@ -6,36 +6,65 @@ import edu.arizona.sista.processors.Document
 import edu.arizona.sista.odin.impl.StringMatcher
 
 trait Mention extends Equals {
+  /** A sequence of labels for this mention.
+    * The first label in the sequence is considered the default.
+    */
   def labels: Seq[String]
+
+  /** The interval of token indicess that form this mention. */
   def tokenInterval: Interval
+
+  /** The index of the sentence where this mention occurs. */
   def sentence: Int
+
+  /** The document where this mention occurs. */
   def document: Document
+
+  /** Should we report this mention at the end? */
   def keep: Boolean
+
+  /** A map from argument name to a sequence of mentions.
+    *
+    * The value of the map is a sequence because there are events
+    * that can have several arguments with the same name.
+    * For example, in the biodomain, Binding may have several themes.
+    */
   val arguments: Map[String, Seq[Mention]]
 
-  // points to an Xref object that represents an entry in an external database
+  /** points to an Xref object that represents an entry in an external database */
   var xref: Option[Xref] = None
   def isGrounded: Boolean = xref.isDefined
   def ground(namespace: String, id: String) = xref = Some(Xref(namespace, id))
 
-  // default label
+  /** default label */
   def label: String = labels.head
 
-  // name of matching rule
+  /** name of matching rule */
   def foundBy: String
 
+  /** index of the first token in the mention */
   def start: Int = tokenInterval.start
+
+  /** one after the last token in the mention */
   def end: Int = tokenInterval.end
 
-  // character offsets
+  /** character offset of the mention beginning */
   def startOffset: Int = document.sentences(sentence).startOffsets(start)
+
+  /** character offset of the mention end */
   def endOffset: Int = document.sentences(sentence).endOffsets(end - 1)
 
+  /** returns true if the string matches any of the mention labels */
   def matches(label: String): Boolean = labels contains label
+
+  /** returns true if the StringMatcher matches any of the mention labels */
   def matches(matcher: StringMatcher): Boolean = labels exists matcher.matches
 
-  // returns all tokens in mention
+  /** returns all tokens in mention */
   def words: Seq[String] = document.sentences(sentence).words.slice(start, end)
+
+  /** returns a string that contains the mention */
+  // FIXME This string doesn't match the original text. We can do better than this.
   def text: String = words.mkString(" ")
 
   override def canEqual(a: Any) = a.isInstanceOf[Mention]
