f68c6ee5cd61785739d2fa4a3c2b2ce4e95d8368
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-04-09 19:43:37 -0700

    renamed AndThenDarpaFlow to ComposedDarpaFlow

--- src/main/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/DarpaFlow.scala
+++ src/main/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/DarpaFlow.scala
@@ -5,17 +5,17 @@ import edu.arizona.sista.odin._
 trait DarpaFlow {
   /** Gets the candidate mentions and returns the final mentions.
     *
-    * @param mentions this iteration's candidate mentions
+    * @param mentions current iteration's candidate mentions
     * @param state contains mentions from previous iterations
-    * @return the final mentions
+    * @return current iteration's final mentions
     */
   def apply(mentions: Seq[Mention], state: State): Seq[Mention]
 
   /** Composes two instances of DarpaFlow into a single DarpaFlow */
-  def andThen(that: DarpaFlow): DarpaFlow = new AndThenDarpaFlow(this, that)
+  def andThen(that: DarpaFlow): DarpaFlow = new ComposedDarpaFlow(this, that)
 }
 
-class AndThenDarpaFlow(step1: DarpaFlow, step2: DarpaFlow) extends DarpaFlow {
+class ComposedDarpaFlow(step1: DarpaFlow, step2: DarpaFlow) extends DarpaFlow {
   def apply(mentions: Seq[Mention], state: State): Seq[Mention] =
     step2(step1(mentions, state), state)
 }
