795ab694da6bdfd8a052328324da76b0d483d6a5
Tom Hicks <hickst@tohono.com>
2015-04-10 22:28:44 -0700

    Abstract out the 2 column, TSV KB accessor. Convert HMDB to 2-col TSV and rename.

--- src/main/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/LocalKBAccessors.scala
+++ src/main/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/LocalKBAccessors.scala
@@ -6,10 +6,94 @@ import edu.arizona.sista.odin._
 import edu.arizona.sista.odin.extern.inward._
 
 /**
-  * Classes which implements project internal knowledge base accessors.
+  * A collections of classes which implement project internal knowledge base accessors.
   *   Written by Tom Hicks. 4/10/2015.
-  *   Last Modified: Add subcellular locations (cell components) KB.
+  *   Last Modified: Abstract out the 2 column, TSV KB accessor.
   */
+
+/**
+  * Abstract class which reads two-column, tab-separated-value (TSV) text files
+  * where the first column is the name string and the second is the ID string.
+  * Several of our knowledge bases follow this pattern and can simply extend this class.
+  */
+abstract class AzNameIdKBAccessor extends ExternalKBAccessor {
+  protected val theKB = scala.collection.mutable.Map[String, Map[String,String]]()
+
+  override def resolve (mention:Mention): Map[String,String] = {
+    val key = getLookupKey(mention).toLowerCase // lookup keys are all lowercase in this KB
+    theKB.getOrElseUpdate(key, Map.empty)
+  }
+
+  protected def readAndFillKB (kbResourcePath:String) = {
+    val kbStream = this.getClass.getResourceAsStream(kbResourcePath)
+    for (line <- Source.fromInputStream(kbStream).getLines) {
+      val fields = line.split("\t").map(_.trim)
+      if ((fields.size == 2) && fields(0).nonEmpty && fields(1).nonEmpty) {
+        val storageKey = fields(0).toLowerCase // lookup keys are all lowercase in this KB
+        theKB(storageKey) = Map(               // create new entry in KB
+          "referenceID" -> fields(1),
+          "namespace" -> namespace,
+          "baseURI" -> baseURI,
+          "resourceID" -> resourceID,
+          "key" -> storageKey,
+          "text" -> fields(0)               // return original text
+        )
+      }
+    }
+    kbStream.close()
+  }
+}
+
+
+/** KB accessor to resolve protein names in mentions. */
+class AzProteinKBAccessor extends ExternalKBAccessor {
+  def baseURI = "http://identifiers.org/uniprot/"
+  def namespace = "uniprotkb"
+  def resourceID = "MIR:00100164"
+  override def resolve (mention:Mention): Map[String,String] = Map.empty // TODO: IMPLEMENT
+}
+
+
+/** KB accessor to resolve protein family names in mentions. */
+class AzProteinFamilyKBAccessor extends ExternalKBAccessor {
+  def baseURI = "http://identifiers.org/pfam/"
+  def namespace = "pfam"
+  def resourceID = "MIR:00000028"
+  override def resolve (mention:Mention): Map[String,String] = Map.empty // TODO: IMPLEMENT
+}
+
+
+/** KB accessor to resolve small molecule (chemical) names in mentions. */
+class AzSmallMoleculeKBAccessor extends AzNameIdKBAccessor {
+  def baseURI = "http://identifiers.org/hmdb/"
+  def namespace = "hmdb"
+  def resourceID = "MIR:00000051"
+
+  // MAIN: load KB to initialize class
+  readAndFillKB("/edu/arizona/sista/odin/domains/bigmechanism/summer2015/hmdb.tsv")
+}
+
+
+/** KB accessor to resolve subcellular location names in mentions. */
+class AzSubcellularLocationKBAccessor extends AzNameIdKBAccessor {
+  def baseURI = "http://identifiers.org/uniprot/"
+  def namespace = "uniprot"
+  def resourceID = "MIR:00000005"
+
+  // MAIN: load KB to initialize class
+  readAndFillKB("/edu/arizona/sista/odin/domains/bigmechanism/summer2015/subcellular_locations.tsv")
+}
+
+
+/** KB accessor to resolve tissue type names in mentions. */
+class AzTissueTypeKBAccessor extends AzNameIdKBAccessor {
+  def baseURI = "http://identifiers.org/uniprot/"
+  def namespace = "uniprot"
+  def resourceID = "MIR:00000005"
+
+  // MAIN: load KB to initialize class
+  readAndFillKB("/edu/arizona/sista/odin/domains/bigmechanism/summer2015/tissue-type.tsv")
+}
 
 
 /** KB accessor implementation which always resolves each mention with a local, fake ID. */
@@ -35,139 +119,6 @@ class AzFailsafeKBAccessor extends ExternalKBAccessor {
     )
   }
 }
-
-
-/** KB accessor to resolve protein names in mentions. */
-class AzProteinKBAccessor extends ExternalKBAccessor {
-  def baseURI = "http://identifiers.org/uniprot/"
-  def namespace = "uniprotkb"
-  def resourceID = "MIR:00100164"
-  override def resolve (mention:Mention): Map[String,String] = Map.empty
-}
-
-
-/** KB accessor to resolve protein family names in mentions. */
-class AzProteinFamilyKBAccessor extends ExternalKBAccessor {
-  def baseURI = "http://identifiers.org/pfam/"
-  def namespace = "pfam"
-  def resourceID = "MIR:00000028"
-  override def resolve (mention:Mention): Map[String,String] = Map.empty
-}
-
-
-/** KB accessor to resolve small molecule (chemical) names in mentions. */
-class AzSmallMoleculeKBAccessor extends ExternalKBAccessor {
-  def baseURI = "http://identifiers.org/hmdb/"
-  def namespace = "hmdb"
-  def resourceID = "MIR:00000051"
-
-  private val theKB = scala.collection.mutable.Map[String, Map[String,String]]()
-
-  override def resolve (mention:Mention): Map[String,String] = {
-    val key = getLookupKey(mention).toLowerCase // lookup keys are all lowercase in this KB
-    theKB.getOrElseUpdate(key, Map.empty)
-  }
-
-  private def readAndFillKB = {
-    val kbStream = this.getClass.getResourceAsStream(
-      "/edu/arizona/sista/odin/domains/bigmechanism/summer2015/hmdb.kb")
-    for (line <- Source.fromInputStream(kbStream).getLines) {
-      val fields = line.split("\t").map(_.trim)
-      if ((fields.size > 2) && fields(0).nonEmpty && fields(2).nonEmpty) {
-        val storageKey = fields(0).toLowerCase // lookup keys are all lowercase in this KB
-        theKB(storageKey) = Map(               // create new entry in KB
-          "referenceID" -> fields(2),
-          "namespace" -> namespace,
-          "baseURI" -> baseURI,
-          "resourceID" -> resourceID,
-          "key" -> storageKey,
-          "text" -> fields(0)               // return original text
-        )
-      }
-    }
-    kbStream.close()
-  }
-
-  // MAIN: load KB to initialize class
-  readAndFillKB
-}
-
-
-/** KB accessor to resolve tissue type names in mentions. */
-class AzTissueTypeKBAccessor extends ExternalKBAccessor {
-  def baseURI = "http://identifiers.org/uniprot/"
-  def namespace = "uniprot"
-  def resourceID = "MIR:00000005"
-
-  private val theKB = scala.collection.mutable.Map[String, Map[String,String]]()
-
-  override def resolve (mention:Mention): Map[String,String] = {
-    val key = getLookupKey(mention).toLowerCase // lookup keys are all lowercase in this KB
-    theKB.getOrElseUpdate(key, Map.empty)
-  }
-
-  private def readAndFillKB = {
-    val kbStream = this.getClass.getResourceAsStream(
-      "/edu/arizona/sista/odin/domains/bigmechanism/summer2015/tissue-type.tsv")
-    for (line <- Source.fromInputStream(kbStream).getLines) {
-      val fields = line.split("\t").map(_.trim)
-      if ((fields.size >= 2) && fields(0).nonEmpty && fields(1).nonEmpty) {
-        val storageKey = fields(0).toLowerCase // lookup keys are all lowercase in this KB
-        theKB(storageKey) = Map(               // create new entry in KB
-          "referenceID" -> fields(1),
-          "namespace" -> namespace,
-          "baseURI" -> baseURI,
-          "resourceID" -> resourceID,
-          "key" -> storageKey,
-          "text" -> fields(0)               // return original text
-        )
-      }
-    }
-    kbStream.close()
-  }
-
-  // MAIN: load KB to initialize class
-  readAndFillKB
-}
-
-
-/** KB accessor to resolve subcellular location names in mentions. */
-class AzSubcellularLocationKBAccessor extends ExternalKBAccessor {
-  def baseURI = "http://identifiers.org/uniprot/"
-  def namespace = "uniprot"
-  def resourceID = "MIR:00000005"
-
-  private val theKB = scala.collection.mutable.Map[String, Map[String,String]]()
-
-  override def resolve (mention:Mention): Map[String,String] = {
-    val key = getLookupKey(mention).toLowerCase // lookup keys are all lowercase in this KB
-    theKB.getOrElseUpdate(key, Map.empty)
-  }
-
-  private def readAndFillKB = {
-    val kbStream = this.getClass.getResourceAsStream(
-      "/edu/arizona/sista/odin/domains/bigmechanism/summer2015/subcellular_locations.tsv")
-    for (line <- Source.fromInputStream(kbStream).getLines) {
-      val fields = line.split("\t").map(_.trim)
-      if ((fields.size >= 2) && fields(0).nonEmpty && fields(1).nonEmpty) {
-        val storageKey = fields(0).toLowerCase // lookup keys are all lowercase in this KB
-        theKB(storageKey) = Map(               // create new entry in KB
-          "referenceID" -> fields(1),
-          "namespace" -> namespace,
-          "baseURI" -> baseURI,
-          "resourceID" -> resourceID,
-          "key" -> storageKey,
-          "text" -> fields(0)               // return original text
-        )
-      }
-    }
-    kbStream.close()
-  }
-
-  // MAIN: load KB to initialize class
-  readAndFillKB
-}
-
 
 /** Class to implement an incrementing counter for generating unique IDs. */
 class IncrementingCounter {
