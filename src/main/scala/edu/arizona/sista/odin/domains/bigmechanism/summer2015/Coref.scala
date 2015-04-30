ad910304bf59be30e70f1d3da53f1a0c3c86489a
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-04-30 13:27:34 -0700

    deleted conflicting code

--- src/main/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/Coref.scala
+++
@@ -1,8 +0,0 @@
-package edu.arizona.sista.odin.domains.bigmechanism.summer2015
-
-import edu.arizona.sista.odin._
-
-class Coref extends DarpaFlow {
-  // someday this method will do coreference resolution
-  def apply(mentions: Seq[Mention], state: State): Seq[Mention] = mentions
-}
