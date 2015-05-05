9b0f871a9666aee1b158a75c110830aacd7f345d
Tom Hicks <hickst@tohono.com>
2015-05-05 13:40:46 -0700

    Add printString representation to Xref.

--- src/main/scala/edu/arizona/sista/odin/Xref.scala
+++ src/main/scala/edu/arizona/sista/odin/Xref.scala
@@ -1,4 +1,7 @@
 package edu.arizona.sista.odin
 
 /** Represents an entry in an external database. Used for grounding mentions */
-case class Xref(namespace: String, id: String)
+case class Xref(namespace: String, id: String) {
+  /** Return a printable string representation of this Xref. */
+  def printString (): String = { s"${namespace}:${id}" }
+}
