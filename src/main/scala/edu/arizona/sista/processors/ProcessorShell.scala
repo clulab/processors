e1cdb9adce89c12914ae39ce314854e014ba465e
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-05-31 13:07:18 -0700

    Updated shell to have history, preloading, etc.

--- src/main/scala/edu/arizona/sista/processors/ProcessorShell.scala
+++ src/main/scala/edu/arizona/sista/processors/ProcessorShell.scala
@@ -1,9 +1,11 @@
 package edu.arizona.sista.processors
 
-import scala.io.StdIn
 import edu.arizona.sista.processors.bionlp.BioNLPProcessor
 import edu.arizona.sista.processors.corenlp.CoreNLPProcessor
 import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
+import java.io.File
+import jline.console.ConsoleReader
+import jline.console.history.FileHistory
 
 /**
  * A simple interactive shell
@@ -11,17 +13,61 @@ import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
  * Date: 3/13/14
  */
 object ProcessorShell {
+
+  val history = new FileHistory(new File(System.getProperty("user.home"), ".processorshellhistory"))
+  sys addShutdownHook {
+    history.flush() // flush file before exiting
+  }
+
+  val reader = new ConsoleReader
+  reader.setPrompt(">>> ")
+  reader.setHistory(history)
+
+  val commands = Map(
+    ":help" -> "show commands",
+    ":exit" -> "exit system"
+  )
+
   def main(args:Array[String]) {
     // create the processor
     //val proc:Processor = new CoreNLPProcessor(withDiscourse = false) // this uses the slow but better discourse parser
     //val proc:Processor = new FastNLPProcessor(useMalt = false) // this uses the fast but slightly worse discourse parser
     val proc:Processor = new BioNLPProcessor(withDiscourse = false, removeFigTabReferences = true)
 
-    while(true) {
-      print("> ")
-      var text = StdIn.readLine()
-      val doc = proc.annotate(text)
-      ProcessorExample.printDoc(doc)
+
+    println("Loading processor...\n")
+    proc.annotate("blah")
+    
+    println("\nWelcome to the ProcessorShell!")
+    printCommands()
+
+    var running = true
+
+    while (running) {
+      reader.readLine match {
+        case ":help" =>
+          printCommands()
+
+        case ":exit" | null =>
+          running = false
+
+        case text =>
+          val doc = proc.annotate(text)
+          ProcessorExample.printDoc(doc)
+      }
     }
+
+    // manual terminal cleanup
+    reader.getTerminal.restore()
+    reader.shutdown()
+
+  }
+
+  // summarize available commands
+  def printCommands(): Unit = {
+    println("\nCOMMANDS:")
+    for ((cmd, msg) <- commands)
+      println(s"\t$cmd\t=> $msg")
+    println()
   }
 }
