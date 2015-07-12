1315344ffc8b00781436fec25608f6d50e3be88a
Mihai Surdeanu <surdeanu@gmail.com>
2015-07-12 11:04:59 -0400

    TestPerceptronClassifier now uses scalatest

--- src/test/scala/edu/arizona/sista/learning/TestPerceptronClassifier.scala
+++ src/test/scala/edu/arizona/sista/learning/TestPerceptronClassifier.scala
@@ -2,17 +2,15 @@ package edu.arizona.sista.learning
 
 import java.io.File
 
-import org.scalatest.junit.AssertionsForJUnit
-import org.junit.Test
-import junit.framework.Assert._
+import org.scalatest._
 
 /**
  *
  * User: mihais
  * Date: 12/15/13
  */
-class TestPerceptronClassifier extends AssertionsForJUnit {
-  @Test def testClassifier() {
+class TestPerceptronClassifier extends FlatSpec with Matchers {
+  "PerceptronClassifier" should "have an accuracy > .97 on this dataset" in {
     val classifier = new PerceptronClassifier[Int, String](
       epochs = 10,
       marginRatio = 1.0)
@@ -21,17 +19,17 @@ class TestPerceptronClassifier extends AssertionsForJUnit {
 
     val datums = RVFDataset.mkDatumsFromSvmLightFormat("src/test/resources/edu/arizona/sista/learning/classification_test.txt.gz")
     val acc = computeAcc(datums, classifier)
-    println("Accuracy: " + acc)
-    assertTrue(acc > 0.97)
+    //println("Accuracy: " + acc)
+    acc should be > 0.97
 
     // make sure scores are the same after saving/loading
     val file = File.createTempFile("model", "dat")
-    println(s"Saving classifier to $file")
+    //println(s"Saving classifier to $file")
     file.deleteOnExit()
     classifier.saveTo(file.getAbsolutePath)
     val loadedClassifier = PerceptronClassifier.loadFrom[Int, String](file.getAbsolutePath)
     val newAcc = computeAcc(datums, loadedClassifier)
-    assertTrue(acc == newAcc)
+    acc should be (newAcc)
   }
 
   def computeAcc(datums:Iterable[Datum[Int, String]], classifier:Classifier[Int, String]) = {
