28c1cdcf9ef260dd92d8467bdc7ca579e15222f7
Mihai Surdeanu <surdeanu@gmail.com>
2015-07-12 09:18:49 -0400

    TestHashTrie now uses scalatest

--- src/test/scala/edu/arizona/sista/processors/TestHashTrie.scala
+++ src/test/scala/edu/arizona/sista/processors/TestHashTrie.scala
@@ -1,17 +1,15 @@
 package edu.arizona.sista.processors
 
 import edu.arizona.sista.struct.HashTrie
-import org.junit.Test
-import org.scalatest.junit.AssertionsForJUnit
-import org.junit.Assert._
+import org.scalatest._
 
 /**
  *
  * User: mihais
  * Date: 5/12/15
  */
-class TestHashTrie extends AssertionsForJUnit {
-  @Test def test1(): Unit = {
+class TestHashTrie extends FlatSpec with Matchers {
+  "the trie" should "label the text correctly with BIO labels" in {
     val trie = new HashTrie()
     trie.add(Array("a", "a", "b"))
     trie.add(Array("a", "b", "b"))
@@ -19,14 +17,25 @@ class TestHashTrie extends AssertionsForJUnit {
     trie.add(Array("a", "b", "b", "c"))
     trie.add(Array("a"))
 
-    println("TRIE:\n" + trie)
+    //println("TRIE:\n" + trie)
 
     val tokens = Array("a", "a", "b", "d", "a", "b", "d", "b", "b", "b")
     val labels = trie.find(tokens, "E", "O")
-    println("TOKENS: " + tokens.mkString(" "))
-    println("LABELS: " + labels.mkString(" "))
+    //println("TOKENS: " + tokens.mkString(" "))
+    //println("LABELS: " + labels.mkString(" "))
 
-    assertTrue(sameLabels(Array("B-E", "I-E", "I-E", "O", "B-E", "O", "O", "B-E", "I-E", "I-E"), labels))
+    sameLabels(Array("B-E", "I-E", "I-E", "O", "B-E", "O", "O", "B-E", "I-E", "I-E"), labels) should be (true)
+  }
+
+  "the trie" should "have 2 entries and contain 5 unique strings" in {
+    val trie = new HashTrie()
+    trie.add(Array("a", "a", "b"))
+    trie.add(Array("a", "b"))
+    trie.add(Array("c", "d", "e"))
+
+    //println("TRIE:\n" + trie)
+    trie.entries.size should be (2)
+    trie.uniqueStrings.size should be (5)
   }
 
   private def sameLabels(l1:Array[String], l2:Array[String]):Boolean = {
@@ -34,16 +43,5 @@ class TestHashTrie extends AssertionsForJUnit {
     for(i <- 0 until l1.length)
       if(l1(i) != l2(i)) return false
     true
-  }
-
-  @Test def test2(): Unit = {
-    val trie = new HashTrie()
-    trie.add(Array("a", "a", "b"))
-    trie.add(Array("a", "b"))
-    trie.add(Array("c", "d", "e"))
-
-    println("TRIE:\n" + trie)
-    assertTrue(trie.entries.size == 2)
-    assertTrue(trie.uniqueStrings.size == 5)
   }
 }
