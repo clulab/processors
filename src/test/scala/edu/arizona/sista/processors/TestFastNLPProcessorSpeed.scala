f83cdc285510bcb6974f937d28d1c0c11fcb4767
Mihai Surdeanu <surdeanu@gmail.com>
2015-07-12 10:04:42 -0400

    TestFastNLPProcessorSpeed now uses scalatest

--- src/test/scala/edu/arizona/sista/processors/TestFastNLPProcessorSpeed.scala
+++ src/test/scala/edu/arizona/sista/processors/TestFastNLPProcessorSpeed.scala
@@ -1,9 +1,7 @@
 package edu.arizona.sista.processors
 
 import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
-import org.junit.{Test, Before}
-import org.scalatest.junit.AssertionsForJUnit
-import org.junit.Assert._
+import org.scalatest._
 
 
 import scala.io.Source
@@ -13,18 +11,12 @@ import scala.io.Source
  * User: mihais
  * Date: 2/25/15
  */
-class TestFastNLPProcessorSpeed extends AssertionsForJUnit {
-  var proc:Processor = null
+class TestFastNLPProcessorSpeed extends FlatSpec with Matchers {
+  var proc:Processor = new FastNLPProcessor(internStrings = true, withDiscourse = false, useMalt = false)
 
-  @Before def constructProcessor() {
-    proc = new FastNLPProcessor(internStrings = true, withDiscourse = false, useMalt = false)
-  }
-
-  @Test def testParser1(): Unit = {
+  "FastNLPProcessor" should "process a file successfully" in {
     val text = Source.fromFile("src/test/resources/edu/arizona/sista/processors/raw_text.txt").getLines.mkString(" ")
-    println("Started parsing the big file...")
     val doc = proc.annotate(text)
-    assertTrue(true)
+    doc.sentences.length should be > 0
   }
-
 }
