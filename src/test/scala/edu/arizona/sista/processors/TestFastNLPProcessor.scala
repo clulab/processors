117e9329b4bcd58d3e5f166f7b29675eb5c3d659
Mihai Surdeanu <surdeanu@gmail.com>
2015-07-12 10:11:21 -0400

    TestFastNLPProcessor now uses scalatest

--- src/test/scala/edu/arizona/sista/processors/TestFastNLPProcessor.scala
+++ src/test/scala/edu/arizona/sista/processors/TestFastNLPProcessor.scala
@@ -2,9 +2,7 @@ package edu.arizona.sista.processors
 
 import edu.arizona.sista.discourse.rstparser.RelationDirection
 import edu.arizona.sista.struct.DirectedGraphEdgeIterator
-import org.scalatest.junit.AssertionsForJUnit
-import org.junit.{Test, Before}
-import org.junit.Assert._
+import org.scalatest._
 import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
 
 /**
@@ -12,54 +10,47 @@ import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
  * User: mihais
  * Date: 1/7/14
  */
-class TestFastNLPProcessor extends AssertionsForJUnit {
-  var proc:Processor = null
+class TestFastNLPProcessor extends FlatSpec with Matchers {
+  var proc:Processor = new FastNLPProcessor(internStrings = true, withDiscourse = true)
 
-  @Before def constructProcessor() {
-    proc = new FastNLPProcessor(internStrings = true, withDiscourse = true)
-  }
-
-  @Test def testParser1() {
+  "FastNLPProcessor" should "generate correct dependencies in test sentence 1" in {
     val doc = proc.annotate("John Smith went to China.")
 
     // malt can generate only Stanford basic dependencies rather than collapsed ones
     // so, for example, we will see "prep" labels rather than "prep_to"
     //println(doc.sentences.head.dependencies)
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(1, 0, "nn"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(2, 1, "nsubj"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(2, 3, "prep"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(3, 4, "pobj"))
+    doc.sentences.head.dependencies.get.hasEdge(1, 0, "nn") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(2, 1, "nsubj") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(2, 3, "prep") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(3, 4, "pobj") should be (true)
 
+    /*
     val it = new DirectedGraphEdgeIterator[String](doc.sentences.head.dependencies.get)
     while(it.hasNext) {
       val d = it.next()
       println(d._1 + " " + d._2 + " " + d._3)
     }
+    */
   }
 
-  @Test def testParser2() {
+  "FastNLPProcessor" should "generate correct dependencies in test sentence 2" in {
     val doc = proc.annotate("He bought some shoes.")
 
     //println(doc.sentences.head.dependencies)
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(1, 0, "nsubj"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(1, 3, "dobj"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(1, 4, "punct"))
-    assertTrue(doc.sentences.head.dependencies.get.hasEdge(3, 2, "det"))
-
-    val it = new DirectedGraphEdgeIterator[String](doc.sentences.head.dependencies.get)
-    while(it.hasNext) {
-      val d = it.next()
-      println(d._1 + " " + d._2 + " " + d._3)
-    }
+    doc.sentences.head.dependencies.get.hasEdge(1, 0, "nsubj") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(1, 3, "dobj") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(1, 4, "punct") should be (true)
+    doc.sentences.head.dependencies.get.hasEdge(3, 2, "det") should be (true)
   }
 
-  @Test def testDiscourse() {
+  "FastNLPProcessor" should "generate correct discourse relations in test 3" in {
     val doc = proc.annotate("John Smith went to China. He visited Beijing, on January 10th, 2013.")
     doc.clear()
 
     val d = doc.discourseTree.get
-    assertTrue(d.relationLabel == "elaboration")
-    assertTrue(d.relationDirection == RelationDirection.LeftToRight)
-    assertTrue(! d.isTerminal && d.children.length == 2)
+    d.relationLabel should be ("elaboration")
+    d.relationDirection should be (RelationDirection.LeftToRight)
+    d.isTerminal should be (false)
+    d.children.length should be (2)
   }
 }
