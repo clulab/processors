3186d2fc578a97421f16f941ba10a9095280f80a
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-07-17 12:38:26 -0700

    OdinExceptions now form a heirarchy

--- src/main/scala/edu/arizona/sista/odin/impl/OdinException.scala
+++ src/main/scala/edu/arizona/sista/odin/impl/OdinException.scala
@@ -3,6 +3,35 @@ package edu.arizona.sista.odin.impl
 /**
  * Custom Exceptions for Odin
  */
-class OdinException(msg: String) extends RuntimeException(msg)
 
-case class OdinCompileException(msg: String, ruleName: Option[String] = None) extends OdinException(msg)
+/**
+ * This is the basic Exception for Odin
+ * @param msg a String describing the Exception
+ */
+class OdinException(val msg: String) extends RuntimeException(msg)
+// unapply method must live in the companion object
+object  OdinException {
+  def unapply(e: OdinException): Option[String] = Some(e.msg)
+}
+
+/**
+ * This is the basic compile Exception for Odin
+ * @param msg a description of the compilation error encountered
+ */
+class OdinCompileException(msg: String) extends OdinException(msg)
+
+object  OdinCompileException {
+  def unapply(e: OdinCompileException): Option[String] = Some(e.msg)
+}
+
+/**
+ * This is the compile Exception for Odin when the failing rule name is available
+ * @param msg a description of the compilation error encountered
+ * @param ruleName the name of the failing rule
+ */
+class OdinNamedCompileException(msg: String, val ruleName: String) extends OdinCompileException(msg)
+
+object OdinNamedCompileException {
+  def unapply(e: OdinNamedCompileException): Option[(String, String)] = Some((e.msg, e.ruleName))
+}
+
