16c6b5b059297aca1a65de3b0383bb2818b5240e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-08-08 11:28:41 -0700

    use toy example to test master file

--- src/main/scala/edu/arizona/sista/odin/domains/toydomain/ToyDomainExample.scala
+++ src/main/scala/edu/arizona/sista/odin/domains/toydomain/ToyDomainExample.scala
@@ -10,62 +10,75 @@ object ToyDomainExample extends App {
                 |""".stripMargin
 
   // rules are written in yaml and support yaml comments (starting with the '#' character)
-  val rules = """| # this rule creates Protein mentions from named entity tags in IOB notation
-                 |- name: rule1
-                 |  label: Protein
-                 |  type: token
-                 |  pattern: |
-                 |    [entity='B-Gene_or_gene_product'] [entity='I-Gene_or_gene_product']*
+  val rules = """|taxonomy:
+                 |  - Entity:
+                 |    - Protein
+                 |  - Event:
+                 |    - SimpleEvent:
+                 |      - Binding
+                 |      - BioChemicalEvent:
+                 |        - Phosphorylation
+                 |        - Ubiquitination
+                 |    - ComplexEvent:
+                 |      - UpRegulation
+                 |      - DownRegulation
+                 |  - ExampleRelation
                  |
-                 | # this rule creates phosphorylation events
-                 | # the arguments are Protein mentions
-                 |- name: rule2
-                 |  label: [Phosphorylation, Event]
-                 |  pattern: |
-                 |    trigger = [word=/^phospho/ & tag=/^VB/]
-                 |    theme: Protein = dobj
-                 |    cause: Protein = nsubj
+                 |rules:
+                 |  # this rule creates Protein mentions from named entity tags in IOB notation
+                 |  - name: rule1
+                 |    label: Protein
+                 |    type: token
+                 |    pattern: |
+                 |      [entity='B-Gene_or_gene_product'] [entity='I-Gene_or_gene_product']*
                  |
-                 | # creates ubiquitination events
-                 | # note that the "trigger" field name is case insensitive
-                 |- name: rule3
-                 |  label:
-                 |    - Ubiquitination
-                 |    - Event
-                 |  pattern: |
-                 |    trigger = ubiquitination
-                 |    theme: Protein = prep_of
+                 |  # this rule creates phosphorylation events
+                 |  # the arguments are Protein mentions
+                 |  - name: rule2
+                 |    label: Phosphorylation
+                 |    pattern: |
+                 |      trigger = [word=/^phospho/ & tag=/^VB/]
+                 |      theme: Protein = dobj
+                 |      cause: Protein = nsubj
                  |
-                 | # creates a regulation with a ubiquitination event as the theme
-                 |- name: rule4
-                 |  label: DownRegulation
-                 |  pattern: |
-                 |    trigger = [lemma=inhibit]
-                 |    theme: Event = dobj
-                 |    cause: Protein = nsubj
+                 |  # creates ubiquitination events
+                 |  # note that the "trigger" field name is case insensitive
+                 |  - name: rule3
+                 |    label: Ubiquitination
+                 |    pattern: |
+                 |      trigger = ubiquitination
+                 |      theme: Protein = prep_of
                  |
-                 | # example of a surface rule that captures an event
-                 | # note that you need to capture a trigger for this to be an event mention
-                 |- name: rule5
-                 |  label: DownRegulation
-                 |  type: token
-                 |  pattern: |
-                 |    @theme:Phosphorylation and? (?<trigger> inhibits) the @cause:Ubiquitination
+                 |  # creates a regulation with a ubiquitination event as the theme
+                 |  - name: rule4
+                 |    label: DownRegulation
+                 |    pattern: |
+                 |      trigger = [lemma=inhibit]
+                 |      theme: Event = dobj
+                 |      cause: Protein = nsubj
                  |
-                 | # example of an argument with a quantifier
-                 | # with the '+' this rule finds a single binding event with 3 themes
-                 | # without the '+' this rule finds 3 bindings with 1 theme each
-                 |- name: rule6
-                 |  label: Binding
-                 |  pattern: |
-                 |    trigger = binds
-                 |    theme: Protein+ = nsubj | prep_to
+                 |  # example of a surface rule that captures an event
+                 |  # note that you need to capture a trigger for this to be an event mention
+                 |  - name: rule5
+                 |    label: DownRegulation
+                 |    type: token
+                 |    pattern: |
+                 |      @theme:Phosphorylation and? (?<trigger> inhibits) the @cause:Ubiquitination
                  |
-                 |- name: rule7
-                 |  label: ExampleRelation
-                 |  pattern: |
-                 |    prot1: Protein
-                 |    prot2: Protein = conj_and
+                 |  # example of an argument with a quantifier
+                 |  # with the '+' this rule finds a single binding event with 3 themes
+                 |  # without the '+' this rule finds 3 bindings with 1 theme each
+                 |  - name: rule6
+                 |    label: Binding
+                 |    pattern: |
+                 |      trigger = binds
+                 |      theme: Protein+ = nsubj | prep_to
+                 |
+                 |  - name: rule7
+                 |    label: ExampleRelation
+                 |    pattern: |
+                 |      prot1: Protein
+                 |      prot2: Protein = conj_and
                  |""".stripMargin
 
   // creates an extractor engine using the rules and the default actions
