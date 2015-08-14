2ad463357198336c8b360d5cdd2d7a142fa217db
Mihai Surdeanu <surdeanu@gmail.com>
2015-08-14 13:32:06 -0700

    BioNLPProcessor now converts Unicode to ASCII, just like nxml2fries

--- src/main/scala/edu/arizona/sista/processors/bionlp/BioNLPPreProcessor.scala
+++ src/main/scala/edu/arizona/sista/processors/bionlp/BioNLPPreProcessor.scala
@@ -1,6 +1,10 @@
 package edu.arizona.sista.processors.bionlp
 
+import java.io.{InputStreamReader, BufferedReader}
 import java.util.regex.Pattern
+
+import scala.StringBuilder
+import scala.collection.mutable
 
 /**
  * Preprocesses bio text, including Unicode normalization, and removing figure and table references
@@ -8,6 +12,8 @@ import java.util.regex.Pattern
  * Date: 8/14/15
  */
 class BioNLPPreProcessor(removeFigTabReferences:Boolean) {
+  val unicodes = BioNLPPreProcessor.loadUnicodes
+
   def preprocess(origText:String):String = {
     val textWithoutUnicode = replaceUnicodeWithAscii(origText)
     val textWithoutFigRefs = removeFigureAndTableReferences(textWithoutUnicode)
@@ -15,7 +21,16 @@ class BioNLPPreProcessor(removeFigTabReferences:Boolean) {
   }
 
   def replaceUnicodeWithAscii(origText:String):String = {
-    origText
+    val os = new StringBuilder()
+    for(i <- 0 until origText.length) {
+      val c = origText.charAt(i)
+      if(unicodes.contains(c)) {
+        os.append(unicodes.get(c).get)
+      } else {
+        os.append(c)
+      }
+    }
+    os.toString()
   }
 
   def removeFigureAndTableReferences(origText:String):String = {
@@ -54,4 +69,37 @@ object BioNLPPreProcessor {
   val FIGTAB_REFERENCE_WITH_PARENS = Pattern.compile("\\((\\s*see)?(\\s*supplementary)?\\s*(figure|table|fig\\.|tab\\.)[^\\)]*\\)", Pattern.CASE_INSENSITIVE)
   val FIGTAB_REFERENCE = Pattern.compile("\\s*see(\\s*supplementary)?\\s*(figure|table|fig\\.|tab\\.)\\s*[0-9A-Za-z\\.]+", Pattern.CASE_INSENSITIVE)
 
+  val UNICODE_TO_ASCII = "edu/arizona/sista/processors/bionlp/unicode_to_ascii.tsv"
+
+  def loadUnicodes:Map[Char, String] = {
+    val map = new mutable.HashMap[Char, String]()
+    val is = BioNLPPreProcessor.getClass.getClassLoader.getResourceAsStream(UNICODE_TO_ASCII)
+    assert(is != null, s"Failed to find resource file $UNICODE_TO_ASCII in the classpath!")
+    val reader = new BufferedReader(new InputStreamReader(is))
+    var done = false
+    while(! done) {
+      var line = reader.readLine()
+      if(line == null) {
+        done = true
+      } else {
+        line = line.trim
+        if(! line.startsWith("#")) {
+          val tokens = line.split("\\t")
+          if(tokens.length > 2)
+            throw new RuntimeException(s"ERROR: invalid line [$line] in resource file $UNICODE_TO_ASCII")
+          if(tokens.length == 1) {
+            map += (toUnicodeChar(tokens(0)) -> "")
+          } else {
+            map += (toUnicodeChar(tokens(0)) -> tokens(1))
+          }
+        }
+      }
+    }
+    reader.close()
+    map.toMap
+  }
+
+  def toUnicodeChar(s:String):Char = {
+    Integer.parseInt(s, 16).toChar
+  }
 }
