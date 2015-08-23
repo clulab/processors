4d566feb6b198f97819c7753416092c28b1b769a
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-08-23 14:50:43 -0700

    added chunker

--- src/main/scala/edu/arizona/sista/processors/shallownlp/ShallowNLPProcessor.scala
+++ src/main/scala/edu/arizona/sista/processors/shallownlp/ShallowNLPProcessor.scala
@@ -2,8 +2,10 @@ package edu.arizona.sista.processors.shallownlp
 
 import java.util
 import java.util.Properties
+import java.nio.file.Paths
 
 import edu.arizona.sista.processors.corenlp.CoreNLPDocument
+import edu.arizona.sista.processors.corenlp.chunker.CRFChunker
 import edu.arizona.sista.processors._
 import edu.stanford.nlp.ling.CoreAnnotations._
 import edu.stanford.nlp.ling.{CoreAnnotations, CoreLabel}
@@ -26,6 +28,7 @@ class ShallowNLPProcessor(val internStrings:Boolean = true) extends Processor {
   lazy val posTagger = mkPosTagger
   lazy val lemmatizer = mkLemmatizer
   lazy val ner = mkNer
+  lazy val chunker = mkChunker
 
   def mkTokenizerWithoutSentenceSplitting: StanfordCoreNLP = {
     val props = new Properties()
@@ -57,6 +60,12 @@ class ShallowNLPProcessor(val internStrings:Boolean = true) extends Processor {
     val props = new Properties()
     props.put("annotators", "ner")
     new StanfordCoreNLP(props, false)
+  }
+
+  def mkChunker: CRFChunker = {
+    val url = getClass.getClassLoader.getResource("edu/arizona/sista/processors/corenlp/chunker/chunker.crf.gz")
+    val file = Paths.get(url.toURI).toFile
+    CRFChunker.load(file)
   }
 
   /**
@@ -357,7 +366,12 @@ class ShallowNLPProcessor(val internStrings:Boolean = true) extends Processor {
   }
 
   def chunking(doc:Document) {
-    // TODO: add shallow parsing here!
+    for (s <- doc.sentences) {
+      val words = s.words
+      val tags = s.tags.get
+      val chunks = chunker.classify(words, tags)
+      s.chunks = Some(chunks)
+    }
   }
 
   def labelSemanticRoles(doc:Document) {
