e21595fda02e7bbda152d8353d57c9d70b34be6a
Dane Bell <dane@email.arizona.edu>
2015-10-23 11:36:07 -0700

    Use proper tag for Generic_entity in requirement for args in EventMentions

--- src/main/scala/edu/arizona/sista/odin/Mention.scala
+++ src/main/scala/edu/arizona/sista/odin/Mention.scala
@@ -186,7 +186,7 @@ class EventMention(
     val foundBy: String
 ) extends Mention {
 
-  require(arguments.values.flatten.nonEmpty || labels.contains("Unresolved"), "EventMentions need arguments")
+  require(arguments.values.flatten.nonEmpty || labels.contains("Generic_entity"), "EventMentions need arguments")
 
   def this(
     label: String,
