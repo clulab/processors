360d9d0ee39af7ddaeff9d91e69e0359f81976a7
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-12-29 21:37:22 -0700

    renamed methodMirror => action

--- src/main/scala/edu/arizona/sista/odin/impl/ActionMirror.scala
+++ src/main/scala/edu/arizona/sista/odin/impl/ActionMirror.scala
@@ -11,21 +11,21 @@ class ActionMirror(actions: Actions) {
 
   def reflect(name: String): Action = {
     val methodSymbol = instanceMirror.symbol.typeSignature.member(TermName(name)).asMethod
-    val methodMirror = instanceMirror.reflectMethod(methodSymbol)
+    val action = instanceMirror.reflectMethod(methodSymbol)
     // handle action based on its return type
     val returnType = methodSymbol.returnType
     if (returnType =:= typeOf[Action]) {
       // val action: Action
-      methodMirror().asInstanceOf[Action]
+      action().asInstanceOf[Action]
     } else if (returnType =:= typeOf[Seq[Mention]]) {
       // def action(mentions: Seq[Mention], state: State): Seq[Mention]
       (ms: Seq[Mention], st: State) => {
-        methodMirror(ms, st).asInstanceOf[Seq[Mention]]
+        action(ms, st).asInstanceOf[Seq[Mention]]
       }
     } else if (returnType =:= typeOf[JList[Mention]]) {
       // java.util.List<Mention> action(java.util.List<Mention> mentions, State state)
       (ms: Seq[Mention], st: State) => {
-        methodMirror(ms.asJava, st).asInstanceOf[JList[Mention]].asScala
+        action(ms.asJava, st).asInstanceOf[JList[Mention]].asScala
       }
     } else {
       sys.error(s"invalid action '$name'")
