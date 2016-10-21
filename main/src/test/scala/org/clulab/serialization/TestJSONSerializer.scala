6d005d5e4593a9dc13834e80d74d27090bb9f636
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-10-21 00:15:46 -0700

    Test json serialization for CrossSentenceMention

--- main/src/test/scala/org/clulab/serialization/TestJSONSerializer.scala
+++ main/src/test/scala/org/clulab/serialization/TestJSONSerializer.scala
@@ -55,6 +55,36 @@ class TestJSONSerializer extends FlatSpec with Matchers {
     m.tokenInterval should equal (mentions.head.tokenInterval)
   }
 
+  it should "serialize/deserialize a CrossSentenceMention to/from json correctly" in {
+    val text = "Barack Obama is the 44th President of the United States. He was born on August 4, 1961 in Honolulu, Hawaii."
+    val doc = jsonStringToDocument(""" {"sentences":[{"words":["Barack","Obama","is","the","44th","President","of","the","United","States","."],"startOffsets":[0,7,13,16,20,25,35,38,42,49,55],"endOffsets":[6,12,15,19,24,34,37,41,48,55,56],"tags":["NNP","NNP","VBZ","DT","JJ","NN","IN","DT","NNP","NNPS","."],"lemmas":["Barack","Obama","be","the","44th","president","of","the","United","States","."],"entities":["PERSON","PERSON","O","O","ORDINAL","O","O","O","LOCATION","LOCATION","O"],"norms":["O","O","O","O","44.0","O","O","O","O","O","O"],"chunks":["B-NP","I-NP","B-VP","B-NP","I-NP","I-NP","B-PP","B-NP","I-NP","I-NP","O"],"graphs":{}},{"words":["He","was","born","on","August","4",",","1961","in","Honolulu",",","Hawaii","."],"startOffsets":[57,60,64,69,72,79,80,82,87,90,98,100,106],"endOffsets":[59,63,68,71,78,80,81,86,89,98,99,106,107],"tags":["PRP","VBD","VBN","IN","NNP","CD",",","CD","IN","NNP",",","NNP","."],"lemmas":["he","be","bear","on","August","4",",","1961","in","Honolulu",",","Hawaii","."],"entities":["O","O","O","O","DATE","DATE","DATE","DATE","O","LOCATION","O","LOCATION","O"],"norms":["O","O","O","O","1961-08-04","1961-08-04","1961-08-04","1961-08-04","O","O","O","O","O"],"chunks":["B-NP","B-VP","I-VP","B-PP","B-NP","I-NP","O","B-NP","B-PP","B-NP","O","B-NP","O"],"graphs":{}}]} """)
+
+    val rule =
+      """
+        |- name: coref-example
+        |  label: Coref
+        |  priority: 2
+        |  # the maximum number of sentences to search to the left of the anchor
+        |  left-window: 1
+        |  # don't attempt to match anywhere to the right of the anchor
+        |  example: "Barack Obama is the 44th President of the United States. He was born on August 4, 1961 in Honolulu, Hawaii."
+        |  type: cross-sentence
+        |  pattern: |
+        |    # start here at the pattern's anchor
+        |    # the pronoun to be resolved
+        |    anaphor: Entity = [lemma="he"]
+        |    antecedent: Entity = Barack Obama
+      """.stripMargin
+    val ee = ExtractorEngine(rule)
+    val mentions = ee.extractFrom(doc)
+    mentions should have size (1)
+    val Seq(coref) = mentions
+    (coref matches "Coref") should be (true)
+    val deserializedMentions = JSONSerializer.toMentions(mentions.jsonAST)
+    deserializedMentions should have size (mentions.size)
+    deserializedMentions.map(_.equivalenceHash).toSet should equal(mentions.map(_.equivalenceHash).toSet)
+  }
+
   it should "serialize/deserialize a Seq[Mention] to/from json correctly " in {
     val mentions2 = JSONSerializer.toMentions(mentions.jsonAST)
     mentions2 should have size mentions.size
