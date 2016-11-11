1cf11276020df500793e3d862b3733f8ab43627c
Terron Ishihara <tishihara@email.arizona.edu>
2016-11-10 21:56:42 -0700

    Initial ConllxReader implementation, dependency tree to be built

--- corenlp/src/main/scala/org/clulab/processors/corenlp/parser/ParserUtils.scala
+++ corenlp/src/main/scala/org/clulab/processors/corenlp/parser/ParserUtils.scala
@@ -1,11 +1,17 @@
 package org.clulab.processors.corenlp.parser
 
-import java.io.Reader
+import java.io.{BufferedReader, Reader}
 import java.util.Properties
+
 import com.typesafe.config.ConfigFactory
 import edu.stanford.nlp.parser.nndep.DependencyParser
 import edu.stanford.nlp.trees._
+import org.clulab.processors.{Document, Sentence}
+import org.clulab.struct.{GraphMap, MutableNumber, Tree}
+
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.ClassTag
 
 
 object TrainDependencyParser extends App {
@@ -62,4 +68,101 @@ object TreebankConverter extends App {
     tp.printTree(t)
     System.out.println("----------")
   }
+}
+
+// adapted from main/org.clulab.serialization.DocumentSerializer
+class ConllxReader {
+  val NIL = "_"
+  val SEP = "\t"
+  val NUM_COLS = 10
+
+  def load(r:BufferedReader): Document = {
+    var line:String = null
+    val sents = new ArrayBuffer[Sentence]
+    var sent:Sentence = null
+
+    while ((sent = loadSentence(r)) != null) {
+      sents += sent
+    }
+
+    val doc = Document(sents.toArray)
+    doc
+  }
+
+  private def loadSentence(r:BufferedReader): Sentence = {
+    // Ensure there is something to read
+    if (r.readLine == null) return null
+    r.reset // resets to most recent mark, not the beginning
+
+    val wordBuffer = new ArrayBuffer[String]
+    val startOffsetBuffer = new ArrayBuffer[Int]
+    val endOffsetBuffer = new ArrayBuffer[Int]
+    val tagBuffer = new ArrayBuffer[String]
+    var nilTags = true
+    val lemmaBuffer = new ArrayBuffer[String]
+    var nilLemmas = true
+
+    var line:String = null
+    var tokenCount = 0
+    while((line = r.readLine) != null && line != "") {
+      val bits = line.split(SEP)
+
+      if(bits.length != NUM_COLS) {
+        throw new RuntimeException("ERROR: invalid line: " + bits.mkString(" "))
+      }
+
+      assert(bits.length == NUM_COLS)
+      startOffsetBuffer += bits(0).toInt - 1
+      endOffsetBuffer += bits(6).toInt - 1 // may end up being -1 (root), which should be ignored
+      wordBuffer += bits(1)
+      lemmaBuffer += bits(2)
+      if (bits(2) != NIL) nilLemmas = false
+      tagBuffer += bits(4) // fine-grained POS tag (coarse-grained is column 3)
+      if (bits(4) != NIL) nilTags = false
+
+      tokenCount += 1
+    }
+
+    assert(wordBuffer.size == tokenCount)
+    assert(startOffsetBuffer.size == tokenCount)
+    assert(endOffsetBuffer.size == tokenCount)
+    assert(tagBuffer.isEmpty || tagBuffer.size == tokenCount)
+    assert(lemmaBuffer.isEmpty || lemmaBuffer.size == tokenCount)
+
+    var deps = new GraphMap
+    var tree:Option[Tree] = None
+    // FIXME: build dependency tree
+//    do {
+//      bits = read(r)
+//      if (bits(0) == START_DEPENDENCIES) {
+//        val dt = bits(1)
+//        val sz = bits(2).toInt
+//        val d = loadDependencies(r, sz)
+//        deps += (dt -> d)
+//      } else if (bits(0) == START_CONSTITUENTS) {
+//        val position = new MutableNumber[Int](0)
+//        bits = read(r)
+//        tree = Some(loadTree(bits, position))
+//      }
+//    } while(bits(0) != END_OF_SENTENCE)
+
+    Sentence(
+      wordBuffer.toArray,
+      startOffsetBuffer.toArray,
+      endOffsetBuffer.toArray,
+      bufferOption(tagBuffer, nilTags),
+      bufferOption(lemmaBuffer, nilLemmas),
+      None,
+      None,
+      None,
+      tree, deps
+    )
+  }
+
+  private def bufferOption[T: ClassTag](b:ArrayBuffer[T], allNils:Boolean): Option[Array[T]] = {
+    if (b.isEmpty) return None
+    if (allNils) return None
+    Some(b.toArray)
+  }
+
 }
