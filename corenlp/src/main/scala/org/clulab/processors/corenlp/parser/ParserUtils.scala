ba6a7c21a6bcffa298bc28ef538c6b6d2a53165e
Terron Ishihara <tishihara@email.arizona.edu>
2016-12-25 18:39:56 -0700

    Read args to determine corpora, embeddings, and k value

--- corenlp/src/main/scala/org/clulab/processors/corenlp/parser/ParserUtils.scala
+++ corenlp/src/main/scala/org/clulab/processors/corenlp/parser/ParserUtils.scala
@@ -13,14 +13,46 @@ object ParserUtils {
 
 object TrainDependencyParser extends App {
 
+  if (args.length < 2 ||
+    !(args(0) == "wsj" || args(0) == "genia" || args(0) == "wsj-genia") ||
+    !(args(1) == "w2v" || args(1) == "pmc" || args(1) == "w2v-pmc")) {
+    println("Usage: TrainDependencyParser [wsj|genia|wsj-genia] [w2v|pmc|w2v-pmc] [1|2|3|4|5]")
+    System.exit(1)
+  }
+
   val config = ConfigFactory.load()
 
-  val language = config.getString("corenlp.language")
-  val tf = config.getString("corenlp.parser.wsj.trainFile")
-  val df = config.getString("corenlp.parser.wsj.devFile")
-  val ef = config.getString("corenlp.parser.w2v.embeddings")
-  val es = config.getString("corenlp.parser.w2v.embeddingsDim")
-  val mf = config.getString("corenlp.parser.wsj.model")
+  // wsj w2v
+  var language = config.getString("corenlp.language")
+  var tf = config.getString("corenlp.parser.wsj.trainFile")
+  var df = config.getString("corenlp.parser.wsj.devFile")
+  var ef = config.getString("corenlp.parser.w2v.embeddings")
+  var es = config.getString("corenlp.parser.w2v.embeddingsDim")
+  var mf = config.getString("corenlp.parser.wsj.model")
+
+  if (args(0) == "genia") {
+    tf = config.getString("corenlp.parser.genia.trainFile")
+    df = config.getString("corenlp.parser.genia.devFile")
+    mf = config.getString("corenlp.parser.genia.model")
+  }
+
+  if (args(0) == "wsj-genia") {
+    val k = args(2)
+    assert(k == "1" || k == "2" || k == "3" || k == "4" || k == "5")
+    tf = config.getString(s"corenlp.parser.wsj-genia.${k}.trainFile")
+    df = config.getString(s"corenlp.parser.wsj-genia.${k}.devFile")
+    mf = config.getString(s"corenlp.parser.wsj-genia.${k}.model")
+  }
+
+  if (args(1) == "pmc") {
+    ef = config.getString("corenlp.parser.pmc.embeddings")
+    es = config.getString("corenlp.parser.pmc.embeddingsDim")
+  }
+
+  if (args(1) == "w2v-pmc") {
+    ef = config.getString("corenlp.parser.w2v-pmc.embeddings")
+    es = config.getString("corenlp.parser.w2v-pmc.embeddingsDim")
+  }
 
   // prepare dependency parser
   val props = new Properties()
@@ -35,19 +67,19 @@ object TrainDependencyParser extends App {
 //  import java.io.File
 
   val dep = new DependencyParser(props)
-  dep.loadModelFile("/net/kate/storage/work/tishihara/en-bio-dep-genia-parser.model.txt.gz")
-  println("Loaded model!")
-
-  import org.clulab.processors.fastnlp.FastNLPProcessor
-  val proc = new FastNLPProcessor(withChunks = false)
-  val doc = proc.annotate("My name is Terron.")
-  println(s"Num of sentences: ${doc.sentences.length}")
-
-  import org.clulab.processors.corenlp.CoreNLPUtils
-  val cm = CoreNLPUtils.sentenceToCoreMap(doc.sentences.head)
-  val gs = dep.predict(cm)
-  println(s"${gs}")
+//  dep.loadModelFile("/net/kate/storage/work/tishihara/en-bio-dep-genia-parser.model.txt.gz")
+//  println("Loaded model!")
+//
+//  import org.clulab.processors.fastnlp.FastNLPProcessor
+//  val proc = new FastNLPProcessor(withChunks = false)
+//  val doc = proc.annotate("My name is Terron.")
+//  println(s"Num of sentences: ${doc.sentences.length}")
+//
+//  import org.clulab.processors.corenlp.CoreNLPUtils
+//  val cm = CoreNLPUtils.sentenceToCoreMap(doc.sentences.head)
+//  val gs = dep.predict(cm)
+//  println(s"${gs}")
 
   // train parser
-//  dep.train(tf, df, mf, ef, null)
+  dep.train(tf, df, mf, ef, null)
 }
