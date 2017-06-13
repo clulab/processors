3e25c59b20609d8e6a983d09914974752dc581a7
Tom Hicks <hickst@tohono.com>
2017-06-12 18:22:14 -0700

    Add tests for remaining actor methods.

--- corenlp/src/test/scala/org/clulab/processors/coserver/TestProcessorActor.scala
+++ corenlp/src/test/scala/org/clulab/processors/coserver/TestProcessorActor.scala
@@ -21,7 +21,7 @@ import ProcessorCoreServerMessages._
 /**
   * Unit tests of the ProcessorActor class.
   *   Written by: Tom Hicks. 6/6/2016.
-  *   Last Modified: Add tests for make document methods.
+  *   Last Modified: Add tests for remaining methods.
   */
 class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
     with FlatSpecLike
@@ -54,17 +54,27 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   "ProcessorActor" should "make document from zero-length text, keep text" in {
     val probe = TestProbe()
     val text = ""
-    probe.send(procActor, MkDocumentCmd(text, true))
+    probe.send(procActor, MkDocumentCmd(text, true)) // keep text
     val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
     (reply.doc.sentences.size) must equal(0)
     (reply.doc.text).isDefined must be (true)
     (reply.doc.text) must equal(Some(text))
   }
 
+  it should "make document from simple text, default keep" in {
+    val probe = TestProbe()
+    val text = "This is a test."
+    probe.send(procActor, MkDocumentCmd(text)) // default keep
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
   it should "make document from simple text, keep text" in {
     val probe = TestProbe()
     val text = "This is a test."
-    probe.send(procActor, MkDocumentCmd(text, true))
+    probe.send(procActor, MkDocumentCmd(text, true)) // keep text
     val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
     (reply.doc.sentences.size) must equal(1)
     (reply.doc.text).isDefined must be (true)
@@ -74,7 +84,7 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   it should "make document from simple text, discard text" in {
     val probe = TestProbe()
     val text = "This is a test."
-    probe.send(procActor, MkDocumentCmd(text, false))
+    probe.send(procActor, MkDocumentCmd(text, false)) // discard text
     val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
     (reply.doc.sentences.size) must equal(1)
     (reply.doc.text).isDefined must be (false)
@@ -85,7 +95,7 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   it should "make document from sentences, keep text" in {
     val probe = TestProbe()
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
-    probe.send(procActor, MkDocumentFromSentencesCmd(sents, true))
+    probe.send(procActor, MkDocumentFromSentencesCmd(sents, true)) // keep text
     val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
     (reply.doc.sentences.size) must equal(3)
     (reply.doc.text).isDefined must be (true)
@@ -95,7 +105,7 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   it should "make document from sentences, discard text" in {
     val probe = TestProbe()
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
-    probe.send(procActor, MkDocumentFromSentencesCmd(sents, false))
+    probe.send(procActor, MkDocumentFromSentencesCmd(sents, false)) // discard text
     val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
     (reply.doc.sentences.size) must equal(3)
     (reply.doc.text).isDefined must be (false)
@@ -146,32 +156,45 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   }
 
 
-  // annotate
-  it should "annotate zero-length document" in {
+  // preprocessText
+  it should "preprocess text from zero-length text" in {
     val probe = TestProbe()
-    val doc0 = processor.mkDocument("")
-    probe.send(procActor, AnnotateCmd(doc0))
-    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
-    (reply.doc.sentences.size) must equal(0)
+    val text = ""
+    probe.send(procActor, PreprocessTextCmd(text))
+    val reply = probe.expectMsgClass(timeout, classOf[TextMsg])
+    (reply.text) must equal(text)
   }
 
-  it should "annotate single sentence document" in {
+  it should "preprocess simple text" in {
     val probe = TestProbe()
-    val doc1 = processor.mkDocument("This is a document with a single sentence.")
-    probe.send(procActor, AnnotateCmd(doc1))
-    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
-    (reply.doc.sentences.size) must equal(1)
+    val text = "Testing is performed."
+    probe.send(procActor, PreprocessTextCmd(text))
+    val reply = probe.expectMsgClass(timeout, classOf[TextMsg])
+    (reply.text) must equal(text)
   }
 
-  it should "annotate multi-sentence document" in {
+  // preprocessSentences
+  it should "preprocess sentences" in {
     val probe = TestProbe()
-    val doc3 = processor.mkDocument(
-"""This document has multiple sentences. Each should be processed by the processor.
-   A Reach document should be returned.""")
-    probe.send(procActor, AnnotateCmd(doc3))
-    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
-    (reply.doc.sentences.size) must equal(3)
+    val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
+    probe.send(procActor, PreprocessSentencesCmd(sents))
+    val reply = probe.expectMsgClass(timeout, classOf[SentencesMsg])
+    (reply.sentences.size) must equal(3)
+    reply.sentences.zipWithIndex.foreach { case(replySent, ndx) =>
+      (replySent) must equal(sents(ndx))
+    }
   }
+
+  // preprocessTokens
+  it should "preprocess tokens" in {
+    val probe = TestProbe()
+    val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
+    probe.send(procActor, PreprocessTokensCmd(toks))
+    val reply = probe.expectMsgClass(timeout, classOf[TokensMsg])
+    (reply.tokens.size) must equal(2)
+    (reply.tokens.flatten) must equal(toks.flatten)
+  }
+
 
   // tagPartsOfSpeech
   it should "tagPartsOfSpeech in a small document" in {
@@ -289,5 +312,179 @@ class TestProcessorActor extends TestKit(ActorSystem("test-proc-actor"))
   //   reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
   //   (reply.doc.discourseTree) must not be (empty)
   // }
+
+
+  // annotate(text)
+  it should "annotate text, default keep" in {
+    val probe = TestProbe()
+    val text = "This is a document with a single sentence."
+    probe.send(procActor, AnnotateTextCmd(text))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  it should "annotate text, keep text" in {
+    val probe = TestProbe()
+    val text = "This is single sentence test."
+    probe.send(procActor, AnnotateTextCmd(text, true)) // explicit keep
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (true)
+    (reply.doc.text) must equal(Some(text))
+  }
+
+  it should "annotate text, discard text" in {
+    val probe = TestProbe()
+    val text = "This is a document with a single sentence."
+    probe.send(procActor, AnnotateTextCmd(text, false)) // explicit discard
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  // annotateFromSentences
+  it should "annotate sentences, default keep" in {
+    val probe = TestProbe()
+    val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
+    probe.send(procActor, AnnotateFromSentencesCmd(sents))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  it should "annotate sentences, keep text" in {
+    val probe = TestProbe()
+    val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
+    probe.send(procActor, AnnotateFromSentencesCmd(sents, true)) // explicit keep
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (true)
+    (reply.doc.text) must equal(Some(sents.mkString(" ")))
+  }
+
+  it should "annotate sentences, discard text" in {
+    val probe = TestProbe()
+    val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
+    probe.send(procActor, AnnotateFromSentencesCmd(sents, false)) // explicit discard
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+
+  // annotateFromTokens
+  it should "annotate tokens, default keep" in {
+    val probe = TestProbe()
+    val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
+    probe.send(procActor, AnnotateFromTokensCmd(toks))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(2)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  it should "annotate tokens, keep text" in {
+    val probe = TestProbe()
+    val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
+    val text = toks.map(t => t.mkString(" ")).mkString(" ")  // spacing: tok=1, sent=1
+    probe.send(procActor, AnnotateFromTokensCmd(toks, true)) // explicit keep
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(2)
+    (reply.doc.text).isDefined must be (true)
+    (reply.doc.text) must equal(Some(text))
+  }
+
+  it should "annotate tokens, discard text" in {
+    val probe = TestProbe()
+    val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
+    probe.send(procActor, AnnotateFromTokensCmd(toks, false)) // explicit discard
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(2)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+
+  // annotate(Document)
+  it should "annotate zero-length Document, default keep" in {
+    val probe = TestProbe()
+    val doc0 = processor.mkDocument("")
+    probe.send(procActor, AnnotateCmd(doc0))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(0)
+    (reply.doc.text).isDefined must be (false)
+  }
+
+  it should "annotate single sentence Document, no text to propagate" in {
+    val probe = TestProbe()
+    val text = "This is a document with a single sentence."
+    val doc = processor.mkDocument(text)    // doc w/o kept text
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  it should "annotate single sentence Document, keep text" in {
+    val probe = TestProbe()
+    val text = "This is a document with a single sentence."
+    val doc = processor.mkDocument(text, true) // doc with text kept
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (true)
+    (reply.doc.text) must equal(Some(text))
+  }
+
+  it should "annotate single sentence Document, discard text" in {
+    val probe = TestProbe()
+    val text = "This is a document with a single sentence."
+    val doc = processor.mkDocument(text, false) // doc w/o kept text
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(1)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+
+  it should "annotate multi-sentence Document, no text to propagate" in {
+    val probe = TestProbe()
+    val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
+    val doc = processor.mkDocument(text)    // doc w/o kept text
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
+
+  it should "annotate multi-sentence Document, keep text" in {
+    val probe = TestProbe()
+    val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
+    val doc = processor.mkDocument(text, true) // doc with text kept
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (true)
+    (reply.doc.text) must equal(Some(text))
+  }
+
+  it should "annotate multi-sentence Document, discard text" in {
+    val probe = TestProbe()
+    val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
+    val doc = processor.mkDocument(text)    // doc w/o kept text
+    probe.send(procActor, AnnotateCmd(doc))
+    val reply = probe.expectMsgClass(timeout, classOf[DocumentMsg])
+    (reply.doc.sentences.size) must equal(3)
+    (reply.doc.text).isDefined must be (false)
+    (reply.doc.text) must equal(None)
+  }
 
 }
