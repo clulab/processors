82435c0a5744683e2a0a56e88e15b770e05fa413
Gus Hahn-Powell <gushahnpowell@gmail.com>
2017-06-19 00:47:02 -0700

    Tests for #153

--- odin/src/test/scala/org/clulab/odin/TestGraphPattern.scala
+++ odin/src/test/scala/org/clulab/odin/TestGraphPattern.scala
@@ -4,9 +4,9 @@ import org.clulab.TestUtils.jsonStringToDocument
 import org.clulab.struct.Interval
 import org.scalatest._
 
-class TestDependencyPattern extends FlatSpec with Matchers {
+class TestGraphPattern extends FlatSpec with Matchers {
 
-  "DependencyPattern" should "support multiline patterns" in {
+  "GraphPattern" should "support multiline patterns" in {
 
     val text = "I saw Kermit at the pond."
     val doc = jsonStringToDocument(""" {"sentences":[{"words":["I","saw","Kermit","at","the","pond","."],"startOffsets":[0,2,6,13,16,20,24],"endOffsets":[1,5,12,15,19,24,25],"tags":["PRP","VBD","NNP","IN","DT","NN","."],"lemmas":["I","see","Kermit","at","the","pond","."],"entities":["O","O","PERSON","O","O","O","O"],"norms":["O","O","O","O","O","O","O"],"chunks":["B-NP","B-VP","B-NP","B-PP","B-NP","I-NP","O"],"graphs":{"stanford-basic":{"edges":[{"source":1,"destination":0,"relation":"nsubj"},{"source":1,"destination":2,"relation":"dobj"},{"source":1,"destination":3,"relation":"prep"},{"source":1,"destination":6,"relation":"punct"},{"source":3,"destination":5,"relation":"pobj"},{"source":5,"destination":4,"relation":"det"}],"roots":[1]},"stanford-collapsed":{"edges":[{"source":1,"destination":0,"relation":"nsubj"},{"source":1,"destination":2,"relation":"dobj"},{"source":1,"destination":5,"relation":"prep_at"},{"source":1,"destination":6,"relation":"punct"},{"source":5,"destination":4,"relation":"det"}],"roots":[1]}}}]} """)
@@ -93,4 +93,135 @@ class TestDependencyPattern extends FlatSpec with Matchers {
 
   }
 
+  "GraphPattern" should "support 'type: graph'" in {
+
+    // "In 1987, Phillip Jeffries disappeared in a Buenos Aires hotel."
+    val doc = jsonStringToDocument(""" { "sentences": [ { "endOffsets": [ 2, 7, 8, 16, 25, 37, 40, 42, 49, 55, 61, 62 ], "entities": [ "O", "DATE", "O", "PERSON", "PERSON", "O", "O", "O", "LOCATION", "LOCATION", "O", "O" ], "graphs": { "stanford-basic": { "edges": [ { "destination": 1, "relation": "pobj", "source": 0 }, { "destination": 3, "relation": "nn", "source": 4 }, { "destination": 0, "relation": "prep", "source": 5 }, { "destination": 2, "relation": "punct", "source": 5 }, { "destination": 4, "relation": "nsubj", "source": 5 }, { "destination": 6, "relation": "prep", "source": 5 }, { "destination": 11, "relation": "punct", "source": 5 }, { "destination": 10, "relation": "pobj", "source": 6 }, { "destination": 7, "relation": "det", "source": 10 }, { "destination": 8, "relation": "nn", "source": 10 }, { "destination": 9, "relation": "nn", "source": 10 } ], "roots": [ 5 ] }, "stanford-collapsed": { "edges": [ { "destination": 3, "relation": "nn", "source": 4 }, { "destination": 1, "relation": "prep_in", "source": 5 }, { "destination": 2, "relation": "punct", "source": 5 }, { "destination": 4, "relation": "nsubj", "source": 5 }, { "destination": 10, "relation": "prep_in", "source": 5 }, { "destination": 11, "relation": "punct", "source": 5 }, { "destination": 7, "relation": "det", "source": 10 }, { "destination": 8, "relation": "nn", "source": 10 }, { "destination": 9, "relation": "nn", "source": 10 } ], "roots": [ 5 ] } }, "lemmas": [ "in", "1987", ",", "Phillip", "Jeffries", "disappear", "in", "a", "Buenos", "Aires", "hotel", "." ], "startOffsets": [ 0, 3, 7, 9, 17, 26, 38, 41, 43, 50, 56, 61 ], "tags": [ "IN", "CD", ",", "NNP", "NNP", "VBD", "IN", "DT", "NNP", "NNP", "NN", "." ], "words": [ "In", "1987", ",", "Phillip", "Jeffries", "disappeared", "in", "a", "Buenos", "Aires", "hotel", "." ] } ], "text": "In 1987, Phillip Jeffries disappeared in a Buenos Aires hotel." } """)
+
+    val mentions = Seq(
+      new TextBoundMention("Person", Interval(3, 5), 0, doc, false, "<test>"),
+      new TextBoundMention("Location", Interval(8, 10), 0, doc, false, "<test>"),
+      new TextBoundMention("Date", Interval(1), 0, doc, false, "<test>")
+    )
+
+    val state = State(mentions)
+
+    val rule1 = """
+                  |- name: "date-graph-default-1"
+                  |  label: Event
+                  |  type: graph
+                  |  pattern: |
+                  |    trigger = [lemma="disappear"]
+                  |    person: Person = nsubj
+                  |    location: Location = prep_in nn?
+                  |
+                  |""".stripMargin
+
+
+    val ee1 = ExtractorEngine(rule1)
+    val results1 = ee1.extractFrom(doc, state)
+    results1 should have size (1)
+  }
+
+  "GraphPattern" should "support 'graph: ' key" in {
+
+    // "In 1987, Phillip Jeffries disappeared in a Buenos Aires hotel."
+    val doc = jsonStringToDocument(""" { "sentences": [ { "endOffsets": [ 2, 7, 8, 16, 25, 37, 40, 42, 49, 55, 61, 62 ], "entities": [ "O", "DATE", "O", "PERSON", "PERSON", "O", "O", "O", "LOCATION", "LOCATION", "O", "O" ], "graphs": { "stanford-basic": { "edges": [ { "destination": 1, "relation": "pobj", "source": 0 }, { "destination": 3, "relation": "nn", "source": 4 }, { "destination": 0, "relation": "prep", "source": 5 }, { "destination": 2, "relation": "punct", "source": 5 }, { "destination": 4, "relation": "nsubj", "source": 5 }, { "destination": 6, "relation": "prep", "source": 5 }, { "destination": 11, "relation": "punct", "source": 5 }, { "destination": 10, "relation": "pobj", "source": 6 }, { "destination": 7, "relation": "det", "source": 10 }, { "destination": 8, "relation": "nn", "source": 10 }, { "destination": 9, "relation": "nn", "source": 10 } ], "roots": [ 5 ] }, "stanford-collapsed": { "edges": [ { "destination": 3, "relation": "nn", "source": 4 }, { "destination": 1, "relation": "prep_in", "source": 5 }, { "destination": 2, "relation": "punct", "source": 5 }, { "destination": 4, "relation": "nsubj", "source": 5 }, { "destination": 10, "relation": "prep_in", "source": 5 }, { "destination": 11, "relation": "punct", "source": 5 }, { "destination": 7, "relation": "det", "source": 10 }, { "destination": 8, "relation": "nn", "source": 10 }, { "destination": 9, "relation": "nn", "source": 10 } ], "roots": [ 5 ] } }, "lemmas": [ "in", "1987", ",", "Phillip", "Jeffries", "disappear", "in", "a", "Buenos", "Aires", "hotel", "." ], "startOffsets": [ 0, 3, 7, 9, 17, 26, 38, 41, 43, 50, 56, 61 ], "tags": [ "IN", "CD", ",", "NNP", "NNP", "VBD", "IN", "DT", "NNP", "NNP", "NN", "." ], "words": [ "In", "1987", ",", "Phillip", "Jeffries", "disappeared", "in", "a", "Buenos", "Aires", "hotel", "." ] } ], "text": "In 1987, Phillip Jeffries disappeared in a Buenos Aires hotel." } """)
+
+    val rule1 = """
+                 |- name: "date-token-collapsed-1"
+                 |  label: Date
+                 |  type: token
+                 |  pattern: |
+                 |    [entity=DATE & incoming=prep_in]
+                 |""".stripMargin
+
+    val ee1 = ExtractorEngine(rule1)
+    val results1 = ee1.extractFrom(doc)
+    results1 should have size (1)
+
+    val rule2 = """
+                  |- name: "date-token-collapsed-2"
+                  |  label: Date
+                  |  graph: "stanford-collapsed"
+                  |  type: token
+                  |  pattern: |
+                  |    [entity=DATE & incoming=prep_in]
+                  |""".stripMargin
+
+    val ee2 = ExtractorEngine(rule2)
+    val results2 = ee2.extractFrom(doc)
+    results2 should have size (1)
+
+    val rule3 = """
+                  |- name: "date-token-basic-1"
+                  |  label: Date
+                  |  graph: "stanford-basic"
+                  |  type: token
+                  |  pattern: |
+                  |    [entity=DATE & incoming=pobj]
+                  |""".stripMargin
+
+    val ee3 = ExtractorEngine(rule3)
+    val results3 = ee3.extractFrom(doc)
+    results3 should have size (1)
+
+
+
+    val mentions = Seq(
+      new TextBoundMention("Person", Interval(3, 5), 0, doc, false, "<test>"),
+      new TextBoundMention("Location", Interval(8, 10), 0, doc, false, "<test>"),
+      new TextBoundMention("Date", Interval(1), 0, doc, false, "<test>")
+    )
+
+    val state = State(mentions)
+
+    val rule4 = """
+                  |- name: "date-graph-collapsed-1"
+                  |  label: Event
+                  |  graph: "stanford-collapsed"
+                  |  pattern: |
+                  |    trigger = [lemma="disappear"]
+                  |    person: Person = nsubj
+                  |    location: Location = prep_in nn?
+                  |
+                  |""".stripMargin
+
+
+    val ee4 = ExtractorEngine(rule4)
+    val results4 = ee4.extractFrom(doc, state)
+    results4 should have size (1)
+
+    val rule5 = """
+                  |- name: "date-graph-collapsed-default-1"
+                  |  label: Event
+                  |  pattern: |
+                  |    trigger = [lemma="disappear"]
+                  |    person: Person = nsubj
+                  |    location: Location = prep_in nn?
+                  |
+                  |""".stripMargin
+
+
+    val ee5 = ExtractorEngine(rule5)
+    val results5 = ee5.extractFrom(doc, state)
+    results5 should have size (1)
+
+    val rule6 = """
+                  |- name: "date-graph-basic-1"
+                  |  label: Event
+                  |  graph: "stanford-basic"
+                  |  pattern: |
+                  |    trigger = [lemma="disappear"]
+                  |    person: Person = nsubj
+                  |    location: Location = prep [lemma="in"] pobj nn?
+                  |    date: Date = prep [lemma="in"] pobj
+                  |""".stripMargin
+
+    val ee6 = ExtractorEngine(rule6)
+    val results6 = ee6.extractFrom(doc, state)
+    results6 should have size (1)
+  }
+
+  //'type: graph'
 }
