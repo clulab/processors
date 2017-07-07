c8d07936998c477e0ca44ea48c9e938806462d46
Tom Hicks <hickst@tohono.com>
2017-07-07 12:58:53 -0700

    Set supervisory strategy on router. Redo instance creation.

--- corenlp/src/main/scala/org/clulab/processors/coserver/ProcessorCoreServer.scala
+++ corenlp/src/main/scala/org/clulab/processors/coserver/ProcessorCoreServer.scala
@@ -4,8 +4,9 @@ import com.typesafe.config.{ Config, ConfigValueFactory, ConfigFactory }
 import com.typesafe.scalalogging.LazyLogging
 
 import akka.actor._
+import akka.actor.SupervisorStrategy._
+import akka.routing._
 import akka.event.Logging
-import akka.routing.FromConfig
 
 import org.clulab.processors._
 import org.clulab.processors.bionlp._
@@ -16,7 +17,7 @@ import org.clulab.processors.shallownlp._
 /**
   * Application to wrap and serve various processor capabilities.
   *   Written by: Tom Hicks. 6/5/2017.
-  *   Last Modified: Use explicit single instance.
+  *   Last Modified: Set supervisory strategy on router. Redo instance creation.
   */
 object ProcessorCoreServer extends App with LazyLogging {
 
@@ -31,11 +32,16 @@ object ProcessorCoreServer extends App with LazyLogging {
 
   /** Create a single instance of the processor core server, only if it has been created. */
   def instance: ActorPath = {
-    val config = ConfigFactory.load().getConfig("ProcessorCoreService")
-    logger.debug(s"(ProcessorCoreServer.instance): config=${config}")
-    if (_server == null)
+    logger.debug(s"(ProcessorCoreServer.instance): server = ${_server}")
+    if (_server == null) {                  // create server, iff not already created
+      val config = ConfigFactory.load().getConfig("ProcessorCoreServer")
+      if (config == null)
+        throw new RuntimeException("(ProcessorCoreServer.instance): Unable to read configuration from configuration file.")
+      logger.debug(s"(ProcessorCoreServer.instance): config=${config}")
       _server = new ProcessorCoreServer(config)
-    _server.getPath
+    }
+    logger.debug(s"(ProcessorCoreServer.instance): server => ${_server}")
+    _server.getPath                         // return actor path to processor pool
   }
 
 }
@@ -49,7 +55,7 @@ class ProcessorCoreServer (
 ) extends LazyLogging {
 
   if (config == null)
-    throw new RuntimeException("Unable to read configuration from configuration file.")
+    throw new RuntimeException("(ProcessorCoreServer.ctor): Empty configuration argument not allowed.")
 
   // create the Processor engine specified by the configuration and used by this server
   private val processor: Processor = {
@@ -63,22 +69,26 @@ class ProcessorCoreServer (
       case _ => new ShallowNLPProcessor()
     }
   }
-  logger.debug(s"(ProcessorCoreServer.Class): processor=${processor}")
+  logger.debug(s"(ProcessorCoreServer.ctor): processor=${processor}")
 
   // fire up the actor system
   private val system = ActorSystem("proc-core-server", config)
 
-  logger.debug(s"(ProcessorCoreServer.Class): system=${system}")
+  logger.debug(s"(ProcessorCoreServer.ctor): system=${system}")
+
+  // set supervisory strategy property of the pool to handle errors
+  private final val restartEachStrategy: SupervisorStrategy =
+    OneForOneStrategy() { case _ => Restart }
 
   // create a pool of processor actors waiting for work
   private val procPool = system.actorOf(
-    FromConfig.props(ProcessorActor.props(processor)), "proc-actor-pool")
+    ProcessorActor.props(processor).withRouter(
+      FromConfig.withSupervisorStrategy(restartEachStrategy)),
+    "proc-actor-pool")
 
-  logger.debug(s"(ProcessorCoreServer.Class): procPoll=${procPool}")
+  logger.debug(s"(ProcessorCoreServer.ctor): procPool=${procPool}")
 
-  // TODO: set supervisory strategy property of the pool to handle errors LATER
 
-  /** Return an actor path to the current instance of the processor pool. */
-  def getPath: ActorPath = procPool.path
-
+  /** Returns an actor path to the current instance of the processor pool. */
+  val getPath: ActorPath = procPool.path
 }
