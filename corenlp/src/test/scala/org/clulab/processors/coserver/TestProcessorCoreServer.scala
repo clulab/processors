ae4c320a9f84f17a1c2fdd8e7ae9dcd508a83ed6
Tom Hicks <hickst@tohono.com>
2017-07-09 19:03:30 -0700

    Add a couple of full Akka tests of PCS router.

--- corenlp/src/test/scala/org/clulab/processors/coserver/TestProcessorCoreServer.scala
+++ corenlp/src/test/scala/org/clulab/processors/coserver/TestProcessorCoreServer.scala
@@ -1,42 +1,71 @@
 package org.clulab.processors.coserver
 
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 import org.scalatest.{ Matchers, FlatSpec }
 
 import com.typesafe.config.{ Config, ConfigValueFactory, ConfigFactory }
 import com.typesafe.scalalogging.LazyLogging
 
 import akka.actor._
+import akka.routing._
+import akka.pattern.ask
+import akka.util.Timeout
+
+import org.clulab.processors.Document
+import org.clulab.processors.coserver.ProcessorCoreServerMessages._
 
 /**
   * Tests of the ProcessorCoreServer.
   *   Written by: Tom Hicks. 6/14/2017.
-  *   Last Modified: Add additional small test of PCS class.
+  *   Last Modified: Add a couple of full Akka tests of PCS router.
   */
 class TestProcessorCoreServer extends FlatSpec with Matchers with LazyLogging {
 
+  // fire up the actor system
   private val system = ActorSystem("proc-core-server")
 
-  // load application configuration from the configuration file
-  val config = ConfigFactory.load().getConfig("ProcessorCoreServer")
-  logger.debug(s"config=${config}")
-
   // create a processor core server instance
-  val pcs = new ProcessorCoreServer(config)
-  logger.debug(s"ProcessorCoreServer instance=${pcs}")
+  val pcs = ProcessorCoreServer.instance
+  logger.debug(s"ProcessorCoreServer.instance=${pcs}")
 
-  "ProcessorCoreServer" should "return actor path to the processor actor pool" in {
-    val path = pcs.getPath
-    logger.debug(s"PATH=${path}")
-    (path) should not be (null)
-    (path.toString) should equal("akka://proc-core-server/user/proc-actor-pool")
+  // get a reference to the pooled router from the core server instance
+  val router = pcs.router
+  logger.debug(s"ProcessorCoreServer.router=${router}")
+
+  implicit val timeout = Timeout(30 seconds)
+
+  /** Send the given message to the server and block until response comes back. */
+  private def callServer (request: ProcessorCoreCommand): ProcessorCoreReply = {
+    val response = router ? request         // call returning Future
+    Await.result(response, timeout.duration).asInstanceOf[ProcessorCoreReply]
   }
 
-  // select the processor actor pool
-  val server: ActorSelection = system.actorSelection(pcs.getPath)
-  logger.debug(s"Found server ref: ${server}")
+  "ProcessorCoreServer" should "the pooled router should not be null" in {
+    (router) should not be (null)
+  }
 
-  it should "return the processor actor pool" in {
-    (server) should not be (null)
+  it should "make document from simple text, keep text" in {
+    val text = "This is some text sent from an application."
+    val reply = callServer(MkDocumentCmd(text, true)) // keep text
+    val doc = reply.asInstanceOf[DocumentMsg].doc
+    (doc) should not be (null)
+    (doc.sentences.size) should equal (1)
+    (doc.text).isDefined should be (true)
+    (doc.text) should equal (Some(text))
+  }
+
+  it should "annotate text, keep text" in {
+    val text = "This is single sentence test."
+    val reply = callServer(AnnotateTextCmd(text, true)) // keep text
+    val doc = reply.asInstanceOf[DocumentMsg].doc
+    (doc) should not be (null)
+    (doc.sentences.size) should equal(1)
+    (doc.text).isDefined should be (true)
+    (doc.text) should equal(Some(text))
   }
 
 }
