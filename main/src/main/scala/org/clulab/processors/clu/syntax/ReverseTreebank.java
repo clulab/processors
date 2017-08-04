56578aca73993568c9f8990b3b9078bac247abd2
Mihai Surdeanu <surdeanu@gmail.com>
2017-08-04 19:41:24 +0300

    Removed the ling package.

---
+++ main/src/main/scala/org/clulab/processors/clu/syntax/ReverseTreebank.java
@@ -0,0 +1,11 @@
+package org.clulab.processors.clu.syntax;
+
+/**
+ * Reverts word order in a CoNLL-X treebank from left-to-right to right-to-left
+ * This is needed to provide support for right-to-left dependency parsers
+ * User: mihais
+ * Date: 8/4/17
+ */
+public class ReverseTreebank {
+  
+}
