11db2398abe39f17601bd20f13b0233e122c1148
Tom Hicks <hickst@tohono.com>
2017-10-23 11:29:05 -0700

    Add debug logging to core client test.

--- main/src/test/scala/org/clulab/processors/coclient/TestProcessorCoreClient.scala
+++ main/src/test/scala/org/clulab/processors/coclient/TestProcessorCoreClient.scala
@@ -6,11 +6,13 @@ import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{ Matchers, FlatSpec }
 
 import org.clulab.processors.Document
+import org.clulab.processors.coshare.ProcessorCoreMessages._
+import org.clulab.utils.StringUtils
 
 /**
   * Tests of the ProcessorCoreClient.
   *   Written by: Tom Hicks. 6/20/2017.
-  *   Last Modified: Update for refactoring.
+  *   Last Modified: Add debug logging to core client test.
   */
 class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
@@ -40,6 +42,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // mkDocument
   it should "make document from zero-length text, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from zero-length text, keep text")
     val text = ""
     val doc = client.mkDocument(text, true) // keep text
     (doc) should not be (null)
@@ -49,6 +52,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from simple text, default keep" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from simple text, default keep")
     val text = "This is a test."
     val doc = client.mkDocument(text) // default keep
     (doc) should not be (null)
@@ -58,6 +62,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from simple text, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from simple text, keep text")
     val text = "This is a test."
     val doc = client.mkDocument(text, true) // keep text
     (doc) should not be (null)
@@ -67,6 +72,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from simple text, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from simple text, discard text")
     val text = "This is a test."
     val doc = client.mkDocument(text, false) // discard text
     (doc) should not be (null)
@@ -77,6 +83,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // mkDocumentFromSentences
   it should "make document from sentences, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from sentences, keep text")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.mkDocumentFromSentences(sents, true) // keep text
     (doc) should not be (null)
@@ -86,6 +93,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from sentences, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from sentences, discard text")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.mkDocumentFromSentences(sents, false) // discard text
     (doc) should not be (null)
@@ -95,6 +103,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from sentences, keep text, add extra spacing" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from sentences, keep text, add extra spacing")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.mkDocumentFromSentences(sents, true, 3)
     (doc) should not be (null)
@@ -105,6 +114,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // mkDocumentFromTokens
   it should "make document from tokens, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from tokens, keep text")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val text = toks.map(t => t.mkString(" ")).mkString(" ") // spacing: tok=1, sent=1
     val doc = client.mkDocumentFromTokens(toks, true)
@@ -115,6 +125,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from tokens, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from tokens, discard text")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val doc = client.mkDocumentFromTokens(toks, false)
     (doc) should not be (null)
@@ -124,6 +135,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "make document from tokens, keep text, add extra word spacing" in {
+    logger.debug(s"(TestProcessorCoreClient): make document from tokens, keep text, add extra word spacing")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val text = toks.map(t => t.mkString("  ")).mkString("   ") // spacing: tok=2, sent=3
     val doc = client.mkDocumentFromTokens(toks, true, 3, 2)
@@ -135,6 +147,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // preprocessText
   it should "preprocess text from zero-length text" in {
+    logger.debug(s"(TestProcessorCoreClient): preprocess text from zero-length text")
     val text = ""
     val reply = client.preprocessText(text)
     (reply) should not be (null)
@@ -142,6 +155,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "preprocess simple text" in {
+    logger.debug(s"(TestProcessorCoreClient): preprocess simple text")
     val text = "Testing is performed."
     val reply = client.preprocessText(text)
     (reply) should not be (null)
@@ -150,6 +164,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // preprocessSentences
   it should "preprocess sentences" in {
+    logger.debug(s"(TestProcessorCoreClient): preprocess sentences")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val reply = client.preprocessSentences(sents)
     (reply) should not be (null)
@@ -161,6 +176,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // preprocessTokens
   it should "preprocess tokens" in {
+    logger.debug(s"(TestProcessorCoreClient): preprocess tokens")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val reply = client.preprocessTokens(toks)
     (reply.size) should equal(2)
@@ -170,6 +186,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // tagPartsOfSpeech
   it should "tagPartsOfSpeech in a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): tagPartsOfSpeech in a small document")
     val text = "This is a document with a single sentence."
     val doc1 = client.mkDocument(text, true)
     val doc = client.tagPartsOfSpeech(doc1)
@@ -180,6 +197,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // lemmatize
   it should "lemmatize a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): lemmatize in a small document")
     val text = "Children like smaller documents with smaller sentences."
     val doc1 = client.mkDocument(text, true)
     val doc2 = client.tagPartsOfSpeech(doc1)
@@ -191,6 +209,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // recognizeNamedEntities
   it should "recognize named entities in a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): NER in a small document")
     val text = "On 6/8/2017, I sent a file containing some C# code to none@nowhere.com."
     val doc1 = client.mkDocument(text, true)
     val doc2 = client.tagPartsOfSpeech(doc1)
@@ -205,6 +224,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // parse
   it should "parse a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): parse a small document")
     val text =
 """This document has multiple sentences. Each should be processed by the processor.
    A Reach document should be returned."""
@@ -219,6 +239,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // chunking
   it should "chunk a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): chunk a small document")
     val text = "Each document can contain many sentences. This document has two sentences."
     val doc1 = client.mkDocument(text, true)
     val doc2 = client.tagPartsOfSpeech(doc1)
@@ -232,6 +253,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // resolveCoreference
   it should "resolve coreference in a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): resolve coreference in a small document")
     val text = "This is a document and it has one sentence and we like it."
     val doc1 = client.mkDocument(text, true)
     val doc2 = client.tagPartsOfSpeech(doc1)
@@ -246,6 +268,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // discourse
   it should "parse discourse in a small document" in {
+    logger.debug(s"(TestProcessorCoreClient): parse discourse in a small document")
     val text = "Despite what he said, this is a simple document containing small sentences."
     val doc1 = client.mkDocument(text, true)
     val doc2 = client.tagPartsOfSpeech(doc1)
@@ -259,6 +282,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // annotate(text)
   it should "annotate text, default keep" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate text, default keep")
     val text = "This is a document with a single sentence."
     val doc = client.annotate(text)
     (doc) should not be (null)
@@ -268,6 +292,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate text, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate text, keep text")
     val text = "This is single sentence test."
     val doc = client.annotate(text, true)   // explicit keep
     (doc) should not be (null)
@@ -277,6 +302,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate text, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate text, discard text")
     val text = "This is a document with a single sentence."
     val doc = client.annotate(text, false)  // explicit discard
     (doc) should not be (null)
@@ -287,6 +313,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // annotateFromSentences
   it should "annotate sentences, default keep" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate sentences, default keep")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.annotateFromSentences(sents)
     (doc) should not be (null)
@@ -296,6 +323,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate sentences, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate sentences, keep text")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.annotateFromSentences(sents, true) // explicit keep
     (doc) should not be (null)
@@ -305,6 +333,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate sentences, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate sentences, discard text")
     val sents = Seq("This is a test.", "It is only a test.", "In the event of a real document.")
     val doc = client.annotateFromSentences(sents, false) // explicit discard
     (doc) should not be (null)
@@ -315,6 +344,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // annotateFromTokens
   it should "annotate tokens, default keep" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate tokens, default keep")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val doc = client.annotateFromTokens(toks)
     (doc) should not be (null)
@@ -324,6 +354,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate tokens, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate tokens, keep text")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val text = toks.map(t => t.mkString(" ")).mkString(" ")  // spacing: tok=1, sent=1
     val doc = client.annotateFromTokens(toks, true)       // explicit keep
@@ -334,6 +365,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate tokens, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate tokens, discard text")
     val toks = Seq(Seq("This", "is", "a", "test."), Seq("It", "is", "only", "a", "test."))
     val doc = client.annotateFromTokens(toks, false) // explicit discard
     (doc) should not be (null)
@@ -344,6 +376,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
 
   // annotate(Document)
   it should "annotate zero-length Document, default keep" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate zero-length document, default keep")
     val docIn = client.mkDocument("")
     val doc = client.annotate(docIn)
     (doc) should not be (null)
@@ -352,6 +385,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate single sentence Document, no text to propagate" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate single sentence document, no text")
     val text = "This is a document with a single sentence."
     val docIn = client.mkDocument(text)     // doc w/o kept text
     val doc = client.annotate(docIn)
@@ -362,6 +396,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate single sentence Document, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate single sentence document, keep text")
     val text = "This is a document with a single sentence."
     val docIn = client.mkDocument(text, true) // doc with text kept
     val doc = client.annotate(docIn)
@@ -372,6 +407,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate single sentence Document, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate single sentence document, discard text")
     val text = "This is a document with a single sentence."
     val docIn = client.mkDocument(text, false) // doc w/o kept text
     val doc = client.annotate(docIn)
@@ -382,6 +418,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate multi-sentence Document, no text to propagate" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate multi-sentence document, no text")
     val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
     val docIn = client.mkDocument(text)    // doc w/o kept text
     val doc = client.annotate(docIn)
@@ -392,6 +429,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate multi-sentence Document, keep text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate multi-sentence document, keep text")
     val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
     val docIn = client.mkDocument(text, true) // doc with text kept
     val doc = client.annotate(docIn)
@@ -402,6 +440,7 @@ class TestProcessorCoreClient extends FlatSpec with Matchers with LazyLogging {
   }
 
   it should "annotate multi-sentence Document, discard text" in {
+    logger.debug(s"(TestProcessorCoreClient): annotate multi-sentence document, discard text")
     val text = "This document has multiple sentences. Each should be processed by the processor. A Reach document should be returned."
     val docIn = client.mkDocument(text)    // doc w/o kept text
     val doc = client.annotate(docIn)
