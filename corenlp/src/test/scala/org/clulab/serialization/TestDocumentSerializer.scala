060ac44d5dfe1f2365354d74d01a5b79defa9393
Tom Hicks <hickst@tohono.com>
2017-11-02 14:27:27 -0700

    Add optional capability to save/load the text field in document serializer.

--- corenlp/src/test/scala/org/clulab/serialization/TestDocumentSerializer.scala
+++ corenlp/src/test/scala/org/clulab/serialization/TestDocumentSerializer.scala
@@ -6,20 +6,38 @@ import org.clulab.processors.shallownlp.ShallowNLPProcessor
 import org.scalatest._
 
 class TestDocumentSerializer extends FlatSpec with Matchers {
-  "DocumentSerializer" should "save/load documents correctly" in {
-    val text = "John Doe went to China. There, he visited Beijing."
-    val proc:Processor = new CoreNLPProcessor(withDiscourse = ShallowNLPProcessor.WITH_DISCOURSE)
-    val doc1 = proc.annotate(text)
-    //println("Constructed a document with " + doc1.sentences.size + " sentences.")
+  val text = "John Doe went to China. There, he visited Beijing."
+  val proc: Processor = new CoreNLPProcessor(withDiscourse = ShallowNLPProcessor.WITH_DISCOURSE)
+  val ser = new DocumentSerializer
 
-    val ser = new DocumentSerializer
+  "DocumentSerializer" should "save/load documents correctly, with defaults" in {
+    val doc1 = proc.annotate(text)
+    //println("Created a document with " + doc1.sentences.size + " sentences.")
     val out1 = ser.save(doc1)
-    //println("Generated annotations:")
     //println(out1)
 
     val doc2 = ser.load(out1)
-    doc2.sentences.size should be (doc1.sentences.size)
+    (doc2.sentences.size) should be (doc1.sentences.size)
+    (doc2.text) should be (empty)
+
     val out2 = ser.save(doc2)
-    out1 should be (out2)
+    (out2) should be (out1)
   }
+
+  "DocumentSerializer" should "save/load documents correctly, keeping text" in {
+    val doc11 = proc.annotate(text, true)    // explicit keep text
+    //println("Created a document with " + doc11.sentences.size + " sentences.")
+    val out11 = ser.save(doc11, keepText=true)
+    // println(out11)                          // DEBUGGING
+
+    val doc22 = ser.load(out11)
+    (doc22.sentences.size) should be (doc11.sentences.size)
+    (doc22.text) should not be (empty)
+    (doc22.text.get) should be (text)
+
+    val out22 = ser.save(doc22, keepText=true)
+    // println(out22)                          // DEBUGGING
+    (out22) should be (out11)
+  }
+
 }
