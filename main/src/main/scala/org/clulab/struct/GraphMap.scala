bbcd5605271ac8c7f788e08574fb63e96026eb98
Rebecca Sharp <bsharp@cottonwood.cs.arizona.edu>
2017-11-28 17:54:40 -0700

    added deps back to GraphMap

--- main/src/main/scala/org/clulab/struct/GraphMap.scala
+++ main/src/main/scala/org/clulab/struct/GraphMap.scala
@@ -10,6 +10,8 @@ class GraphMap extends mutable.HashMap[String, DirectedGraph[String]] {
 object GraphMap {
   val UNIVERSAL_BASIC = "universal-basic" // basic Universal dependencies
   val UNIVERSAL_ENHANCED = "universal-enhanced" // collapsed (or enhanced) Universal dependencies
+  val STANFORD_BASIC = "stanford-basic" // basic Stanford dependencies
+  val STANFORD_COLLAPSED = "stanford-collapsed" // collapsed Stanford dependencies
   val SEMANTIC_ROLES = "semantic-roles" // semantic roles from CoNLL 2008-09, which includes PropBank and NomBank
 
   def apply(existing: Map[String, DirectedGraph[String]]): GraphMap = {
