159ef6779e31846b43dc822876923f5638ad3b82
Mihai Surdeanu <surdeanu@gmail.com>
2018-02-18 12:07:01 -0700

    Reorganized tests for enhnaced deps.

---
+++ main/src/test/scala/org/clulab/processors/TestUniversalEnhancedDependencies.scala
@@ -0,0 +1,10 @@
+package org.clulab.processors
+
+import org.clulab.processors.clu.CluProcessor
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestUniversalEnhancedDependencies extends FlatSpec with Matchers {
+  val proc = new CluProcessor()
+
+  // TODO
+}
