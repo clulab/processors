125dd397c15df447e1cab527af18ab10b2cd5c6c
George Barbosa <gcgbarbosa@dhcp-10-134-233-78.uawifi.arizona.edu>
2018-08-01 10:39:36 -0700

    added missing contractions from pt_br-ud-train.conllu

--- main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepPortugueseContractions.scala
+++ main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepPortugueseContractions.scala
@@ -37,7 +37,7 @@ class TokenizerStepPortugueseContractions extends TokenizerStep {
     val tokens = new ArrayBuffer[RawToken]()
 
     for(input <- inputs) {
-
+      //  TODO: change IFs to a MATCH (improve readability)
       if("""(?i)^do$""".r.findFirstIn(input.raw).isDefined) {
         tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "de"))
         tokens += RawToken(input.raw.substring(1), input.beginPosition+1, "o")
@@ -283,6 +283,44 @@ class TokenizerStepPortugueseContractions extends TokenizerStep {
       else if("""(?i)^pelas$""".r.findFirstIn(input.raw).isDefined) {
         tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "por"))
         tokens += RawToken(input.raw.substring(1), input.beginPosition, "as")
+      }
+      // TODO:
+      // doutros -> de outros
+      // doutras -> de outras
+      // doutra -> de outra
+      // doutro -> de outro
+      else if("""(?i)^doutr(os|as|o|a)$""".r.findFirstIn(input.raw).isDefined) {
+        tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "de"))
+        tokens += RawToken(input.raw.substring(1), input.beginPosition+1, input.raw.substring(1))
+      }
+      // noutras -> em outras
+      // noutros -> em outros
+      // noutra -> em outra
+      // noutro -> em outro
+      else if("""(?i)^noutr(os|as|o|a)$""".r.findFirstIn(input.raw).isDefined) {
+        tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "em"))
+        tokens += RawToken(input.raw.substring(1), input.beginPosition+1, input.raw.substring(1))
+      }      
+      // dalguns -> de alguns
+      // dalgumas -> de algumas
+      // dalguma -> de alguma
+      // dalgum -> de algum
+      // dalguém -> de alguém
+      // dali -> de ali
+      else if("""(?i)^dal(guns|gumas|guma|gum|guém|i)$""".r.findFirstIn(input.raw).isDefined) {
+        tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "de"))
+        tokens += RawToken(input.raw.substring(1), input.beginPosition+1, input.raw.substring(1))
+      }
+      // nalguns - em - alguns
+      // nalgumas - em algumas
+      else if("""(?i)^nal(guns|gumas)$""".r.findFirstIn(input.raw).isDefined) {
+        tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "em"))
+        tokens += RawToken(input.raw.substring(1), input.beginPosition+1, input.raw.substring(1))
+      }
+      // donde - de onde
+      else if("""(?i)^donde$""".r.findFirstIn(input.raw).isDefined) {
+        tokens += RawToken(input.raw.substring(0, 1), input.beginPosition, matchCase(input.raw, "de"))
+        tokens += RawToken(input.raw.substring(1), input.beginPosition+1, "onde")
       }
       // any other token
       else {
