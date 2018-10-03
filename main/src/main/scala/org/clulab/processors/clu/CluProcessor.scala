d53626f52d76c086a3d050fbc67be56147c9d2b6
George C. G. Barbosa <gcgbarbosa@gmail.com>
2018-10-02 21:03:39 -0700

    lemmatize after chunk, so we use cheap lemmas for chunking too

--- main/src/main/scala/org/clulab/processors/clu/CluProcessor.scala
+++ main/src/main/scala/org/clulab/processors/clu/CluProcessor.scala
@@ -273,10 +273,10 @@ class PortugueseCluProcessor extends CluProcessor(config = ConfigFactory.load("c
   override def annotate(doc:Document): Document = {
     cheapLemmatize(doc)
     tagPartsOfSpeech(doc)
-    lemmatize(doc)
     recognizeNamedEntities(doc)
     parse(doc)
     chunking(doc)
+    lemmatize(doc)
     resolveCoreference(doc)
     discourse(doc)
     doc.clear()
