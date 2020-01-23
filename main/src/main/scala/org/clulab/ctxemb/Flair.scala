68948e26c38ffa4b202b4927ec5d11ede61634ed
Mihai Surdeanu <surdeanu@gmail.com>
2020-01-23 10:51:47 -0700

    d

--- main/src/main/scala/org/clulab/ctxemb/Flair.scala
+++ main/src/main/scala/org/clulab/ctxemb/Flair.scala
@@ -206,11 +206,12 @@ class Flair {
       println(s"i = $i; c = ${characters(i)}; gold = $gold; gold as int = ${gold.toInt}; gold id = $goldTid")
       val prob = pick(softmax(emissionScoresForSeq(i)), goldTid)
       println(s"prob of gold = ${prob.value().toFloat()}")
-      val v = math.pow(1.0 / prob.value().toFloat(), characters.length.toDouble)
+      val v = 1.0 / prob.value().toFloat() // math.pow(1.0 / prob.value().toFloat(), characters.length.toDouble)
       println(s"v = $v")
       pp *= v
       println(s"pp = $pp")
     }
+    pp = math.pow(pp, characters.length.toDouble)
     pp
   }
 
