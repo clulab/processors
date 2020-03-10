8f28eb994423f3c6ff8aefe9e644cdb9e7b460e7
Mihai Surdeanu <surdeanu@gmail.com>
2020-03-09 19:55:42 -0700

    forward LSTM LM

--- main/src/main/scala/org/clulab/lm/LstmLMTrainer.scala
+++ main/src/main/scala/org/clulab/lm/LstmLMTrainer.scala
@@ -1,8 +1,8 @@
 package org.clulab.lm
 
 import com.typesafe.config.ConfigFactory
-import edu.cmu.dynet.{ComputationGraph, Dim, ExpressionVector, LookupParameter, LstmBuilder, Parameter, ParameterCollection, RMSPropTrainer, RnnBuilder, Trainer}
-import org.clulab.sequences.LstmUtils
+import edu.cmu.dynet.{ComputationGraph, Dim, Expression, ExpressionVector, LookupParameter, LstmBuilder, Parameter, ParameterCollection, RMSPropTrainer, RnnBuilder}
+import org.clulab.sequences.{LstmUtils, SafeTrainer}
 import org.clulab.sequences.LstmUtils.initializeDyNet
 import org.clulab.struct.Counter
 import org.slf4j.{Logger, LoggerFactory}
@@ -10,6 +10,9 @@ import org.slf4j.{Logger, LoggerFactory}
 import scala.collection.mutable
 import scala.io.Source
 import LstmLMTrainer._
+import edu.cmu.dynet.Expression.{pick, pickNegLogSoftmax, softmax, sum}
+
+import scala.collection.mutable.ArrayBuffer
 
 class LstmLMTrainer (val w2i: Map[String, Int],
                      val t2i: Map[String, Int],
@@ -20,11 +23,10 @@ class LstmLMTrainer (val w2i: Map[String, Int],
                      val fwO: Parameter,
                      val bwO: Parameter) {
 
-  def mkTrainer(): Trainer = {
-    val trainer = new RMSPropTrainer(parameters)
-    trainer.clippingEnabled_=(true)
-    trainer.clipThreshold_=(CLIP_THRESHOLD)
-    trainer
+  val eosTagId:Int = t2i(LstmUtils.EOS_WORD)
+
+  def mkTrainer(): SafeTrainer = {
+    SafeTrainer(new RMSPropTrainer(parameters))
   }
 
   /**
@@ -51,8 +53,156 @@ class LstmLMTrainer (val w2i: Map[String, Int],
     var batchLosses = new ExpressionVector()
 
     for(sentence <- source.getLines()) {
+      val (words, tags) = sentenceToWords(sentence)
 
+      //
+      // left-to-right prediction
+      //
+      val fwIn = words
+      val fwTags = tags
+      val fwEmissionScores = emissionScoresAsExpressions(fwIn, fwTags, wordFwRnnBuilder, fwO, doDropout = true)
+      val fwLoss = languageModelLoss(fwEmissionScores, fwTags)
+      batchLosses.add(fwLoss)
+
+      //
+      // book keeping
+      //
+      sentCount += 1
+      numTagged += words.length + 1
+
+      //
+      // backprop
+      // we do this only when the batch is full
+      //
+      if(batchLosses.size >= BATCH_SIZE) {
+        val comboLoss = sum(batchLosses) / batchLosses.size
+        cummulativeLoss += comboLoss.value().toFloat()
+        ComputationGraph.backward(comboLoss)
+        trainer.update(parameters)
+
+        // report perplexity if a dev file is available
+        if(sentCount % saveCheckpoint == 0 && devFileName.nonEmpty){
+          reportPerplexity(devFileName.get)
+        }
+
+        // reset for the next batch
+        ComputationGraph.renew()
+        batchLosses = new ArrayBuffer[Expression]()
+        //println("Renewed graph!")
+      }
+
+      //
+      // reporting and model saving
+      //
+      if(sentCount % logCheckpoint == 0) {
+        logger.debug(s"Processed $sentCount sentences. Cummulative loss: ${cummulativeLoss / numTagged}.")
+
+        // save a model every 50K sentences
+        if(sentCount % saveCheckpoint == 0){
+          //val baseModelName = s"lstmlm_s$sentCount"
+          // save(baseModelName)
+        }
+      }
     }
+  }
+
+  def emissionScoresAsExpressions(words: Array[Int],
+                                  tags: Array[Int],
+                                  rnnBuilder: RnnBuilder,
+                                  pO:Parameter,
+                                  doDropout:Boolean = false): ExpressionVector = {
+    val embeddings = words.map(mkEmbedding)
+
+    if(doDropout) {
+      rnnBuilder.setDropout(DROPOUT_PROB)
+    } else {
+      rnnBuilder.disableDropout()
+    }
+
+    val states = LstmUtils.transduce(embeddings, rnnBuilder)
+
+    val O = Expression.parameter(pO)
+    val emissionScores = new ExpressionVector()
+    for(s <- states) {
+      emissionScores.add(O * s)
+    }
+
+    emissionScores
+  }
+
+  def mkEmbedding(wi: Int): Expression = Expression.lookup(wordLookupParameters, wi)
+
+  /** Greedy loss function, ignoring transition scores */
+  def languageModelLoss(emissionScoresForSeq:ExpressionVector,
+                        tags:Array[Int]): Expression = {
+
+    val goldLosses = new ExpressionVector()
+
+    for(i <- emissionScoresForSeq.indices) {
+      val goldTid = getGoldTagId(tags, i)
+
+      // emissionScoresForSeq(i) = all tag emission scores for the word at position i
+      goldLosses.add(pickNegLogSoftmax(emissionScoresForSeq(i), goldTid))
+    }
+
+    sum(goldLosses)
+  }
+
+  def getGoldTagId(tags: Array[Int], i:Int): Int = {
+    if (i < tags.length - 1) tags(i + 1)
+    else eosTagId
+  }
+
+  def reportPerplexity(devFileName: String): Unit = {
+    val source = Source.fromFile(devFileName)
+    var sentCount = 0
+    var cummulativeFwPerplexity = 0.0
+    logger.debug("Computing perplexity in dev...")
+    for(sentence <- source.getLines()) {
+      val (words, tags) = sentenceToWords(sentence)
+      ComputationGraph.renew()
+
+      val fwIn = words
+      val fwTags = tags
+      val fwEmissionScores = emissionScoresAsExpressions(fwIn, fwTags, wordFwRnnBuilder, fwO) // no dropout during testing!
+      val fwPp = perplexity(fwEmissionScores, fwTags)
+
+      cummulativeFwPerplexity += fwPp
+      sentCount += 1
+    }
+    source.close()
+    logger.info(s"Average forward perplexity: ${cummulativeFwPerplexity / sentCount.toDouble}")
+  }
+
+  /** Computes perplexity for this sentence */
+  def perplexity(emissionScoresForSeq: ExpressionVector, tags: Array[Int]): Double = {
+    var pp = 1.0
+    for(i <- emissionScoresForSeq.indices) {
+      val goldTid = getGoldTagId(tags, i)
+      val prob = pick(softmax(emissionScoresForSeq(i)), goldTid)
+      pp *= math.pow(1.0 / prob.value().toFloat(), 1.0 / tags.length.toDouble)
+    }
+    pp
+  }
+
+  def sentenceToWords(sentence: String): (Array[Int], Array[Int]) = {
+    val tokens = sentence.split("\\s+") // the input sentence come pre-tokenized
+    val words = new ArrayBuffer[Int]()
+    val tags = new ArrayBuffer[Int]()
+
+    for(token <- tokens) {
+      words += wordId(token)
+      tags += tagId(token)
+    }
+
+    (words.toArray, tags.toArray)
+  }
+
+  def wordId(word: String): Int = {
+    w2i.getOrElse(word, w2i(LstmUtils.UNK_WORD))
+  }
+  def tagId(word: String): Int = {
+    t2i.getOrElse(word, t2i(LstmUtils.UNK_WORD))
   }
 }
 
@@ -67,9 +217,10 @@ object LstmLMTrainer {
 
   val CLIP_THRESHOLD = 5.0f
   val DROPOUT_PROB:Float = 0.2f
+  val BATCH_SIZE = 1
 
   protected def generateKnownWords(trainFileName: String): (Set[String], Set[String], Int) = {
-    logger.debug(s"Counting characters in file $trainFileName...")
+    logger.debug(s"Counting words in file $trainFileName...")
     val counts = new Counter[String]()
     val source = Source.fromFile(trainFileName)
     var sentCount = 0
@@ -99,10 +250,12 @@ object LstmLMTrainer {
       }
     }
     logger.debug(s"Found ${knownWords.size} not unknown words.")
+    logger.debug(s"Found ${tags.size} + 2 tags.")
 
-    // add the virtual characters we need
+    // add the virtual words/tags we need
     knownWords += LstmUtils.UNK_WORD
-    knownWords += LstmUtils.EOS_WORD
+    tags += LstmUtils.UNK_WORD
+    tags += LstmUtils.EOS_WORD
 
     (knownWords.toSet, tags.toSet, sentCount)
   }
@@ -114,7 +267,7 @@ object LstmLMTrainer {
     val wordLookupParameters = parameters.addLookupParameters(w2i.size, Dim(WORD_EMBEDDING_SIZE))
 
     val wordFwBuilder = new LstmBuilder(1, WORD_EMBEDDING_SIZE, WORD_RNN_STATE_SIZE, parameters)
-    val wordBwBuilder = new LstmBuilder()(1, WORD_EMBEDDING_SIZE, WORD_RNN_STATE_SIZE, parameters)
+    val wordBwBuilder = new LstmBuilder(1, WORD_EMBEDDING_SIZE, WORD_RNN_STATE_SIZE, parameters)
 
     val fwO = parameters.addParameters(Dim(t2i.size, WORD_RNN_STATE_SIZE))
     val bwO = parameters.addParameters(Dim(t2i.size, WORD_RNN_STATE_SIZE))
@@ -151,9 +304,9 @@ object LstmLMTrainer {
       val lm = mkParams(w2i, t2i)
       lm.train(
         trainFileName,
-        Some(config.getArgString("flair.train.dev", None)),
-        config.getArgInt("flair.train.logCheckpoint", Some(1000)),
-        config.getArgInt("flair.train.saveCheckpoint", Some(50000))
+        Some(config.getArgString("lstm.train.dev", None)),
+        config.getArgInt("lstm.train.logCheckpoint", Some(1000)),
+        config.getArgInt("lstm.train.saveCheckpoint", Some(50000))
       )
     }
   }
