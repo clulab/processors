5633306000d2952ea839cb477d8074e2e03dec50
Mihai Surdeanu <surdeanu@gmail.com>
2020-04-27 20:28:50 -0700

    hidden state = 128, Adam opt

--- main/src/main/scala/org/clulab/sequences/LstmCrfMtl.scala
+++ main/src/main/scala/org/clulab/sequences/LstmCrfMtl.scala
@@ -93,7 +93,7 @@ class LstmCrfMtl(val taskManagerOpt: Option[TaskManager], lstmCrfMtlParametersOp
   def train(modelNamePrefix:String):Unit = {
     require(taskManagerOpt.isDefined)
 
-    val trainer = SafeTrainer(new RMSPropTrainer(model.parameters)) // RMSPropTrainer(model.parameters))
+    val trainer = SafeTrainer(new AdamTrainer(model.parameters)) // RMSPropTrainer(model.parameters))
 
     var cummulativeLoss = 0.0
     var numTagged = 0
