53f0b750fdd2363aeea33c7bed891bf12e907648
Mihai Surdeanu <surdeanu@gmail.com>
2020-05-05 15:07:24 -0700

    comments

--- main/src/main/scala/org/clulab/lm/LM.scala
+++ main/src/main/scala/org/clulab/lm/LM.scala
@@ -6,9 +6,13 @@ import edu.cmu.dynet.Expression
 
 /** Trait for language model (LM) functionality */
 trait LM {
+  // produce an encoding of a text, with optional POS tags, and position embeddings relative to a predicate
   def mkEmbeddings(words: Iterable[String], posTags: Option[Iterable[String]], predPosition: Option[Int], doDropout:Boolean): (Iterable[Expression], Iterable[Expression])
 
+  // the dimension of the word state
   def dimensions: Int
+
+  // the dimension of the word embedding
   def wordDimensions: Int
 
   def saveX2i(printWriter: PrintWriter)
