78ebac7575cd889f32067cacbba9960963c88a44
Mihai Surdeanu <surdeanu@gmail.com>
2020-05-10 22:14:04 -0700

    Added ConstEmbeddings

---
+++ main/src/test/scala/org/clulab/dynet/TestConstEmbeddingsGlove.scala
@@ -0,0 +1,15 @@
+package org.clulab.dynet
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestConstEmbeddingsGlove extends FlatSpec with Matchers {
+  lazy val embeddings = ConstEmbeddingsGlove("/test_vectors.txt", true)
+
+  DyNetUtils.initializeDyNet()
+
+  "ConstEmbeddingsGlove" should "not be empty" in {
+    val e = embeddings.get("time")
+    e != null should be(true)
+    e.dim().get(0) should be (5)
+  }
+}
