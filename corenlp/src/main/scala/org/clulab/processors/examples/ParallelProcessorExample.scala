23cb0c8bf80a2618998eceec1597204cc2fffe47
Keith Alcock <github@keithalcock.com>
2020-06-22 14:23:16 -0700

    Test output of parallel processing
    Not crashing is insufficient now

--- corenlp/src/main/scala/org/clulab/processors/examples/ParallelProcessorExample.scala
+++ corenlp/src/main/scala/org/clulab/processors/examples/ParallelProcessorExample.scala
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.FilenameFilter
 import java.io.PrintWriter
+import java.io.StringWriter
 import java.nio.charset.StandardCharsets
 
 import org.clulab.dynet.Utils
@@ -21,55 +22,55 @@ import scala.io.Source
 
 object ParallelProcessorExample {
 
-  def main(args: Array[String]): Unit = {
-    val utf8: String = StandardCharsets.UTF_8.toString
+  class Timer(val description: String) {
+    var elapsedTime: Option[Long] = None
+    var startTime: Option[Long] = None
 
-    class Timer(val description: String) {
-      var elapsedTime: Option[Long] = None
-      var startTime: Option[Long] = None
+    def time[R](block: => R): R = {
+      val t0 = System.currentTimeMillis()
+      val result: R = block // call-by-name
+      val t1 = System.currentTimeMillis()
 
-      def time[R](block: => R): R = {
-        val t0 = System.currentTimeMillis()
-        val result: R = block // call-by-name
+      elapsedTime = Some(t1 - t0)
+      result
+    }
+
+    def start(): Unit = {
+      val t0 = System.currentTimeMillis()
+
+      startTime = Some(t0)
+    }
+
+    def stop(): Unit = {
+      if (startTime.isDefined) {
         val t1 = System.currentTimeMillis()
 
-        elapsedTime = Some(t1 - t0)
-        result
-      }
-
-      def start(): Unit = {
-        val t0 = System.currentTimeMillis()
-
-        startTime = Some(t0)
-      }
-
-      def stop(): Unit = {
-        if (startTime.isDefined) {
-          val t1 = System.currentTimeMillis()
-
-          elapsedTime = Some(t1 - startTime.get)
-        }
-      }
-
-      override def toString: String = {
-        if (elapsedTime.isDefined)
-          s"\tTime\t$description\t${diffToString(elapsedTime.get)}"
-        else if (startTime.isDefined)
-          s"\tStart\t$description\t${startTime.get}\tms"
-        else
-          s"\tTimer\t$description"
-      }
-
-      def diffToString(diff: Long): String = {
-        val days = (diff / (1000 * 60 * 60 * 24)) / 1
-        val hours = (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        val mins = (diff % (1000 * 60 * 60)) / (1000 * 60)
-        val secs = (diff % (1000 * 60)) / 1000
-        val msecs = (diff % (1000 * 1)) / 1
-
-        f"$days:$hours%02d:$mins%02d:$secs%02d.$msecs%03d"
+        elapsedTime = Some(t1 - startTime.get)
       }
     }
+
+    override def toString: String = {
+      if (elapsedTime.isDefined)
+        s"\tTime\t$description\t${diffToString(elapsedTime.get)}"
+      else if (startTime.isDefined)
+        s"\tStart\t$description\t${startTime.get}\tms"
+      else
+        s"\tTimer\t$description"
+    }
+
+    def diffToString(diff: Long): String = {
+      val days = (diff / (1000 * 60 * 60 * 24)) / 1
+      val hours = (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      val mins = (diff % (1000 * 60 * 60)) / (1000 * 60)
+      val secs = (diff % (1000 * 60)) / 1000
+      val msecs = (diff % (1000 * 1)) / 1
+
+      f"$days:$hours%02d:$mins%02d:$secs%02d.$msecs%03d"
+    }
+  }
+
+  def mainWithCallback(args: Array[String])(callback: (File, String) => Unit): Unit = {
+    val utf8: String = StandardCharsets.UTF_8.toString
 
     def findFiles(collectionDir: String, extension: String): Seq[File] = {
       val dir = new File(collectionDir)
@@ -140,19 +141,36 @@ object ParallelProcessorExample {
         text
       }
 
-      val document = processor.annotate(text)
-      val savedDocument = documentSerializer.save(document)
-
       val outputFile = new File(outputDir + "/" + file.getName)
-      val printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(outputFile)))
+      val document = processor.annotate(text)
+      val printedDocument = {
+        val stringWriter = new StringWriter
+        val printWriter = new PrintWriter(stringWriter)
 
-      // This is done so that output of serial and parallel versions can be compared.
-      printDocument(document, printWriter)
-      printWriter.println(savedDocument)
-      printWriter.close
+        printDocument(document, printWriter)
+        printWriter.close()
+
+        val result = stringWriter.toString
+        result
+      }
+      val savedDocument = documentSerializer.save(document)
+      val outputDocument = printedDocument + savedDocument
+
+      callback(outputFile, outputDocument)
     }
 
     timer.stop()
     println(timer.toString)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    mainWithCallback(args) { case (file: File, contents: String) =>
+      // Print these to a file for analysis.
+      val printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))
+
+      printWriter.println(contents)
+      printWriter.close
+    }
   }
 }
--- corenlp/src/test/scala/org/clulab/processors/TestParallel.scala
+++ corenlp/src/test/scala/org/clulab/processors/TestParallel.scala
@@ -1,18 +1,43 @@
 package org.clulab.processors
 
+import java.io.File
+
 import org.clulab.processors.examples.ParallelProcessorExample
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.slf4j.{Logger, LoggerFactory}
 
-import TestParallel._
+import scala.collection.mutable
 
 class TestParallel extends FlatSpec with Matchers {
+  import TestParallel._
 
-  "Processing documents in parallel" should "not cause crash" in {
-    logger.info("Starting processing...")
-    ParallelProcessorExample.main(Array("./corenlp/src/test/resources/documents", ".", "txt", "2"))
-    logger.info("Processing complete.")
+  def save(map: mutable.HashMap[String, String], file: File, value: String): Unit =
+    map(file.getName) = value
+
+  behavior of "Processing documents in parallel"
+
+  it should "match processing documents serially" in {
+    val inputDir = "./corenlp/src/test/resources/documents"
+    val outputDir = "."
+    val extension = "txt"
+
+    val parResults = mutable.HashMap.empty[String, String]
+    val serResults = mutable.HashMap.empty[String, String]
+
+    logger.info("Starting processing in parallel...")
+    ParallelProcessorExample.mainWithCallback(Array(inputDir, outputDir, extension, "2")) { case (file, contents) =>
+      save(parResults, file, contents)
+    }
+    logger.info("Parallel processing complete.")
+
+    logger.info("Starting processing serially...")
+    ParallelProcessorExample.mainWithCallback(Array(inputDir, outputDir, extension, "1")) { case (file, contents) =>
+      save(serResults, file, contents)
+    }
+    logger.info("Serial processing complete.")
+
+    parResults should contain theSameElementsAs serResults
   }
 }
 
