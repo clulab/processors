421fd075ef24511bba84fe0f72e81098d9c1c5ba
Mihai Surdeanu <surdeanu@gmail.com>
2020-06-24 13:17:33 -0700

    cnn example

--- main/src/main/scala/org/clulab/dynet/CnnExample.scala
+++ main/src/main/scala/org/clulab/dynet/CnnExample.scala
@@ -15,20 +15,20 @@ object CnnExample extends App {
   val embeddings = pc.addLookupParameters(5, Dim(1, 1, embSize)) // embbedings of size 3 for 5 words
 
   val winSize = 2 // size of the CNN window
-  val filterSize = 10 // size of the CNN filter
+  val filterSize = 4 // size of the CNN filter
   val cnnParams = pc.addParameters(Dim(1, winSize, embSize, filterSize))
   val cnnBias = pc.addParameters(Dim(filterSize))
 
   val w1 = Expression.lookup(embeddings, 1)
   val w2 = Expression.lookup(embeddings, 2)
   val w3 = Expression.lookup(embeddings, 3)
-  val cnnIn = Expression.concatenateCols(w1, w2, w3)
+  val w4 = Expression.lookup(embeddings, 4)
+  val cnnIn = Expression.concatenateCols(w1, w2, w3, w4)
   println(cnnIn.dim())
   println(cnnIn.value().toVector().mkString(" "))
 
   val stride = new UnsignedVector(Seq(1L, 1L))
 
-  // TODO: this complains due to incorrect dimensions
   val cnnOut = Expression.conv2d(cnnIn, Expression.parameter(cnnParams), Expression.parameter(cnnBias), stride)
 
   println(cnnOut.dim())
