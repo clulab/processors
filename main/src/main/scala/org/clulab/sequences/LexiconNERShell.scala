fc3a01751deb0220e4adc2fbd7fa5c8d25320a7a
Mihai Surdeanu <surdeanu@gmail.com>
2020-09-04 20:55:38 -0700

    Added lexicon NER shell

---
+++ main/src/main/scala/org/clulab/sequences/LexiconNERShell.scala
@@ -0,0 +1,29 @@
+package org.clulab.sequences
+
+import org.clulab.dynet.Utils
+import org.clulab.processors.clu.CluProcessor
+import org.clulab.utils.Shell
+
+class LexiconNERShell(val lexiconNer: LexiconNER) extends Shell {
+  var proc: CluProcessor = _
+
+  override def initialize(): Unit = {
+    proc = new CluProcessor()
+  }
+
+  override def work(text: String): Unit = {
+    val doc = proc.mkDocument(text)
+    for(sent <- doc.sentences) {
+      val labels = lexiconNer.find(sent)
+      println(labels.mkString(", "))
+    }
+  }
+}
+
+object LexiconNERShell extends App {
+  Utils.initializeDyNet()
+  val kb = args(0) // pass the KB file name as the first argument
+  val lexiconNer = LexiconNER(Seq(kb))
+  val shell = new LexiconNERShell(lexiconNer)
+  shell.shell()
+}
