81b714ebbac3433923a7cfa6d352453f42cfc841
Mihai Surdeanu <surdeanu@gmail.com>
2020-10-02 17:54:42 -0700

    New test for graph sizes

---
+++ main/src/test/scala/org/clulab/processors/TestDepGraphSizes.scala
@@ -0,0 +1,50 @@
+package org.clulab.processors
+
+import org.clulab.TestUtils
+import org.clulab.dynet.Utils
+import org.clulab.processors.clu.CluProcessor
+import org.clulab.struct.DirectedGraph
+import org.scalatest.{FlatSpec, Matchers}
+
+/** Makes sure that CluProcessor produces dependency graphs of correct sizes */
+class TestDepGraphSizes extends FlatSpec with Matchers {
+  lazy val proc = {
+    Utils.initializeDyNet()
+    new CluProcessor()
+  }
+
+  "CluProcessor" should "produce dependency graphs that have the same size as the sentence" in {
+    val text = TestUtils.readFile("3_ALPAN_Paper_10.txt")
+    text.length > 0 should be(true)
+
+    val doc = proc.annotate(text)
+    for(sent <- doc.sentences) {
+      val size = sent.size
+
+      val d1 = sent.universalBasicDependencies.get
+      checkGraph(sent, d1, "universalBasicDependencies")
+      d1.size should be (size)
+
+      val d2 = sent.universalEnhancedDependencies.get
+      checkGraph(sent, d2, "universalEnhancedDependencies")
+      d2.size should be (size)
+
+      val d3 = sent.semanticRoles.get
+      checkGraph(sent, d3, "semanticRoles")
+      d3.size should be (size)
+
+      val d4 = sent.enhancedSemanticRoles.get
+      checkGraph(sent, d4, "enhancedSemanticRoles")
+      d4.size should be (size)
+    }
+  }
+
+  def checkGraph(sent: Sentence, graph: DirectedGraph[String], name: String): Unit = {
+    if(graph.size != sent.size) {
+      println(s"The graph $name for this sentence is irregular: ${sent.size} tokens vs. ${graph.size} graph size!")
+      println("Tokens: " + sent.words.mkString(", "))
+      println(s"Graph $name:")
+      println(graph)
+    }
+  }
+}
