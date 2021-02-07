5988fd934fcf866356f028199e5e94a38390a8cb
Mihai Surdeanu <surdeanu@gmail.com>
2021-02-06 21:43:51 -0700

    Added NumericEntityRecognizer.

---
+++ corenlp/src/main/scala/org/clulab/processors/clucore/CluCoreProcessor.scala
@@ -0,0 +1,10 @@
+package org.clulab.processors.clucore
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.clulab.processors.clu.CluProcessor
+
+class CluCoreProcessor(config: Config = ConfigFactory.load("cluprocessor"))
+  extends CluProcessor(config) {
+
+  // TODO: ner merge, SRL ner labels, doc date, shell
+}
