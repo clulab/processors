55cd084892f22842dd8bf65e8e2da130dc1304d3
Keith Alcock <github@keithalcock.com>
2021-03-14 00:35:54 -0700

    Change constructor of ConstEmbeddingsGlove

--- main/src/main/scala/org/clulab/dynet/ConstEmbeddingsGlove.scala
+++ main/src/main/scala/org/clulab/dynet/ConstEmbeddingsGlove.scala
@@ -1,5 +1,6 @@
 package org.clulab.dynet
 import edu.cmu.dynet.{Dim, Expression, FloatVector}
+import org.clulab.embeddings.WordEmbeddingMap
 import org.clulab.embeddings.WordEmbeddingMapPool
 import org.slf4j.{Logger, LoggerFactory}
 import org.clulab.utils.ConfigWithDefaults
@@ -9,35 +10,40 @@ import org.clulab.utils.StringUtils
  * Implements the ConstEmbeddings as a thin wrapper around WordEmbeddingMap
  *   with additional functionality to produce embeddings as DyNet Expressions
  */
-class ConstEmbeddingsGlove(matrixResourceName: String) extends ConstEmbeddings {
+class ConstEmbeddingsGlove(wordEmbeddingMap: WordEmbeddingMap) extends ConstEmbeddings {
+  val dynetDim: Dim = Dim(wordEmbeddingMap.dim)
 
-  val wordVectors = {
-    // This is really meant to be a resource location, but we'll take a file if it's there.
-    val name = StringUtils.afterLast(matrixResourceName, '/', all = true, keep = false)
-    val location = StringUtils.beforeLast(matrixResourceName, '/', all = false, keep = true)
+  override def dim: Int = wordEmbeddingMap.dim
 
-    WordEmbeddingMapPool.getOrElseCreate(name, compact = true, location, location)
+  override def mkEmbedding(word: String): Expression = {
+    val embeddingSeq: IndexedSeq[Float] = wordEmbeddingMap.getOrElseUnknown(word)
+    val floatVector = new FloatVector(embeddingSeq.length)
+    // Populate this manually to avoid floatToFloat and asJavaCollection
+    var i = 0
+    while (i < embeddingSeq.length) {
+      floatVector.update(i, embeddingSeq(i))
+      i += 1
+    }
+    Expression.input(dynetDim, floatVector)
   }
-  // CompactWordEmbeddingMap(matrixResourceName, resource = isResource, cached = false)
-
-  override def dim: Int = wordVectors.dim
-
-  override def mkEmbedding(word:String): Expression = {
-    val vector = wordVectors.getOrElseUnknown(word)
-    Expression.input(Dim(dim), new FloatVector(vector))
-  }
-
 }
 
 object ConstEmbeddingsGlove {
   val logger:Logger = LoggerFactory.getLogger(classOf[ConstEmbeddingsGlove])
 
-  private def UNK:String = "" // empty string for UNK
-
-  def apply(configName:String = "org/clulab/glove.conf"): ConstEmbeddingsGlove = {
+  def apply(configName: String = "org/clulab/glove.conf"): ConstEmbeddingsGlove = {
     val config = ConfigWithDefaults(configName)
     val matrixResourceName = config.getArgString("glove.matrixResourceName", None)
-//    val isResource = config.getArgBoolean("glove.isResource", Some(true))
-    new ConstEmbeddingsGlove(matrixResourceName)
+    val wordEmbeddingMap = {
+      // This is really meant to be a resource location, but we'll take a file if it's there.
+      // val isResource = config.getArgBoolean("glove.isResource", Some(true))
+      val name = StringUtils.afterLast(matrixResourceName, '/', all = true, keep = false)
+      val location = StringUtils.beforeLast(matrixResourceName, '/', all = false, keep = true)
+
+      WordEmbeddingMapPool.getOrElseCreate(name, compact = true, location, location)
+      // CompactWordEmbeddingMap(matrixResourceName, resource = isResource, cached = false)
+    }
+
+    new ConstEmbeddingsGlove(wordEmbeddingMap)
   }
 }
