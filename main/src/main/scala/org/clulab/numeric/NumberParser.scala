b129ab607d9b186c3ab4d4c5c7df8fffe26f67c4
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2021-08-04 10:01:42 -0700

    cross-compile

--- main/src/main/scala/org/clulab/numeric/NumberParser.scala
+++ main/src/main/scala/org/clulab/numeric/NumberParser.scala
@@ -149,7 +149,7 @@ object NumberParser {
     "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine",
     "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine",
     "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine",
-    "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine",
+    "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine"
   )
 
 }
