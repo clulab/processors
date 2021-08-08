3c48a0c507b5ad84544189856e1074111ee067a0
mithunpaul08 <mithunpaul08@gmail.com>
2021-08-08 16:47:20 -0700

    adding metric tonnes spelt out

--- main/src/test/scala/org/clulab/numeric/TestNumericEntityRecognition.scala
+++ main/src/test/scala/org/clulab/numeric/TestNumericEntityRecognition.scala
@@ -135,6 +135,7 @@ class TestNumericEntityRecognition extends FlatSpec with Matchers {
     ensure("It was 12 ha", Interval(2, 4), "MEASUREMENT", "12.0 ha")
     //TODO : below all were new measurement formats found in habitus docs. added on august 8th 2021. Need mihai approval
     ensure(sentence= "yield potentials of 12 mt.", Interval(3, 5), goldEntity="MEASUREMENT", goldNorm= "12.0 mt")
+    ensure(sentence= "yield potentials of 12 metric tonnes.", Interval(3, 6), goldEntity="MEASUREMENT", goldNorm= "12.0 mt")
 
     // tests for unit normalization
     ensure("It was 12 hectares", Interval(2, 4), "MEASUREMENT", "12.0 ha")
