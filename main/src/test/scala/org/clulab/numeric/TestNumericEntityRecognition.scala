bcd33bb8b86b1595af40a674a55c70a17b97fffc
Hubert Kanyamahanga <hubakanye20@gmail.com>
2021-08-15 00:23:36 +0000

    Replaced en dashes to regular dashes on tests

--- main/src/test/scala/org/clulab/numeric/TestNumericEntityRecognition.scala
+++ main/src/test/scala/org/clulab/numeric/TestNumericEntityRecognition.scala
@@ -151,25 +151,25 @@ class TestNumericEntityRecognition extends FlatSpec with Matchers {
   // tests for recognizing fertilizer, seeds and yield measurement units
   it should "recognize literal measurement units" in {
     // these tests should pass 
-    ensure(sentence= "Imports of rice in the decade 2008–2017 amounted on average to 1500000 tonnes", Interval(13, 15), goldEntity="MEASUREMENT", goldNorm="1500000.0 t")
+    ensure(sentence= "Imports of rice in the decade 2008-2017 amounted on average to 1500000 tonnes", Interval(13, 15), goldEntity="MEASUREMENT", goldNorm="1500000.0 t")
     ensure(sentence= "They had yield potentials of 10 metric tons per hectare", Interval(5, 10), goldEntity="MEASUREMENT", goldNorm="10.0 t/ha")
     ensure(sentence= "Such observations were replaced with a cap value of 700 kilograms per hectare", Interval(9, 13), goldEntity="MEASUREMENT", goldNorm="700.0 kg/ha")
     ensure(sentence= "The production from the SRV was therefore 360000 tons of paddy", Interval(7, 9), goldEntity="MEASUREMENT", goldNorm="360000.0 t")
     ensure(sentence= "Total production was 6883 thousand tons", Interval(3, 6), goldEntity="MEASUREMENT", goldNorm="6883000.0 t")
     ensure(sentence= "During 2009-10, area under rice cultivation was 2883 thousand hectares", Interval(8, 11), goldEntity="MEASUREMENT", goldNorm="2883000.0 ha")
-    ensure(sentence= "Senegal is forecast at 2.4 million MT", Interval(4, 7), goldEntity="MEASUREMENT", goldNorm="2400000 t")
-    ensure(sentence= "To determine the effect of planting date on key agronomic traits in rice, an 8 yr data", Interval(15, 17), goldEntity="MEASUREMENT", goldNorm="8 y")
-    ensure(sentence= "Planting dates were tentatively spaced by 2 wk", Interval(6, 8), goldEntity="MEASUREMENT", goldNorm="2 w")
+    ensure(sentence= "Senegal is forecast at 2.4 million MT", Interval(4, 7), goldEntity="MEASUREMENT", goldNorm="2400000.0 t")
+    ensure(sentence= "To determine the effect of planting date on key agronomic traits in rice, an 8 yr data", Interval(15, 17), goldEntity="MEASUREMENT", goldNorm="8.0 y")
+    ensure(sentence= "Planting dates were tentatively spaced by 2 wk", Interval(6, 8), goldEntity="MEASUREMENT", goldNorm="2.0 w")
     ensure(sentence= "1.68 ton for one hectare as a result of that the rainfall", Interval(0, 5), goldEntity="MEASUREMENT", goldNorm="1.68 t/ha")
-    ensure(sentence= "Rice is planted in early May next 5% reduction is only 7 d after that (24 April)", Interval(11, 13), goldEntity="MEASUREMENT", goldNorm="7 d")
+    ensure(sentence= "Rice is planted in early May next 5% reduction is only 7 d after that (24 April)", Interval(11, 13), goldEntity="MEASUREMENT", goldNorm="7.0 d")
 
     // these tests will fail as range numbers, numbers with commas and dot etc are not yet handled.
-    ensure(sentence= "Imports of rice in the decade 2008–2017 amounted on average to 1,500,000 tonnes", Interval(13, 15), goldEntity="MEASUREMENT", goldNorm="1500000.0 t")
+    ensure(sentence= "Imports of rice in the decade 2008-2017 amounted on average to 1,500,000 tonnes", Interval(13, 15), goldEntity="MEASUREMENT", goldNorm="1500000.0 t")
     ensure(sentence= "The production from the SRV was therefore 360.000 tons of paddy", Interval(7, 9), goldEntity="MEASUREMENT", goldNorm="360000.0 t")
-    ensure(sentence= "Weeding timing ranged from –2 to 17 days", Interval(4, 0), goldEntity="MEASUREMENT", goldNorm="2-17 d")
+    ensure(sentence= "Weeding timing ranged from -2 to 17 days", Interval(4, 0), goldEntity="MEASUREMENT", goldNorm="2.0 -- 17.0 d")
     
     // TODO: Mihai to decide on what should be the output of such measurement '3 or 4 days'
-    ensure(sentence= "and lasted 3 or 4 days in both wet seasons", Interval(2, 6), goldEntity="MEASUREMENT", goldNorm="3 or 4 d")
+    ensure(sentence= "and lasted 3 or 4 days in both wet seasons", Interval(2, 6), goldEntity="MEASUREMENT", goldNorm="4.0 d")
 
   }
 
@@ -193,46 +193,46 @@ class TestNumericEntityRecognition extends FlatSpec with Matchers {
   // tests for recognizing complex measurement units
   it should "recognize complex measurement units" in {
     ensure(sentence= "Recommended seed usage is 130 kg/ha", Interval(4, 8), goldEntity="MEASUREMENT", goldNorm="130.0 kg/ha")
-    ensure(sentence= "1.25 to 1.65 mt/ha higher on average", Interval(0, 6), goldEntity="MEASUREMENT", goldNorm="1.25-1.65 t/ha")
+    ensure(sentence= "1.25 to 1.65 mt/ha higher on average", Interval(0, 6), goldEntity="MEASUREMENT", goldNorm="1.25 -- 1.65 t/ha")
     ensure(sentence= "With average yields of 6-7 mt/ha", Interval(4, 10), goldEntity="MEASUREMENT", goldNorm="6-7 t/ha")
-    ensure(sentence= "Average yield reached 7.2 t ha–1 in 1999", Interval(3, 8), goldEntity="MEASUREMENT", goldNorm="7.2 t/ha")
+    ensure(sentence= "Average yield reached 7.2 t ha-1 in 1999", Interval(3, 8), goldEntity="MEASUREMENT", goldNorm="7.2 t/ha")
     ensure(sentence= "The Nakhlet farmers’ organization bought 7 tonnes of urea", Interval(6, 8), goldEntity="MEASUREMENT", goldNorm="7.0 t")
-    ensure(sentence= "Fertilizers were given to farmers proportionally to their cultivated area at the rate of 250 kg urea ha–1", Interval(14, 20), goldEntity="MEASUREMENT", goldNorm="250.0 kg/ha")
-    ensure(sentence= "Rainfed rice yields average 1-2 MT/hectare", Interval(4, 10), goldEntity="MEASUREMENT", goldNorm="1-2 t/ha")
+    ensure(sentence= "Fertilizers were given to farmers proportionally to their cultivated area at the rate of 250 kg urea ha-1", Interval(14, 20), goldEntity="MEASUREMENT", goldNorm="250.0 kg/ha")
+    ensure(sentence= "Rainfed rice yields average 1-2 MT/hectare", Interval(4, 10), goldEntity="MEASUREMENT", goldNorm="1.0 -- 2.0 t/ha")
     ensure(sentence= "having a gross plot size of 3.0 m × 6.0 m", Interval(6, 11), goldEntity="MEASUREMENT", goldNorm="18.0 m2")
     ensure(sentence= "500 mL acre-1 was applied on moist soil after 30-35 days of planting each crop", Interval(0, 5), goldEntity="MEASUREMENT", goldNorm="500.0 ml/acre")
     ensure(sentence= "The total area represented in each image was 3.24 cm2", Interval(8, 10), goldEntity="MEASUREMENT", goldNorm="3.24 cm2")
-    ensure(sentence= "Average yields, at 1 to 2 tonnes/ha, are much lower than in the SRV", Interval(4, 10), goldEntity="MEASUREMENT", goldNorm="1-2 t/ha")
-    ensure(sentence= "Irrigated rice yields are consistently high, averaging 5 to 6 MT/hectare", Interval(8, 14), goldEntity="MEASUREMENT", goldNorm="5-6 t/ha")
+    ensure(sentence= "Average yields, at 1 to 2 tonnes/ha, are much lower than in the SRV", Interval(4, 10), goldEntity="MEASUREMENT", goldNorm="1.0 -- 2.0 t/ha")
+    ensure(sentence= "Irrigated rice yields are consistently high, averaging 5 to 6 MT/hectare", Interval(8, 14), goldEntity="MEASUREMENT", goldNorm="5.0 -- 6.0 t/ha")
     ensure(sentence= "Pandan Wangi has a grain yield of 4.94 tons/ha", Interval(7, 11), goldEntity="MEASUREMENT", goldNorm="4.94 t/ha")
-    ensure(sentence= "close to the recommendations of 120 kg N ha–1", Interval(5, 11), goldEntity="MEASUREMENT", goldNorm="120.0 kg/ha")
-    ensure(sentence= "19 kg P ha–1 were applied in two top-dressed applications", Interval(0, 6), goldEntity="MEASUREMENT", goldNorm="19.0 kg/ha")
-    ensure(sentence= "cultivated area at the rate of 250 kg urea ha–1", Interval(7, 13), goldEntity="MEASUREMENT", goldNorm="250.0 kg/ha")
-    ensure(sentence= "farmers applied a combination of propanil 4 liters ha–1", Interval(6, 10), goldEntity="MEASUREMENT", goldNorm="4.0 l/ha")
+    ensure(sentence= "close to the recommendations of 120 kg N ha-1", Interval(5, 11), goldEntity="MEASUREMENT", goldNorm="120.0 kg/ha")
+    ensure(sentence= "19 kg P ha-1 were applied in two top-dressed applications", Interval(0, 6), goldEntity="MEASUREMENT", goldNorm="19.0 kg/ha")
+    ensure(sentence= "cultivated area at the rate of 250 kg urea ha-1", Interval(7, 13), goldEntity="MEASUREMENT", goldNorm="250.0 kg/ha")
+    ensure(sentence= "farmers applied a combination of propanil 4 liters ha-1", Interval(6, 10), goldEntity="MEASUREMENT", goldNorm="4.0 l/ha")
     ensure(sentence= "East with land area of 147,141 Km2", Interval(5, 7), goldEntity="MEASUREMENT", goldNorm="147141.0 km2")
     ensure(sentence= "Rice should not be planted when the average air and soil temperature is below 15 ˚C", Interval(14, 17), goldEntity="MEASUREMENT", goldNorm="15 ˚C")
     ensure(sentence= "Potassium (150 kg K2O ha−1) was split equally at basal fertilization", Interval(2, 8), goldEntity="MEASUREMENT", goldNorm="150.0 kg/ha")
     ensure(sentence= "Grain yield was determined from a 5-m2 area in each plot", Interval(6, 9), goldEntity="MEASUREMENT", goldNorm="5.0 m2")
     ensure(sentence= "Punjab has 3.5 million ha under wheat cultivation with productivity of 5.2-ton ha-1 respectively.", Interval(11, 17), goldEntity="MEASUREMENT", goldNorm="5.2 t/ha")
-    ensure(sentence= "daily maximum temperature of 40–45 C in May", Interval(4, 8), goldEntity="MEASUREMENT", goldNorm="40–45 °C")
+    ensure(sentence= "daily maximum temperature of 40-45 C in May", Interval(4, 8), goldEntity="MEASUREMENT", goldNorm="40.0 -- 45.0 °C")
     ensure(sentence= "with a corresponding increase in unit yield of 337.5 kg·ha-1 and only 249 kg·ha-1", Interval(8, 14), goldEntity="MEASUREMENT", goldNorm="337.5 kg/ha")
-    ensure(sentence= "CEC varied from 18 to 29 cmol kg–1", Interval(2, 10), goldEntity="MEASUREMENT", goldNorm="18-29 cmol/kg")
+    ensure(sentence= "CEC varied from 18 to 29 cmol kg-1", Interval(2, 10), goldEntity="MEASUREMENT", goldNorm="18.0 -- 29.0 cmol/kg")
 
   }
 
   // tests for mass and concentation units (Soil bulk density, volume basis etc)
   it should "recognize mass and concentration measurement units" in {
-    ensure(sentence= "N content ranged from 0.37 to 0.71 g kg–1 soil", Interval(4, 11), goldEntity="MEASUREMENT", goldNorm="0.37-0.71 g/kg")
-    ensure(sentence= "C content ranged from 4.4 to 7.9 mg g–1 soil, ", Interval(4, 11), goldEntity="MEASUREMENT", goldNorm="4.4-7.9 mg/g")
-    ensure(sentence= "P-Olsen ranged from 4.3 to 17 g.kg-1 soil", Interval(5, 11), goldEntity="MEASUREMENT", goldNorm="4.3-17.0 g/kg")
+    ensure(sentence= "N content ranged from 0.37 to 0.71 g kg-1 soil", Interval(4, 11), goldEntity="MEASUREMENT", goldNorm="0.37 -- 0.71 g/kg")
+    ensure(sentence= "C content ranged from 4.4 to 7.9 mg g-1 soil, ", Interval(4, 11), goldEntity="MEASUREMENT", goldNorm="4.4 -- 7.9 mg/g")
+    ensure(sentence= "P-Olsen ranged from 4.3 to 17 g.kg-1 soil", Interval(5, 11), goldEntity="MEASUREMENT", goldNorm="4.3 -- 17.0 g/kg")
     ensure(sentence= "with concentrations reaching 3.99 mg kg−1", Interval(3, 8), goldEntity="MEASUREMENT", goldNorm="3.99 mg/kg")
     ensure(sentence= "the irrigation water supply was above 700 mm", Interval(6, 8), goldEntity="MEASUREMENT", goldNorm="700.0 mm")
-    ensure(sentence= "sugar 6976 µg/g", Interval(1, 5), goldEntity="MEASUREMENT", goldNorm="6976 µg/g")
+    ensure(sentence= "sugar 6976 µg/g", Interval(1, 5), goldEntity="MEASUREMENT", goldNorm="6976.0 µg/g")
     ensure(sentence= "1.1 mg/g uronic acid", Interval(0, 4), goldEntity="MEASUREMENT", goldNorm="1.1 mg/g")
     ensure(sentence= "731.5 µg/g protein", Interval(0, 4), goldEntity="MEASUREMENT", goldNorm="731.5 µg/g")
     ensure(sentence= "Saturated water content 4.54 m3 m-3", Interval(3, 7), goldEntity="MEASUREMENT", goldNorm="4.54 m3/m3")
     ensure(sentence= "Soil organic carbon (SOC) under fallow varied from 7.1 g kg-1", Interval(8, 13), goldEntity="MEASUREMENT", goldNorm="7.1 g/kg")
-    ensure(sentence= "the rainfall during October and November was about (144 ml) during 2015–2016 season may cause better moisture availability", Interval(9, 11), goldEntity="MEASUREMENT", goldNorm="144 ml")
+    ensure(sentence= "the rainfall during October and November was about (144 ml) during 2015-2016 season may cause better moisture availability", Interval(9, 11), goldEntity="MEASUREMENT", goldNorm="144.0 ml")
   }
 
   //
