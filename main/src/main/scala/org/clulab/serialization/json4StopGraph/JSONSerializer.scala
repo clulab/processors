b736875b0d9aa4efcfb36b22298b64959b5f055a
Keith Alcock <github@keithalcock.com>
2021-09-22 13:37:19 -0700

    Submit rough draft

---
+++ main/src/main/scala/org/clulab/serialization/json4StopGraph/JSONSerializer.scala
@@ -0,0 +1,62 @@
+package org.clulab.serialization.json4StopGraph
+
+import org.clulab.processors.Document
+import org.clulab.serialization.json.stringify
+import org.clulab.struct.DirectedGraph
+import org.clulab.struct.GraphMap
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson._
+
+import java.io.PrintWriter
+
+class JSONSerializer(val printWriter: PrintWriter) {
+  implicit val formats = DefaultFormats
+
+  def serialize(doc: Document): Unit = printWriter.println(toJson(doc))
+
+  def toJson(doc: Document): String = {
+    val jValue = toJValue(doc)
+    val json = stringify(jValue, pretty = true)
+
+    json
+  }
+
+  def toJValue(directedGraph: DirectedGraph[String]): JValue = {
+    directedGraph.edges.map { edge =>
+      new JArray(List(edge.source, edge.destination, edge.relation))
+    }
+ }
+
+  def toJValue(doc: Document): JValue = {
+    val jSentences = doc.sentences.toList.map { sentence =>
+
+      def graphToJValueOpt(key: String): Option[JValue] = sentence.graphs.get(key).map(toJValue)
+
+      val jTokens = sentence.words.zipWithIndex.toList.map { case (word, index) =>
+        new JArray(List(word, index))
+      }
+      val jStarts = sentence.startOffsets.toList
+      val jEnds = sentence.endOffsets.toList
+      val jChunksOpt = sentence.chunks.map(_.toList)
+      val jBasicSyntacticDependenciesOpt    = graphToJValueOpt(GraphMap.UNIVERSAL_BASIC)
+      val jEnhancedSyntacticDependenciesOpt = graphToJValueOpt(GraphMap.UNIVERSAL_ENHANCED)
+      val jSemanticDependenciesOpt          = graphToJValueOpt(GraphMap.SEMANTIC_ROLES)
+      val jEnhancedSemanticDependenciesOpt  = graphToJValueOpt(GraphMap.ENHANCED_SEMANTIC_ROLES)
+      val jSentence: JObject = {
+          ("Tokens" -> jTokens) ~
+          ("Start_character_offsets" -> jStarts) ~
+          ("End_character_offsets" -> jEnds) ~
+          ("Chunks" -> jChunksOpt) ~
+          ("Basic_syntactic_dependencies"    -> jBasicSyntacticDependenciesOpt) ~
+          ("Enhanced_syntactic_dependencies" -> jEnhancedSyntacticDependenciesOpt) ~
+          ("Semantic_dependencies"           -> jSemanticDependenciesOpt) ~
+          ("Enhanced_semantic_dependencies"  -> jEnhancedSemanticDependenciesOpt)
+      }
+
+      jSentence
+    }
+
+    jSentences
+  }
+}
