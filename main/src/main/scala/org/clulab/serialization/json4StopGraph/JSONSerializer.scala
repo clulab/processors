0c20b67c7c4d8233bff64c2b82a5179822b65038
Keith Alcock <github@keithalcock.com>
2021-09-22 13:53:00 -0700

    Reorder vertically, horizontally
    Move chunks up, sort dependencies by source, destination

--- main/src/main/scala/org/clulab/serialization/json4StopGraph/JSONSerializer.scala
+++ main/src/main/scala/org/clulab/serialization/json4StopGraph/JSONSerializer.scala
@@ -23,9 +23,11 @@ class JSONSerializer(val printWriter: PrintWriter) {
   }
 
   def toJValue(directedGraph: DirectedGraph[String]): JValue = {
-    directedGraph.edges.map { edge =>
-      new JArray(List(edge.source, edge.destination, edge.relation))
-    }
+    directedGraph.edges
+        .sortBy { edge => (edge.source, edge.destination, edge.relation) }
+        .map { edge =>
+          new JArray(List(edge.source, edge.destination, edge.relation))
+        }
  }
 
   def toJValue(doc: Document): JValue = {
@@ -36,18 +38,22 @@ class JSONSerializer(val printWriter: PrintWriter) {
       val jTokens = sentence.words.zipWithIndex.toList.map { case (word, index) =>
         new JArray(List(word, index))
       }
+      val jChunksOpt = sentence.chunks.map { chunks =>
+        chunks.zipWithIndex.toList.map { case (chunk, index) =>
+          new JArray(List(chunk, index))
+        }
+      }
       val jStarts = sentence.startOffsets.toList
       val jEnds = sentence.endOffsets.toList
-      val jChunksOpt = sentence.chunks.map(_.toList)
       val jBasicSyntacticDependenciesOpt    = graphToJValueOpt(GraphMap.UNIVERSAL_BASIC)
       val jEnhancedSyntacticDependenciesOpt = graphToJValueOpt(GraphMap.UNIVERSAL_ENHANCED)
       val jSemanticDependenciesOpt          = graphToJValueOpt(GraphMap.SEMANTIC_ROLES)
       val jEnhancedSemanticDependenciesOpt  = graphToJValueOpt(GraphMap.ENHANCED_SEMANTIC_ROLES)
       val jSentence: JObject = {
           ("Tokens" -> jTokens) ~
+          ("Chunks" -> jChunksOpt) ~
           ("Start_character_offsets" -> jStarts) ~
           ("End_character_offsets" -> jEnds) ~
-          ("Chunks" -> jChunksOpt) ~
           ("Basic_syntactic_dependencies"    -> jBasicSyntacticDependenciesOpt) ~
           ("Enhanced_syntactic_dependencies" -> jEnhancedSyntacticDependenciesOpt) ~
           ("Semantic_dependencies"           -> jSemanticDependenciesOpt) ~
