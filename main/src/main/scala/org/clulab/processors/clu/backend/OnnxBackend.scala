4f8cd3a3eab992b39bc36dc8bf8b2b76c2e6ed47
Keith Alcock <github@keithalcock.com>
2021-10-05 16:46:31 -0700

    Add Onnx backend

--- main/src/main/scala/org/clulab/processors/clu/backend/OnnxBackend.scala
+++ main/src/main/scala/org/clulab/processors/clu/backend/OnnxBackend.scala
@@ -2,27 +2,27 @@ package org.clulab.processors.clu.backend
 
 import org.clulab.processors.clu.AnnotatedSentence
 
-object ScalaBackend extends CluBackend
+object OnnxBackend extends CluBackend
 
-class ScalaPosBackend(modelFilenamePrefix: String) extends PosBackend {
+class OnnxPosBackend(modelFilenamePrefix: String) extends PosBackend {
 
   def predict(annotatedSentence: AnnotatedSentence, embeddingsAttachment: EmbeddingsAttachment):
       (IndexedSeq[String], IndexedSeq[String], IndexedSeq[String]) = ???  // tags, chunks, and preds
 }
 
-class ScalaNerBackend(modelFilenamePrefix: String) extends NerBackend {
+class OnnxNerBackend(modelFilenamePrefix: String) extends NerBackend {
 
   def predict(annotatedSentence: AnnotatedSentence, embeddingsAttachment: EmbeddingsAttachment):
       IndexedSeq[String] = ??? // labels
 }
 
-class ScalaSrlaBackend(modelFilenamePrefix: String) extends SrlaBackend {
+class OnnxSrlaBackend(modelFilenamePrefix: String) extends SrlaBackend {
 
   def predict(taskId: Int, annotatedSentence: AnnotatedSentence, embeddingsAttachment: EmbeddingsAttachment):
       IndexedSeq[String] = ??? // labels
 }
 
-class ScalaDepsBackend(modelFilenamePrefix: String) extends DepsBackend {
+class OnnxDepsBackend(modelFilenamePrefix: String) extends DepsBackend {
 
   def predict(annotatedSentence: AnnotatedSentence, embeddingsAttachment: EmbeddingsAttachment):
     IndexedSeq[(Int, String)] = ??? // heads and labels
