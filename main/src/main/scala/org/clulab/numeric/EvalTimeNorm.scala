9dd31ef9bd5554d3c0029ff20b2a99d2121c9cc9
EgoLaparra <ego.laparra@gmail.com>
2021-10-26 18:44:43 -0700

    make everything inmutable in EvalTimeNorm

--- main/src/main/scala/org/clulab/numeric/EvalTimeNorm.scala
+++ main/src/main/scala/org/clulab/numeric/EvalTimeNorm.scala
@@ -3,6 +3,7 @@ package org.clulab.numeric
 import org.clulab.dynet.Utils
 import org.clulab.numeric.mentions.Norm
 import org.clulab.processors.clu.CluProcessor
+
 import scala.io.Source
 
 object EvalTimeNorm {
@@ -14,45 +15,37 @@ object EvalTimeNorm {
     val goldStream = getClass.getResourceAsStream(s"$timeNormEvalDir/$testFile")
     val goldLines = Source.fromInputStream(goldStream).getLines
     // Build a Map with the gold time expressions.
-    // The key is tuple with the document name and the document creation time
-    // The values is the a Seq with the time expressions (TimeNormScorer.Timex corresponding) in the document
+    // The key is the document name. The value is a Seq with the time expressions in the document
     val goldTimex = (for ((goldLine, goldIdx) <- goldLines.toSeq.zipWithIndex) yield {
       goldLine.split(",").map(_.trim) match {
         case Array(docId, startSpan, endSpan, startIntervalStr) =>
           (docId, (startSpan, endSpan, startIntervalStr))
       }
     }).groupBy(t => t._1).mapValues(_.map(_._2))
-    var matchers = 0
-    var all_predictions = 0
-    var all_gold_labels = 0
-    // For each docId in goldTimex keys get the, parse the document and extract the time expressions found
-    for (docId <- goldTimex.keys.toSeq.sorted) {
+    // For each docId in goldTimex keys get parse the document and get the number of
+    // gold time expressions, predicted time expressions and the intersection
+    val valuesPerDocument = for (docId <- goldTimex.keys.toSeq.sorted) yield {
+      val gold = goldTimex(docId).toSet
       val docStream = getClass.getResourceAsStream(s"$timeNormEvalDir/$docId/$docId")
       val docText = Source.fromInputStream(docStream).getLines().mkString("\n")
       val doc = proc.annotate(docText)
       val mentions = ner.extractFrom(doc)
-      val mention_label = "DATE"
-      val mention_label_range = "DATE-RANGE"
       setLabelsAndNorms(doc, mentions)
-      val predicted_output = for {mention <- mentions
-                                  if mention.isInstanceOf[Norm]
-                                  if mention.asInstanceOf[Norm].neLabel == mention_label || mention.asInstanceOf[Norm].neLabel == mention_label_range} yield {
-        (mention.startOffset.toString, mention.endOffset.toString, mention.asInstanceOf[Norm].neNorm)
-      }
-      val intersection = predicted_output.toSet.intersect(goldTimex(docId).toSet)
-      matchers += intersection.size
-      all_predictions += predicted_output.toSet.size
-      all_gold_labels += goldTimex(docId).toSet.size
-
+      val prediction =  mentions.collect{
+        case m: Norm if m.neLabel.equals("DATE") || m.neLabel.equals("DATE-RANGE") =>
+          (m.startOffset.toString, m.endOffset.toString, m.neNorm)
+      }.toSet
+      val intersection = prediction.intersect(gold)
+      (prediction.size, gold.size, intersection.size)
     }
-    val precision = matchers.toFloat / all_predictions.toFloat
-    val recall = matchers.toFloat / all_gold_labels.toFloat
     // Calculate the overall performance
-
+    val totalValues = valuesPerDocument.reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3))
+    val precision = totalValues._3.toFloat / totalValues._1
+    val recall = totalValues._3.toFloat / totalValues._2
     val fscore = 2 * precision * recall / (precision + recall)
-    println("precision: ", precision)
-    println("recall: ", recall)
-    println("f1 score: ", fscore)
+    printf("Precision: %.3f\n", precision)
+    printf("Recall: %.3f\n", recall)
+    printf("F1 score: %.3f\n", fscore)
     fscore
   }
 
