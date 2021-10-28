ee1c6ddbfe3382868bbcb76780879258cded9dfe
Zheng Tang <oscartang1120@gmail.com>
2021-10-27 21:53:01 -0700

    specified input and output names

--- main/src/main/python/pytorch/pytorch2onnx.py
+++ main/src/main/python/pytorch/pytorch2onnx.py
@@ -10,20 +10,21 @@ if __name__ == '__main__':
     modelName = args.model_file
     model = Metal.load(modelName)
 
-    input_names = [ "input" ]
-    output_names = [ "output" ]
-    
     #In current setting, each layer is a nn.Moudle and we need to export each of them. This is not very elegant...
     for i, layers in enumerate(model):
         if layers.initialLayer is not None:
             #export the initial layer
+            input_names_1 = ["sentence", "const embeddings"]
+            output_names_1 = [ "embeddings" ]
             dummy_input = (sentence, embeddings)# we need some toy sentence and embeddings here, not sure if onnx is happy with this input though...
-            torch.onnx.export(layers.initialLayer, dummy_input_1, "initialLayer_inTask%d.onnx"%i, verbose=True, input_names=input_names, output_names=output_names)
+            torch.onnx.export(layers.initialLayer, dummy_input_1, "initialLayer_inTask%d.onnx"%i, verbose=True, input_names=input_names_1, output_names=output_names_1)
             dummy_input = layers.initialLayer(sentence, embeddings)
         for j, il in enumerate(layers.intermediateLayers):
             #export the intermediate layer layer
-            torch.onnx.export(il, dummy_input_2, "intermediateLayer_%d_inTask%d.onnx"%(i,j), verbose=True, input_names=input_names, output_names=output_names)
+            input_names_2 = ["input", "dropout"]
+            output_names_2 = [ "output" ]
+            torch.onnx.export(il, dummy_input_2, "intermediateLayer_%d_inTask%d.onnx"%(i,j), verbose=True, input_names=input_names_2, output_names=output_names_2)
             dummy_input = il(dummy_input)
         if layers.finalLayer is not None:
             #export the final layer
-            torch.onnx.export(layers.finalLayer, dummy_input, "finalLayer_inTask%d.onnx"%i, verbose=True, input_names=input_names, output_names=output_names)
+            torch.onnx.export(layers.finalLayer, dummy_input, "finalLayer_inTask%d.onnx"%i, verbose=True, input_names=input_names_2, output_names=output_names_2)
