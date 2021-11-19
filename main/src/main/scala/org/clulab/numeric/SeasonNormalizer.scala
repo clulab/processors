4c4eafdadc1ed5683274f71ddb67f8ceab302e95
EgoLaparra <ego.laparra@gmail.com>
2021-11-19 12:14:57 -0700

    Get commented text with CommentedStandardKbSource in SeasonNormalizer

--- main/src/main/scala/org/clulab/numeric/SeasonNormalizer.scala
+++ main/src/main/scala/org/clulab/numeric/SeasonNormalizer.scala
@@ -2,14 +2,44 @@ package org.clulab.numeric
 
 import java.io.File
 
-import org.clulab.utils.FileUtils
+import org.clulab.sequences.CommentedStandardKbSource
+import org.clulab.utils.Closer.AutoCloser
+import org.clulab.utils.Sourcer
 
 import scala.collection.mutable
+import scala.io.Source
 
 object SeasonNormalizer {
-  private val COMMENT = "//"
+  private val normMapper = readNormsFromResource("/org/clulab/numeric/SEASON.tsv")
 
-  private val normMapper = readNorms()
+  def readNormsFromResource(path: String): Map[String, SeasonRange] = {
+    val customResourcePath = {
+      val cwd = new File(System.getProperty("user.dir"), "src/main/resources/")
+      new File(cwd, path)
+    }
+    if (customResourcePath.exists)
+      Sourcer.sourceFromFile(customResourcePath).autoClose(readNormsFromSource)
+    else
+      Sourcer.sourceFromResource(path).autoClose(readNormsFromSource)
+  }
+
+  def readNormsFromSource(source: Source): Map[String, SeasonRange] = {
+    val norms = new mutable.HashMap[String, SeasonRange]()
+
+    CommentedStandardKbSource.read(source) { (season, normOpt) =>
+      assert(normOpt.isDefined) // We're insisting on this.
+
+      val norm = normOpt.get.split("--").map(_.trim)
+      val normTuple = norm match {
+        case Array(start, end) => (start, end)
+        case _ => throw new RuntimeException(s"ERROR: incorrect date range in season file")
+      }
+      val (startMonth, startDay) = getDayMonth(normTuple._1)
+      val (endMonth, endDay) = getDayMonth(normTuple._2)
+      norms += season -> SeasonRange(startDay, startMonth, endDay, endMonth)
+    }
+    norms.toMap
+  }
 
   private def getDayMonth(date: String): (Option[Seq[String]], Option[Seq[String]]) = {
     date.split("-") match {
@@ -17,39 +47,6 @@ object SeasonNormalizer {
       case Array(_, month) => (Some(Seq(month)), None)
       case _ => throw new RuntimeException(s"ERROR: incorrect date value in season file: $date")
     }
-  }
-
-  private def readNorms(): Map[String, SeasonRange] = {
-    val norms = new mutable.HashMap[String, SeasonRange]()
-    val resourcePath = "/org/clulab/numeric/SEASON.tsv"
-    val customResourcePath = {
-      val cwd = new File(System.getProperty("user.dir"), "src/main/resources/")
-      new File(cwd, resourcePath)
-    }
-    val commentedText = if (customResourcePath.exists)
-      FileUtils.getCommentedTextSetFromFile(customResourcePath)
-    else
-      FileUtils.getCommentedTextSetFromResource(resourcePath)
-
-    for(line <- commentedText) {
-      // the text before the comment (//) is the season name; the text after is the month range
-      val commentStart = line.indexOf(COMMENT)
-      assert(commentStart > 0 && commentStart < line.length)
-
-      val season = line.substring(0, commentStart).trim
-      val norm = line.substring(commentStart + COMMENT.length).split("--").map(_.trim)
-      val normTuple = norm match {
-        case Array(start, end) => (start, end)
-        case _ => throw new RuntimeException(s"ERROR: incorrect date range in season file: $line")
-      }
-
-      val (startMonth, startDay) = getDayMonth(normTuple._1)
-      val (endMonth, endDay) = getDayMonth(normTuple._2)
-
-      norms += season -> SeasonRange(startDay, startMonth, endDay, endMonth)
-    }
-
-    norms.toMap
   }
 
   /** Adjust the year start and end according to season date range */
--- main/src/main/scala/org/clulab/utils/FileUtils.scala
+++ main/src/main/scala/org/clulab/utils/FileUtils.scala
@@ -56,12 +56,6 @@ object FileUtils {
     }
 
   // Add FromResource as necessary.  See getText below,
-  def getCommentedTextSetFromFile(file: File): Set[String] =
-    Sourcer.sourceFromFile(file).autoClose { source =>
-      getCommentedLinesFromSource(source).map(_.trim).toSet
-    }
-
-  // Add FromResource as necessary.  See getText below,
   def getCommentedTextFromFile(file: File, sep: String = " "): String =
     Sourcer.sourceFromFile(file).autoClose { source =>
       // These haven't been trimmed in case esp. trailing spaces are important.
