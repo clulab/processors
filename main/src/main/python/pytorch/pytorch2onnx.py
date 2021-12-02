86f12505c049dcbaa183d3ec8120750912738d4f
Zheng Tang <oscartang1120@gmail.com>
2021-12-01 21:58:02 -0700

    get dummy input

--- main/src/main/python/pytorch/pytorch2onnx.py
+++ main/src/main/python/pytorch/pytorch2onnx.py
@@ -1,15 +1,15 @@
 import torch
 
 from pytorch.metal import Metal
-from pytorch.utils import mkCharacterEmbedding2
+from pytorch.utils import *
+from pytorch.constEmbeddingsGlove import ConstEmbeddingsGlove
+from sequences.rowReaders import *
 
 class Saving_Model(nn.Module):
     """docstring for Saving_Model"""
     def __init__(self, model, constEmbeddings):
         super().__init__()
         self.model = model
-        for layers in model:
-            layers.start_eval()
         self.constEmbeddings = constEmbeddings
         self.initialLayers = [None for _ in range(len(model))]
         for i, layers in enumerate(model):
@@ -39,12 +39,48 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_file', type=str, help='Filename of the model.')
+    parser.add_argument('--config', type=str, help='Filename of the configuration.')
+    parser.add_argument('--seed', type=int, default=1234)
     args = parser.parse_args()
+
+    config = ConfigFactory.parse_file(f'../resources/org/clulab/{args.config}.conf')
+    taskManager = TaskManager(config, args.seed)
     modelName = args.model_file
     model = Metal.load(modelName)
+    for layers in model:
+        layers.start_eval()
+    constEmbeddings = ConstEmbeddingsGlove.get_ConstLookupParams()
+    export_model = Saving_Model(model, constEmbeddings)
+    export_model.eval()
 
-    export_model = Saving_Model(model)
+    torch.manual_seed(taskManager.random)
+    random.seed(taskManager.random)
 
-    torch.onnx.export(export_model, dummy_input, "model.onnx", verbose=True, input_names=input_names_2, output_names=output_names_2)
+    for i, layers in enumerate(model):
+        if layers.initialLayer is not None:
+            c2i = layers.initialLayer.c2i
+
+    for taskId in range(0, taskManager.taskCount):
+        taskName = taskManager.tasks[taskId].taskName
+        testSentences = taskManager.tasks[taskId].testSentences
+        if testSentences:
+            reader = MetalRowReader()
+            annotatedSentences = reader.toAnnotatedSentences(testSentences[0])
+
+            asent = annotatedSentences[0]
+            sentence = asent[0]
+            goldLabels = asent[1]
+
+            words = sentence.words
+
+            word_ids = torch.LongTensor([constEmbeddings.w2i[word] if word in constEmbeddings.w2i else 0 for word in words])
+            char_ids_list = [torch.LongTensor([c2i.get(c, UNK_EMBEDDING) for c in w]) for word in words]
+
+            dummy_input = [word_ids, char_ids_list]
+
+            input_names = [ "input1", "input2" ]
+            output_names = [ "output" ]
+
+    torch.onnx.export(export_model, dummy_input, "model.onnx", verbose=True, input_names=input_names, output_names=output_names)
 
     
