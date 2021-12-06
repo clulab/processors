de010c3bfb39215c9c40000e0ce757d8b395664c
Zheng Tang <oscartang1120@gmail.com>
2021-12-06 12:32:58 -0700

    Update pytorch2onnx.py

--- main/src/main/python/pytorch2onnx.py
+++ main/src/main/python/pytorch2onnx.py
@@ -9,6 +9,9 @@ from pytorch.utils import *
 from pytorch.constEmbeddingsGlove import ConstEmbeddingsGlove
 from sequences.rowReaders import *
 
+import onnx
+import onnxruntime
+
 class Saving_Model(torch.nn.Module):
     """docstring for Saving_Model"""
     def __init__(self, model, constEmbeddings):
@@ -106,5 +109,22 @@ if __name__ == '__main__':
                   output_names = ['output'], # the model's output names
                   dynamic_axes = {'input' : {0 : 'batch_size'},    # variable length axes
                                 'output' : {0 : 'batch_size'}})
+
+    onnx_model = onnx.load("model.onnx")
+    onnx.checker.check_model(onnx_model)
+
+    ort_session = onnxruntime.InferenceSession("model.onnx")
+
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    # compare ONNX Runtime and PyTorch results
+    np.testing.assert_allclose(to_numpy(output), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
 
     
