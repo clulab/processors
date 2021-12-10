0d98b41221889d0b471a600d5451ad5706f36027
Keith Alcock <github@keithalcock.com>
2021-12-10 13:43:46 -0700

    Account for Traversable

--- main/src/main/scala/org/clulab/learning/FeatureUpdater.scala
+++ main/src/main/scala/org/clulab/learning/FeatureUpdater.scala
@@ -4,7 +4,13 @@ package org.clulab.learning
  * Allows traversal of a dataset (or ranking dataset)'s features and values, and also destructive updates of the values. Useful for finding the range of values and then rescaling them. Analogous to iterator with destructive updates
  * Created by dfried on 5/27/14.
  */
-trait FeatureUpdater[F, V] extends Traversable[(F, V)] {
+trait FeatureUpdater[F, V] {
+
+  /**
+   * Traverse the dataset's features and values.
+   */
+  def foreach[U](fn: ((F, Double)) => U): Unit
+
   /**
    * Destructively modify all feature values using a function of the feature and value
    */
