31cbb7522e557bf97315aad3df30942f10d64705
Zheng Tang <oscartang1120@gmail.com>
2021-12-15 18:06:16 -0700

    updated build file and test onnx

--- main/src/main/scala/org/clulab/dynet/TestOnnx.scala
+++ main/src/main/scala/org/clulab/dynet/TestOnnx.scala
@@ -9,15 +9,32 @@ import com.typesafe.config.ConfigFactory
 import org.clulab.dynet.Utils._
 import org.clulab.utils.StringUtils
 
+import scala.io.Source
+import scala.util.parsing.json._
 
-object GetWordEmbeddings extends App {
+import ai.onnxruntime.{OnnxTensor, OrtEnvironment, OrtSession}
+
+
+object TestOnnx extends App {
     val props = StringUtils.argsToProperties(args)
 
     val configName = props.getProperty("conf")
     val config = ConfigFactory.load(configName)
     val taskManager = new TaskManager(config)
+    
     val constEmbeddingsGlove = ConstEmbeddingsGlove // Make sure that the embeddings have been loaded.
     val wordEmbeddingMap = WordEmbeddingMapPool.get("glove.840B.300d.10f", compact = true).get
+    
+    val jsonString = Source.fromFile("ner.json").getLines.mkString
+    val parsed = JSON.parseFull(jsonString)
+    val w2i = parsed.get.asInstanceOf[List[Any]](0).asInstanceOf[Map[String, Any]]("x2i").asInstanceOf[Map[String, Any]]("initialLayer").asInstanceOf[Map[String, Any]]("w2i").asInstanceOf[Map[String, Double]]
+    val c2i = parsed.get.asInstanceOf[List[Any]](0).asInstanceOf[Map[String, Any]]("x2i").asInstanceOf[Map[String, Any]]("initialLayer").asInstanceOf[Map[String, Any]]("c2i").asInstanceOf[Map[String, Double]]
+
+    val modelpath1 = "char.onnx"
+    val session1 = ortEnvironment.createSession(modelpath1, new OrtSession.SessionOptions)
+    val modelpath2 = "char.onnx"
+    val session2 = ortEnvironment.createSession(modelpath2, new OrtSession.SessionOptions)
+
     for(taskId <- 0 until taskManager.taskCount) {
         val taskName = taskManager.tasks(taskId).taskName
         val testSentences = taskManager.tasks(taskId).testSentences.get
@@ -31,6 +48,16 @@ object GetWordEmbeddings extends App {
                 for(as <- annotatedSentences) {
                     val sentence = as._1
                     val goldLabels = as._2
+
+                    val words = sentence.words
+
+                    for(word <- words){
+                        wordEmbeddingMap.get(word).get
+                        w2i(word).toLong
+                        for (c <- word){
+                            c2i(c).toLong
+                        }
+                    }
 
                     // val sc = SeqScorer.f1(goldLabels, preds)
                     // scoreCountsByLabel.incAll(sc)
