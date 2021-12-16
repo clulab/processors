356b6b709ad10f7ae19477e139dd7d1315775f68
Zheng Tang <oscartang1120@gmail.com>
2021-12-15 17:24:42 -0700

    Update TestOnnx.scala

--- main/src/main/scala/org/clulab/dynet/TestOnnx.scala
+++ main/src/main/scala/org/clulab/dynet/TestOnnx.scala
@@ -16,32 +16,35 @@ object GetWordEmbeddings extends App {
     val configName = props.getProperty("conf")
     val config = ConfigFactory.load(configName)
     val taskManager = new TaskManager(config)
-    val modelName = props.getProperty("test")
-    val testSentences = taskManager.tasks(taskId).testSentences
-
     val constEmbeddingsGlove = ConstEmbeddingsGlove // Make sure that the embeddings have been loaded.
     val wordEmbeddingMap = WordEmbeddingMapPool.get("glove.840B.300d.10f", compact = true).get
+    for(taskId <- 0 until taskManager.taskCount) {
+        val taskName = taskManager.tasks(taskId).taskName
+        val testSentences = taskManager.tasks(taskId).testSentences
+        val testSentences = taskManager.tasks(taskId).testSentences
+        val taskNumber = taskId + 1
 
-    if(testSentences.nonEmpty){
-        val scoreCountsByLabel = new ScoreCountsByLabel
-        val reader = new MetalRowReader
-        for (sent <- sentences) {
-            val annotatedSentences = reader.toAnnotatedSentences(sent)
-            for(as <- annotatedSentences) {
-                val sentence = as._1
-                val goldLabels = as._2
+        if(testSentences.nonEmpty){
+            val scoreCountsByLabel = new ScoreCountsByLabel
+            val reader = new MetalRowReader
+            for (sent <- testSentences) {
+                val annotatedSentences = reader.toAnnotatedSentences(sent)
+                for(as <- annotatedSentences) {
+                    val sentence = as._1
+                    val goldLabels = as._2
 
-                // val sc = SeqScorer.f1(goldLabels, preds)
-                // scoreCountsByLabel.incAll(sc)
+                    // val sc = SeqScorer.f1(goldLabels, preds)
+                    // scoreCountsByLabel.incAll(sc)
+                }
             }
         }
+        // logger.info(s"Accuracy on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.accuracy()}")
+        // logger.info(s"Precision on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.precision()}")
+        // logger.info(s"Recall on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.recall()}")
+        // logger.info(s"Micro F1 on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.f1()}")
+        // for(label <- scoreCountsByLabel.labels) {
+        //     logger.info(s"\tP/R/F1 for label $label (${scoreCountsByLabel.map(label).gold}): ${scoreCountsByLabel.precision(label)} / ${scoreCountsByLabel.recall(label)} / ${scoreCountsByLabel.f1(label)}")
+        // }
     }
-    // logger.info(s"Accuracy on ${sentences.length} $name sentences: ${scoreCountsByLabel.accuracy()}")
-    // logger.info(s"Precision on ${sentences.length} $name sentences: ${scoreCountsByLabel.precision()}")
-    // logger.info(s"Recall on ${sentences.length} $name sentences: ${scoreCountsByLabel.recall()}")
-    // logger.info(s"Micro F1 on ${sentences.length} $name sentences: ${scoreCountsByLabel.f1()}")
-    // for(label <- scoreCountsByLabel.labels) {
-    //     logger.info(s"\tP/R/F1 for label $label (${scoreCountsByLabel.map(label).gold}): ${scoreCountsByLabel.precision(label)} / ${scoreCountsByLabel.recall(label)} / ${scoreCountsByLabel.f1(label)}")
-    // }
 
 }
