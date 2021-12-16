7f6272bc85269307a954f7b560909a6afb82deea
Zheng Tang <oscartang1120@gmail.com>
2021-12-15 21:00:52 -0700

    Update TestOnnx.scala

--- main/src/main/scala/org/clulab/dynet/TestOnnx.scala
+++ main/src/main/scala/org/clulab/dynet/TestOnnx.scala
@@ -33,7 +33,7 @@ object TestOnnx extends App {
     val ortEnvironment = OrtEnvironment.getEnvironment
     val modelpath1 = "char.onnx"
     val session1 = ortEnvironment.createSession(modelpath1, new OrtSession.SessionOptions)
-    val modelpath2 = "char.onnx"
+    val modelpath2 = "model.onnx"
     val session2 = ortEnvironment.createSession(modelpath2, new OrtSession.SessionOptions)
 
     for(taskId <- 0 until taskManager.taskCount) {
@@ -51,27 +51,39 @@ object TestOnnx extends App {
                     val goldLabels = as._2
 
                     val words = sentence.words
-
-                    for(word <- words){
-                        wordEmbeddingMap.get(word).get
-                        w2i(word).toLong
-                        for (c <- word){
-                            c2i(c).toLong
-                        }
+                    var embeddings:Array[Array[Float]] = new Array[Long](words.length)
+                    var wordIds:Array[Long] = new Array[Long](words.length)
+                    var char_embs:Array[Array[Float]] = new Array[Array[Float]](words.length)
+                    for(i <- words.indices){
+                        val word = words(i)
+                        embeddings(i) = wordEmbeddingMap.get(word).get.toArray
+                        wordIds(i) = w2i(word).toLong
+                        val char_input = new java.util.HashMap[String, OnnxTensor]()
+                        char_input.put("char_ids",  OnnxTensor.createTensor(ortEnvironment, word.map(c => c2i(c.toString).toLong).toArray))
+                        char_embs(i) = session1.run(char_input).get(0).getValue.asInstanceOf[Array[Float]]
                     }
-
-                    // val sc = SeqScorer.f1(goldLabels, preds)
-                    // scoreCountsByLabel.incAll(sc)
+                    val input = new java.util.HashMap[String, OnnxTensor]()
+                    val emb_tensor =  OnnxTensor.createTensor(ortEnvironment, embeddings)
+                    input.put("embed", emb_tensor)
+                    val word_tensor =  OnnxTensor.createTensor(ortEnvironment, wordIds)
+                    input.put("words", word_tensor)
+                    val char_tensor =  OnnxTensor.createTensor(ortEnvironment, char_embs)
+                    input.put("chars", char_tensor)
+                    val emissionScores = session2.run(input)
+                    val labelIds = Utils.greedyPredict(emissionScores)
+                    val preds = labelIds.map(i2t(_))
+                    val sc = SeqScorer.f1(goldLabels, preds)
+                    scoreCountsByLabel.incAll(sc)
                 }
             }
         }
-        // logger.info(s"Accuracy on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.accuracy()}")
-        // logger.info(s"Precision on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.precision()}")
-        // logger.info(s"Recall on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.recall()}")
-        // logger.info(s"Micro F1 on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.f1()}")
-        // for(label <- scoreCountsByLabel.labels) {
-        //     logger.info(s"\tP/R/F1 for label $label (${scoreCountsByLabel.map(label).gold}): ${scoreCountsByLabel.precision(label)} / ${scoreCountsByLabel.recall(label)} / ${scoreCountsByLabel.f1(label)}")
-        // }
+        logger.info(s"Accuracy on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.accuracy()}")
+        logger.info(s"Precision on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.precision()}")
+        logger.info(s"Recall on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.recall()}")
+        logger.info(s"Micro F1 on ${sentences.length} $name sentences for task $taskNumber ($taskName): ${scoreCountsByLabel.f1()}")
+        for(label <- scoreCountsByLabel.labels) {
+            logger.info(s"\tP/R/F1 for label $label (${scoreCountsByLabel.map(label).gold}): ${scoreCountsByLabel.precision(label)} / ${scoreCountsByLabel.recall(label)} / ${scoreCountsByLabel.f1(label)}")
+        }
     }
 
 }
