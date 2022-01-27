b9a8ced1065c2f5b6e1e51a76a29c128447b3881
Zheng Tang <oscartang1120@gmail.com>
2022-01-26 17:42:05 -0700

    Update metal.py

--- main/src/main/python/pytorch/metal.py
+++ main/src/main/python/pytorch/metal.py
@@ -266,8 +266,8 @@ class Metal(object):
 
     def test(self):
 
-        # torch.manual_seed(self.taskManager.random)
-        # random.seed(self.taskManager.random)
+        torch.manual_seed(self.taskManager.random)
+        random.seed(self.taskManager.random)
         for layers in self.model:
             layers.start_eval()
         for taskId in range(0, self.taskManager.taskCount):
