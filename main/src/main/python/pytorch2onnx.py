84d551786cf151b657f07b00539d15403a8c1d4c
Zheng Tang <oscartang1120@gmail.com>
2022-02-02 22:59:56 -0700

    Update pytorch2onnx.py

--- main/src/main/python/pytorch2onnx.py
+++ main/src/main/python/pytorch2onnx.py
@@ -56,6 +56,7 @@ class Saving_Model(torch.nn.Module):
                 state = il(state, False)
             if self.finalLayers[i]:
                 state = self.finalLayers[i](state, None)#headPositions set to be None for now, we can add it in input list later
+        state = self.finalLayers[i].inference(state)
         return state
 
 if __name__ == '__main__':
@@ -195,52 +196,6 @@ if __name__ == '__main__':
     ort_session = onnxruntime.InferenceSession("model.onnx")
     ort_char = onnxruntime.InferenceSession("char.onnx")
     # compute ONNX Runtime output prediction
-
-    for taskId in range(0, taskManager.taskCount):
-        taskName = taskManager.tasks[taskId].taskName
-        sentences = taskManager.tasks[taskId].testSentences
-        if sentences:
-            reader = MetalRowReader()
-            for sent in sentences:
-                annotatedSentences = reader.toAnnotatedSentences(sent)
-
-                for asent in annotatedSentences:
-                    sentence = asent[0]
-                    goldLabels = asent[1]
-
-                    words = sentence.words
-
-                    char_embs = []
-                    for word in words:
-                        char_ids = np.array([c2i.get(c, UNK_EMBEDDING) for c in word])
-                        ort_inputs = {ort_char.get_inputs()[i].name: x for i, x in enumerate([char_ids])}
-                        ort_outs = ort_char.run(None, ort_inputs)
-                        char_embs.append(ort_outs[0])
-                    char_embs = np.stack(char_embs)
-                    embed_ids = torch.LongTensor([constEmbeddings.w2i[word] if word in constEmbeddings.w2i else 0 for word in words])
-                    embeddings = constEmbeddings.emb(embed_ids).detach().cpu().numpy()
-                    word_ids = np.array([w2i[word] if word in w2i else 0 for word in words])
-
-                    dummy_input = (embeddings, word_ids, char_embs)
-
-                    ort_inputs = {ort_session.get_inputs()[i].name: x for i, x in enumerate(dummy_input)}
-                    ort_outs = ort_session.run(None, ort_inputs)
-
-                    emissionScores = ort_outs[0]
-                    preds = [i2t[np.argmax(es)] for es in emissionScores]
-
-                    sc = SeqScorer.f1(goldLabels, preds)
-                    scoreCountsByLabel.incAll(sc)
-
-
-    print (f"Accuracy : {scoreCountsByLabel.accuracy()}")
-    print (f"Precision : {scoreCountsByLabel.precision()}")
-    print (f"Recall on : {scoreCountsByLabel.recall()}")
-    print (f"Micro F1 : {scoreCountsByLabel.f1()}")
-    for label in scoreCountsByLabel.labels():
-        print (f"\tP/R/F1 for label {label} ({scoreCountsByLabel.map[label].gold}): {scoreCountsByLabel.precision(label)} / {scoreCountsByLabel.recall(label)} / {scoreCountsByLabel.f1(label)}")
-    duration = time.time() - start_time
-    print (duration)
 
     ort_inputs = {ort_char.get_inputs()[i].name: to_numpy(x) for i, x in enumerate([char_ids])}
     ort_outs = ort_char.run(None, ort_inputs)
