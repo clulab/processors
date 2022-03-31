1e530b0f9996d2862745de9337972418fd51f5fa
Keith Alcock <github@keithalcock.com>
2022-03-31 14:38:19 -0700

    Make code pretty
    Choose either string interpolation or concatenation but not both

--- corenlp/src/main/scala/org/clulab/processors/examples/DocumentationExample.scala
+++ corenlp/src/main/scala/org/clulab/processors/examples/DocumentationExample.scala
@@ -1,65 +1,64 @@
 package org.clulab.processors.examples
 
 import org.clulab.processors.corenlp.CoreNLPProcessor
-import org.clulab.processors.shallownlp.ShallowNLPProcessor
 import org.clulab.processors.{Document, Processor}
 import org.clulab.struct.DirectedGraphEdgeIterator
 
-class DocumentationExample {
-  // create the processor
-  // any processor works here! Try FastNLPProcessor, or our own CluProcessor
-  val proc:Processor = new CoreNLPProcessor()
+object DocumentationExample extends App {
+  // Create the processor.  Any processor works here!
+  // Try FastNLPProcessor or our own CluProcessor.
+  val proc: Processor = new CoreNLPProcessor()
 
-  // the actual work is done here
-  val doc = proc.annotate("John Smith went to China. He visited Beijing, on January 10th, 2013.")
+  // The actual work is done here.
+  val doc: Document = proc.annotate("John Smith went to China. He visited Beijing on January 10th, 2013.")
 
-  // you are basically done. the rest of this code simply prints out the annotations
+  // You are basically done.  The rest of this code simply prints out the annotations.
 
-  // let's print the sentence-level annotations
-  var sentenceCount = 0
-  for (sentence <- doc.sentences) {
-    println("Sentence #" + sentenceCount + ":")
+  // Let's print the sentence-level annotations.
+  for ((sentence, sentenceIndex) <- doc.sentences.zipWithIndex) {
+    println("Sentence #" + sentenceIndex + ":")
     println("Tokens: " + sentence.words.mkString(" "))
     println("Start character offsets: " + sentence.startOffsets.mkString(" "))
     println("End character offsets: " + sentence.endOffsets.mkString(" "))
 
-    // these annotations are optional, so they are stored using Option objects, hence the foreach statement
-    sentence.lemmas.foreach(lemmas => println(s"Lemmas: ${lemmas.mkString(" ")}"))
-    sentence.tags.foreach(tags => println(s"POS tags: ${tags.mkString(" ")}"))
-    sentence.chunks.foreach(chunks => println(s"Chunks: ${chunks.mkString(" ")}"))
-    sentence.entities.foreach(entities => println(s"Named entities: ${entities.mkString(" ")}"))
-    sentence.norms.foreach(norms => println(s"Normalized entities: ${norms.mkString(" ")}"))
-    sentence.dependencies.foreach(dependencies => {
+    // These annotations are optional, so they are stored using Option objects,
+    // hence the foreach statement.
+    sentence.lemmas.foreach(lemmas => println("Lemmas: " + lemmas.mkString(" ")))
+    sentence.tags.foreach(tags => println("POS tags: " + tags.mkString(" ")))
+    sentence.chunks.foreach(chunks => println("Chunks: " + chunks.mkString(" ")))
+    sentence.entities.foreach(entities => println("Named entities: " + entities.mkString(" ")))
+    sentence.norms.foreach(norms => println("Normalized entities: " + norms.mkString(" ")))
+    sentence.dependencies.foreach { dependencies =>
       println("Syntactic dependencies:")
       val iterator = new DirectedGraphEdgeIterator[String](dependencies)
-      while(iterator.hasNext) {
-        val dep = iterator.next
-        // note that we use offsets starting at 0 (unlike CoreNLP, which uses offsets starting at 1)
-        println(" head:" + dep._1 + " modifier:" + dep._2 + " label:" + dep._3)
+      iterator.foreach { dep =>
+        // Note that we use offsets starting at 0 unlike CoreNLP, which uses offsets starting at 1.
+        println(" head: " + dep._1 + " modifier: " + dep._2 + " label: " + dep._3)
       }
-    })
-    sentence.syntacticTree.foreach(tree => {
-      println("Constituent tree: " + tree)
-      // see the org.clulab.utils.Tree class for more information
-      // on syntactic trees, including access to head phrases/words
-    })
-
-    sentenceCount += 1
-    println("\n")
+    }
+    sentence.syntacticTree.foreach { syntacticTree =>
+      // See the org.clulab.utils.Tree class for more information
+      // on syntactic trees, including access to head phrases/words.
+      println("Constituent tree: " + syntacticTree)
+    }
+    println()
+    println()
   }
 
-  // let's print the coreference chains
-  doc.coreferenceChains.foreach(chains => {
+  // Let's print the coreference chains.
+  doc.coreferenceChains.foreach { chains =>
     for (chain <- chains.getChains) {
       println("Found one coreference chain containing the following mentions:")
       for (mention <- chain) {
-        // note that all these offsets start at 0 too
-        println("\tsentenceIndex:" + mention.sentenceIndex +
-          " headIndex:" + mention.headIndex +
-          " startTokenOffset:" + mention.startOffset +
-          " endTokenOffset:" + mention.endOffset +
-          " text: " + doc.sentences(mention.sentenceIndex).words.slice(mention.startOffset, mention.endOffset).mkString("[", " ", "]"))
+        val text = doc.sentences(mention.sentenceIndex).words
+            .slice(mention.startOffset, mention.endOffset).mkString("[", " ", "]")
+        // Note that all these offsets start at 0, too.
+        println("\tsentenceIndex: " + mention.sentenceIndex +
+            " headIndex: " + mention.headIndex +
+            " startTokenOffset: " + mention.startOffset +
+            " endTokenOffset: " + mention.endOffset +
+            " text: " + text)
       }
     }
-  })
+  }
 }
