ec0f27aaa23424e9a73ae627a1ba20571b28a0cc
Zheng Tang <oscartang1120@gmail.com>
2022-04-27 22:43:27 -0700

    Update eisner.py

--- main/src/main/python/pytorch/eisner.py
+++ main/src/main/python/pytorch/eisner.py
@@ -50,6 +50,7 @@ class Span:
     def apply(cls, left, right, dep, head):
         # product of probabilities, in log space
         if(dep is not None and dep.score != 0):
+            print (dep.score)
             score = left.score + right.score + float(math.log(dep.score))
         else:
             score = left.score + right.score
