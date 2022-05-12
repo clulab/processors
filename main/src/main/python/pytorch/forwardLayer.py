f33fc48a0639229c2073c4356b03e349be3b4469
Zheng Tang <oscartang1120@gmail.com>
2022-05-12 12:14:29 -0700

    Update forwardLayer.py

--- main/src/main/python/pytorch/forwardLayer.py
+++ main/src/main/python/pytorch/forwardLayer.py
@@ -24,7 +24,9 @@ class ForwardLayer(FinalLayer):
         self.inDim = inputSize
         self.outDim = len(t2i)
 
-        self.distanceLookupParameters = None#nn.Embedding(101, distanceEmbeddingSize) if distanceEmbeddingSize > 0 else None
+        self.distanceEmbeddingSize = distanceEmbeddingSize
+
+        self.distanceLookupParameters = nn.Embedding(101, distanceEmbeddingSize) if distanceEmbeddingSize > 0 else None
 
 
     def forward(self, inputExpressions, modHeadPairs):
