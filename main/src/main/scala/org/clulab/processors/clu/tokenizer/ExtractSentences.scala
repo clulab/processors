fa7dad8d3db959f31cd57e1d00657e81e3d35ee4
Keith Alcock <github@keithalcock.com>
2022-11-22 08:12:02 -0700

    Collect example sentences

---
+++ main/src/main/scala/org/clulab/processors/clu/tokenizer/ExtractSentences.scala
@@ -0,0 +1,30 @@
+package org.clulab.processors.clu.tokenizer
+
+import org.clulab.processors.clu.CluProcessor
+import org.clulab.utils.Closer.AutoCloser
+import org.clulab.utils.FileUtils
+
+object ExtractSentences extends App {
+  val directoryName = args.lift(0).getOrElse("../corpora/Doc16k/txt")
+  val fileName = args.lift(1).getOrElse("sentences.txt")
+
+  val files = FileUtils.findFiles(directoryName, ".txt")
+  val processor = new CluProcessor()
+  var count = 0
+
+  FileUtils.printWriterFromFile(fileName).autoClose { printWriter =>
+    files.foreach { file =>
+      val text = FileUtils.getTextFromFile(file)
+      val document = processor.mkDocument(text, keepText = true)
+
+      document.sentences.foreach { sentence =>
+        val words = sentence.words.mkString(" ")
+
+        printWriter.println(words)
+        count += 1
+        if (count >= 10000)
+          throw new RuntimeException("There are too many sentences!")
+      }
+    }
+  }
+}
