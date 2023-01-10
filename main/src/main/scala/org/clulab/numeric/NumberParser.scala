20ac74ee97dbad95f90a7acf96086f6169fdcf3a
Mihai Surdeanu <surdeanu@gmail.com>
2023-01-09 17:27:12 -0700

    added "dozen" to number parser

--- main/src/main/scala/org/clulab/numeric/NumberParser.scala
+++ main/src/main/scala/org/clulab/numeric/NumberParser.scala
@@ -139,6 +139,7 @@ object NumberParser {
     "ten"         -> 10,
     "eleven"      -> 11,
     "twelve"      -> 12,
+    "dozen"       -> 12,
     "thirteen"    -> 13,
     "fourteen"    -> 14,
     "fifteen"     -> 15,
