e296a1e677f7a06bbc1bd58153733b30c0c446ab
Keith Alcock <github@keithalcock.com>
2023-01-18 14:55:13 -0700

    Complete test

--- main/src/test/scala/org/clulab/utils/TestFindHeads.scala
+++ main/src/test/scala/org/clulab/utils/TestFindHeads.scala
@@ -1,26 +1,124 @@
 package org.clulab.utils
 
 import org.clulab.processors.Sentence
-import org.clulab.serialization.json.JSONSerializer
+import org.clulab.struct.GraphMap.UNIVERSAL_BASIC
 import org.clulab.struct.{DirectedGraph, Edge, Interval}
-
-import java.io.File
 
 class TestFindHeads extends Test {
 
-  def newSentence(words: Seq[String]): Sentence = ???
+  def newSentence(words: Array[String], directedGraph: DirectedGraph[String]): Sentence = {
+    val startOffsets = Array(0) // unused
+    val   endOffsets = Array(0) // unused
+    val sentence = new Sentence(words, startOffsets, endOffsets, words)
+
+    sentence.graphs(UNIVERSAL_BASIC) = directedGraph
+    sentence.tags = Some(words)
+    sentence
+  }
 
   behavior of "DirectedGraph"
 
   it should "not suffer from issue #134" in {
-    println(new File(".").getAbsolutePath)
-    val file = new File("./main/src/test/resources/PMC3747764.json")
-    val document = JSONSerializer.toDocument(file)
-    val sentence = document.sentences(679)
-    // this sentence has a single root (token 1)
-    val roots = sentence.dependencies.get.roots
-    val tokenInterval = Interval(0, sentence.size)
-    // SPOILER: this fails
-    DependencyUtils.findHeadsStrict(tokenInterval, sentence)
+    val edges = List(
+      Edge( 1,  0, ""),
+      Edge( 1,  4, ""),
+      Edge( 1,  7, ""),
+      Edge( 1, 77, ""),
+
+      Edge( 4,  2, ""),
+      Edge( 4,  3, ""),
+
+      Edge( 5,  5, ""),
+      Edge( 5,  5, ""),
+      Edge( 5,  5, ""),
+      Edge( 5,  8, ""),
+      Edge( 5, 12, ""),
+      Edge( 5, 13, ""),
+      Edge( 5, 17, ""),
+      Edge( 5, 19, ""),
+      Edge( 5, 22, ""),
+      Edge( 5, 23, ""),
+      Edge( 5, 25, ""),
+
+      Edge( 7,  6, ""),
+
+      Edge(12, 10, ""),
+      Edge(12, 11, ""),
+
+      Edge(17, 15, ""),
+      Edge(17, 16, ""),
+
+      Edge(22, 21, ""),
+
+      Edge(25, 30, ""),
+      Edge(25, 32, ""),
+
+      Edge(28, 27, ""),
+
+      Edge(30, 28, ""),
+      Edge(30, 29, ""),
+      Edge(30, 32, ""),
+
+      Edge(32, 33, ""),
+
+      Edge(33, 36, ""),
+
+      Edge(36, 34, ""),
+      Edge(36, 35, ""),
+      Edge(36, 38, ""),
+      Edge(36, 45, ""),
+
+      Edge(38, 37, ""),
+      Edge(38, 42, ""),
+
+      Edge(42, 40, ""),
+      Edge(42, 41, ""),
+
+      Edge(45, 44, ""),
+      Edge(45, 46, ""),
+      Edge(45, 50, ""),
+      Edge(45, 54, ""),
+      Edge(45, 65, ""),
+
+      Edge(50, 49, ""),
+      Edge(50, 51, ""),
+      Edge(50, 54, ""),
+      Edge(50, 62, ""),
+      Edge(50, 65, ""),
+
+      Edge(54, 52, ""),
+      Edge(54, 53, ""),
+      Edge(54, 55, ""),
+
+      Edge(55, 57, ""),
+
+      Edge(57, 61, ""),
+
+      Edge(61, 59, ""),
+      Edge(61, 60, ""),
+
+      Edge(65, 64, ""),
+      Edge(65, 67, ""),
+
+      Edge(67, 66, ""),
+      Edge(67, 68, ""),
+      Edge(67, 69, ""),
+      Edge(67, 70, ""),
+      Edge(67, 71, ""),
+      Edge(67, 72, ""),
+      Edge(67, 75, ""),
+      Edge(67, 76, ""),
+
+      Edge(75, 73, ""),
+      Edge(75, 74, "")
+    )
+    val len: Int = 78
+    val directedGraph = DirectedGraph(edges)
+    val tokenInterval = Interval(0, len)
+    val words = 1.to(len).map { index => s"word$index" }.toArray
+    val sentence = newSentence(words, directedGraph)
+    val heads = DependencyUtils.findHeadsStrict(tokenInterval, sentence)
+
+    println(heads)
   }
 }
