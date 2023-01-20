512ca12c8e5b201a71af10acb1f13b5c34bf58e7
Keith Alcock <github@keithalcock.com>
2023-01-19 18:48:11 -0700

    Cross=compile

--- main/src/main/scala/org/clulab/utils/PrintUtils.scala
+++ main/src/main/scala/org/clulab/utils/PrintUtils.scala
@@ -42,35 +42,35 @@ object PrintUtils {
     }
 
     def print(): Unit = asArrayOpt(any)
-        .map(_.toIterable.print(arrayStart, arraySep, arrayEnd))
+        .map(_.view.print(arrayStart, arraySep, arrayEnd))
         .getOrElse(System.out.print(any))
     def print(sep: String): Unit = print(empty, sep, empty)
     def print(start: String, sep: String, end: String): Unit =
         print(System.out, start, sep, end)
 
     def print(printing: Printing): Unit = asArrayOpt(any)
-        .map(_.toIterable.print(printing, arrayStart, arraySep, arrayEnd))
+        .map(_.view.print(printing, arrayStart, arraySep, arrayEnd))
         .getOrElse(printing.print(any))
     def print(printing: Printing, sep: String): Unit = print(printing, empty, sep, empty)
     def print(printing: Printing, start: String, sep: String, end: String): Unit =
         execute(printing, start, sep, end, newline = false)
 
     def println(): Unit = asArrayOpt(any)
-        .map(_.toIterable.println(arrayStart, arraySep, arrayEnd))
+        .map(_.view.println(arrayStart, arraySep, arrayEnd))
         .getOrElse(System.out.println(any))
     def println(sep: String): Unit = println(empty, sep, empty)
     def println(start: String, sep: String, end: String): Unit =
         println(System.out, start, sep, end)
 
     def println(printing: Printing): Unit = asArrayOpt(any)
-        .map(_.toIterable.println(printing, arrayStart, arraySep, arrayEnd))
+        .map(_.view.println(printing, arrayStart, arraySep, arrayEnd))
         .getOrElse(printing.println(any))
     def println(printing: Printing, sep: String): Unit = println(printing, empty, sep, empty)
     def println(printing: Printing, start: String, sep: String, end: String): Unit =
         execute(printing, start, sep, end, newline = true)
 
     protected def execute(printing: Printing, start: String, sep: String, end: String, newline: Boolean): Unit = {
-      val patchedAny = asArrayOpt(any).map(_.toIterable).getOrElse(any)
+      val patchedAny = asArrayOpt(any).map(_.view).getOrElse(any)
 
       if (start.nonEmpty) printing.print(start)
       patchedAny match {
