b6e0360655ec2fb82f3258d102c734b5e2f0c29a
Keith Alcock <github@keithalcock.com>
2023-01-20 19:21:32 -0700

    Make classes

--- main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepContractions.scala
+++ main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepContractions.scala
@@ -5,55 +5,69 @@ import scala.collection.mutable.ArrayBuffer
 
 class TokenizerStepContractions extends TokenizerStep {
 
-  // Full won't
-  // Conditional cont'd
+  abstract class Contraction(pattern: Pattern, length: Int) {
 
-  class Contraction(pattern: Pattern, length: Int, leftOpt: Option[String], rightOpt: Option[String]) {
+    def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit
+
+    def splitRight(string: String, n: Int): (String, String) = {
+      val split = string.length - n
+
+      (string.substring(0, split), string.substring(split))
+    }
 
     def matches(rawToken: RawToken): Boolean =
         // Check the length before the expensive match.
         length <= rawToken.raw.length && pattern.matcher(rawToken.raw).find
 
-    def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
-      val raw = rawToken.raw
-      val rawLength = raw.length
-
-      val leftRaw = raw.substring(0, rawLength - length)
-      val rightRaw = raw.substring(rawLength - length)
-
-      val leftWord = leftOpt.getOrElse(leftRaw)
-      val rightWord = rightOpt.getOrElse(rightRaw)
-
-//      if (leftRaw.nonEmpty)
-        tokens += RawToken(leftRaw, rawToken.beginPosition, leftWord)
-      tokens += RawToken(rightRaw, rawToken.beginPosition + rawLength - length, rightWord)
-    }
-
-    def expand(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
-      val raw = rawToken.raw
-
-      if (length == raw.length && raw != "won't")
-        tokens += RawToken(raw, rawToken.beginPosition, rightOpt.getOrElse(raw))
-      else
+    def expand(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit =
         split(rawToken, tokens)
-      true
+  }
+
+  // Match from the right but expand neither left nor right side while still separating them.
+  class NeitherContraction(pattern: Pattern, length: Int) extends Contraction(pattern, length) {
+
+    override def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
+      val (leftRaw, rightRaw) = splitRight(rawToken.raw, length)
+
+      if (leftRaw.nonEmpty)
+        tokens += RawToken(leftRaw, rawToken.beginPosition)
+      tokens += RawToken(rightRaw, rawToken.beginPosition + leftRaw.length)
     }
   }
 
-  // Both has both sides, matches from start to end of word
-  // Neither has no sides, matches from right
-  // Right, matches from right
-  // add exceptions, matches but don't apply
+  // Perform a full match and use the left and right words.  This is mostly for exceptions to
+  // other rules so that they should come first.
+  class BothContraction(pattern: Pattern, length: Int, leftWord: String, rightWord: String) extends Contraction(pattern, length) {
+
+    override def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
+      val (leftRaw, rightRaw) = splitRight(rawToken.raw, length)
+
+      tokens += RawToken(leftRaw, rawToken.beginPosition, leftWord)
+      tokens += RawToken(rightRaw, rawToken.beginPosition + leftRaw.length, rightWord)
+    }
+  }
+
+  // Match again from the right and expand only the right side, using the remainder (if any) for the left.
+  class RightContraction(pattern: Pattern, length: Int, rightWord: String) extends Contraction(pattern, length) {
+
+    override def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
+      val (leftRaw, rightRaw) = splitRight(rawToken.raw, length)
+
+      if (leftRaw.nonEmpty)
+        tokens += RawToken(leftRaw, rawToken.beginPosition)
+      tokens += RawToken(rightRaw, rawToken.beginPosition + leftRaw.length, rightWord)
+    }
+  }
 
   def toPattern(template: String): Pattern = template.r.pattern
 
   val contractions = Array(
-    new Contraction(toPattern("^[wW][oO][nN]'[tT]$"), 5, Some("will"), Some("not")), // won't -> will not
-    new Contraction(toPattern("'[sS]$"),              2, None,         None),        // person's -> person 's
-    new Contraction(toPattern("[nN]'[tT]$"),          3, None,         Some("not")), // don't -> do not
-    new Contraction(toPattern("'[mM]$"),              2, None,         Some("am")),  // I'm -> I am
-    new Contraction(toPattern("'[dD]$"),              2, None,         None),        // he'd -> he 'd
-    new Contraction(toPattern("'[lL][lL]$"),          3, None,         Some("will")) // she'll -> she will
+    new    BothContraction(toPattern("^[wW][oO][nN]'[tT]$"), 5, "will", "not"), // won't -> will not
+    new NeitherContraction(toPattern("'[sS]$"),              2),                // person's -> person 's
+    new   RightContraction(toPattern("[nN]'[tT]$"),          3, "not"),         // don't -> do not
+    new   RightContraction(toPattern("'[mM]$"),              2, "am"),          // I'm -> I am
+    new NeitherContraction(toPattern("'[dD]$"),              2),                // he'd -> he 'd
+    new   RightContraction(toPattern("'[lL][lL]$"),          3, "will")         // she'll -> she will
   )
 
   // Unlike CoreNLP, we allow single quotes inside words
