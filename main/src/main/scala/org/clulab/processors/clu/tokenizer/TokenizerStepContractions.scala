a9f4e4d169d28f70a9d0cfc31762ad8dfacf2cf0
Keith Alcock <github@keithalcock.com>
2023-01-20 18:19:19 -0700

    Make it work once

--- main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepContractions.scala
+++ main/src/main/scala/org/clulab/processors/clu/tokenizer/TokenizerStepContractions.scala
@@ -4,74 +4,82 @@ import java.util.regex.Pattern
 import scala.collection.mutable.ArrayBuffer
 
 class TokenizerStepContractions extends TokenizerStep {
-  // Regex.pattern is a val stored inside this class, so accessing it is cheap.
-  def toPattern(string: String): Pattern = string.r.pattern
+
+  // Full won't
+  // Conditional cont'd
+
+  class Contraction(pattern: Pattern, length: Int, leftOpt: Option[String], rightOpt: Option[String]) {
+
+    def matches(rawToken: RawToken): Boolean =
+        // Check the length before the expensive match.
+        length <= rawToken.raw.length && pattern.matcher(rawToken.raw).find
+
+    def split(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
+      val raw = rawToken.raw
+      val rawLength = raw.length
+
+      val leftRaw = raw.substring(0, rawLength - length)
+      val rightRaw = raw.substring(rawLength - length)
+
+      val leftWord = leftOpt.getOrElse(leftRaw)
+      val rightWord = rightOpt.getOrElse(rightRaw)
+
+//      if (leftRaw.nonEmpty)
+        tokens += RawToken(leftRaw, rawToken.beginPosition, leftWord)
+      tokens += RawToken(rightRaw, rawToken.beginPosition + rawLength - length, rightWord)
+    }
+
+    def expand(rawToken: RawToken, tokens: ArrayBuffer[RawToken]): Boolean = {
+      val raw = rawToken.raw
+
+      if (length == raw.length && raw != "won't")
+        tokens += RawToken(raw, rawToken.beginPosition, rightOpt.getOrElse(raw))
+      else
+        split(rawToken, tokens)
+      true
+    }
+  }
+
+  object Contraction {
+
+    def apply(template: String, length: Int, leftOpt: Option[String], rightOpt: Option[String]): Contraction =
+      new Contraction(template.r.pattern, length, leftOpt, rightOpt)
+  }
 
   // TODO: Pre-calculate length of string needed to match it.
-  private val WON_T = toPattern("""^[wW][oO][nN]'[tT]$""") // won't -> will not
-  private val    _S = toPattern("""'[sS]$""")              // person's -> he is
-  private val   N_T = toPattern("""[nN]'[tT]$""")          // don't -> do not
-  private val    _M = toPattern("""'[mM]$""")              // I'm -> I am
-  private val    _D = toPattern("""'[dD]$""")              // he'd -> he would or he had
-  private val   _LL = toPattern("""'[lL][lL]$""")          // he'll -> he will
+  private val WON_T = Contraction("""^[wW][oO][nN]'[tT]$""", 5, Some("will"), Some("not"))  // won't -> will not
+  private val    _S = Contraction("""'[sS]$""",              2, None,         None)         // person's -> he is
+  private val   N_T = Contraction("""[nN]'[tT]$""",          3, None,         Some("not"))  // don't -> do not
+  private val    _M = Contraction("""'[mM]$""",              2, None,         Some("am"))   // I'm -> I am
+  private val    _D = Contraction("""'[dD]$""",              2, None,         None)         // he'd -> he would or he had
+  private val   _LL = Contraction("""'[lL][lL]$""",          3, None,         Some("will")) // he'll -> he will
 
   override def process(inputs: Array[RawToken]): Array[RawToken] = {
     val output = new ArrayBuffer[RawToken]()
+    // TODO: Add the output to function so that don't have to pass it?
 
     inputs.foreach { input =>
       // An apostrophe heralds all contractions, so resort to expensive
       // regular expressions only after the cheap apostrophe detector sounds.
-      if (!input.raw.contains('\''))
+      if (!input.raw.contains('\'') || !process(input, output))
         output += input
-      else process(input, output)
     }
     output.toArray
   }
 
   // Unlike CoreNLP, we allow single quotes inside words
   // We must separate important linguistic constructs here
-  protected def process(input: RawToken, tokens: ArrayBuffer[RawToken]): Unit = {
-    val raw = input.raw
-    val rawLength = raw.length
-
-    def splitRight(n: Int, leftWordOpt: Option[String] = None, rightWordOpt: Option[String] = None): Unit = {
-      val leftRaw = raw.substring(0, rawLength - n)
-      val rightRaw = raw.substring(rawLength - n)
-
-      val leftWord = leftWordOpt.getOrElse(leftRaw)
-      val rightWord = rightWordOpt.getOrElse(rightRaw)
-
-      if (leftRaw.nonEmpty)
-        tokens += RawToken(leftRaw, input.beginPosition, leftWord)
-      tokens += RawToken(rightRaw, input.beginPosition + rawLength - n, rightWord)
+  protected def process(input: RawToken, tokens: ArrayBuffer[RawToken]): Boolean = {
+    if (false) true
+    else if (_S.matches(input)) _S.expand(input, tokens)
+    else if (WON_T.matches(input)) WON_T.expand(input, tokens)
+    else if (N_T.matches(input)) N_T.expand(input, tokens)
+    else if (_M.matches(input)) _M.expand(input, tokens)
+    else if (_D.matches(input)) {
+      if (input.raw.toLowerCase != "cont'd") _D.expand(input, tokens)
+      else false
     }
-
-    // genitive
-    if (_S.matcher(raw).find) {
-      // TODO: can we detect if genitive or "is" here?
-      if (rawLength > 2) splitRight(2)
-      else tokens += input
-    }
-    else if (WON_T.matcher(raw).find) {
-      splitRight(2, Some("will"), Some("not"))
-    }
-    else if (N_T.matcher(raw).find) {
-      if (rawLength > 3) splitRight(3, None, Some("not"))
-      else tokens += RawToken(raw, input.beginPosition, "not") // n't -> not
-    }
-    else if (_M.matcher(raw).find) {
-      if (rawLength > 2) splitRight(2, None, Some("am"))
-      else tokens += RawToken(raw, input.beginPosition, "am") // 'm -> am
-    }
-    else if (_D.matcher(raw).find) {
-      // TODO: can we detect if "would" or "had" here?
-      if (rawLength > 2 && raw.toLowerCase != "cont'd") splitRight(2)
-      else tokens += input // 'd -> 'd
-    }
-    else if (_LL.matcher(raw).find) {
-      if (rawLength > 3) splitRight(3, None, Some("will"))
-      else tokens += RawToken(raw, input.beginPosition, "will") // 'll -> will
-    }
-    else tokens += input
+    else if (_LL.matches(input)) _LL.expand(input, tokens)
+    else false
   }
 }
