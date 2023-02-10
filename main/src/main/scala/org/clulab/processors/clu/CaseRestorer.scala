a0f587d5904e714e6d7a900f39c519a7c5ed4830
Mihai Surdeanu <surdeanu@gmail.com>
2023-02-10 13:17:58 -0700

    adding ner

--- main/src/main/scala/org/clulab/processors/clu/CaseRestorer.scala
+++ main/src/main/scala/org/clulab/processors/clu/CaseRestorer.scala
@@ -2,10 +2,13 @@ package org.clulab.processors.clu
 
 object CaseRestorer extends App {
   val proc = new CluProcessor()
-  //val doc = proc.mkDocument("mr. scruggs -- who is arguing the case with his son zach")
-  val doc = proc.mkDocumentFromTokens("<YOUR ARRAY OF TOKENS>")
+  val doc = proc.mkDocument("mr. scruggs -- who is arguing the case with his son zach")
+  // tokenize using white spaces, then call method below
+  //val doc = proc.mkDocumentFromTokens("<YOUR ARRAY OF TOKENS>")
   proc.restoreCase(doc)
+  proc.annotate(doc)
   for(sent <- doc.sentences) {
     println(s"${sent.words.mkString(" ")}")
+    println(s"${sent.entities.get.mkString(" ")}")
   }
 }
