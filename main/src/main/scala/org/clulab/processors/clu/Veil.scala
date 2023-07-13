3f2ca9abf87423aa543bdf859a58dcd25d92584b
Keith Alcock <github@keithalcock.com>
2023-07-13 11:39:58 -0700

    Show document type

--- main/src/main/scala/org/clulab/processors/clu/Veil.scala
+++ main/src/main/scala/org/clulab/processors/clu/Veil.scala
@@ -211,7 +211,7 @@ object VeilApp extends App {
     val text = "To be loved by unicorns is the greatest gift of all (Hahn-Powell, 2012)."
     val veiledLetters = Seq(Range.inclusive(text.indexOf('('), text.indexOf(')')))
     val veiledText = new VeiledText(text, veiledLetters)
-    val document = veiledText.mkDocument(processor)
+    val document: Document = veiledText.mkDocument(processor)
 
     Using.resource(new PrintWriter("veiledLetters.out")) { printWriter =>
       val documentSerializer = new DocumentSerializer()
@@ -225,10 +225,10 @@ object VeilApp extends App {
     */
   def veilDocument(processor: Processor): Unit = {
     val text = "To be loved by unicorns is the greatest gift of all (Hahn-Powell, 2012)."
-    val document = processor.mkDocument(text)
+    val document: Document = processor.mkDocument(text)
     val veiledWords = Seq((0, Range.inclusive(document.sentences(0).raw.indexOf("("), document.sentences(0).raw.indexOf(")"))))
     val veiledDocument = new VeiledDocument(document, veiledWords)
-    val annotatedDocument = veiledDocument.annotate(processor)
+    val annotatedDocument: Document = veiledDocument.annotate(processor)
 
     Using.resource(new PrintWriter("veiledWords.out")) { printWriter =>
       val documentSerializer = new DocumentSerializer()
