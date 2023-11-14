21c02576164e22b97ac1415db5360e632fb7de3a
Keith Alcock <github@keithalcock.com>
2023-11-13 18:18:35 -0700

    Add variance for Lazy class

--- main/src/main/scala/org/clulab/utils/Lazy.scala
+++ main/src/main/scala/org/clulab/utils/Lazy.scala
@@ -2,7 +2,7 @@ package org.clulab.utils
 
 // This works around the impossibility of Array[(=> T)].
 // Use instead Array[Lazy[T]].
-class Lazy[T](_value: => T) {
+class Lazy[+T](_value: => T) {
 
   lazy val value: T = _value
 }
