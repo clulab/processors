67d8af9a5aca3e8c3b7ecaff5ad717c2f69feb1e
Keith Alcock <github@keithalcock.com>
2024-01-03 11:39:45 -0700

    Clean up

--- main/src/main/scala/org/clulab/numeric/HolidayNormalizer.scala
+++ main/src/main/scala/org/clulab/numeric/HolidayNormalizer.scala
@@ -1,13 +1,13 @@
 package org.clulab.numeric
 
-import de.jollyday.{Holiday, HolidayCalendar, HolidayManager, ManagerParameters}
+import de.jollyday.{HolidayCalendar, HolidayManager, ManagerParameters}
 
 import java.time.LocalDate
 import scala.jdk.CollectionConverters._
 
 object HolidayNormalizer {
-  private val normMapper: Map[String, NormAndUnitClass] = UnitNormalizer.readNormsFromResource("/org/clulab/numeric/HOLIDAY.tsv")
-  private val holidayManager = HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.UNITED_STATES))
+  protected val normMapper: Map[String, NormAndUnitClass] = UnitNormalizer.readNormsFromResource("/org/clulab/numeric/HOLIDAY.tsv")
+  protected val holidayManager = HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.UNITED_STATES))
   // Get keys for states in case there is ever a state-specific holiday.
   protected val stateKeys: Array[String] = holidayManager.getCalendarHierarchy.getChildren.keySet.asScala.toArray
 
@@ -21,7 +21,6 @@ object HolidayNormalizer {
       // If year is None use current year as default.  This can lead to problems if the year is never filled in!
       // Do not run this code around around December 31 or January 1 because your results might be inconsistent.
       val year = yearOpt.map(_.mkString.toInt).getOrElse(LocalDate.now.getYear)
-      // Get holidays from jollyday for the U.S. and all the states.
       val generalHolidayOpt = holidayManager.getHolidays(year).asScala
           .filter(_.getDescription == holidayCanonical).headOption
       val holidayOpt = generalHolidayOpt.orElse {
