cf0e62312a0ef1476432b0ccd871be5dd4c3294a
Keith Alcock <github@keithalcock.com>
2024-01-08 09:20:19 -0700

    Clean up normalizer

--- main/src/main/scala/org/clulab/numeric/HolidayNormalizer.scala
+++ main/src/main/scala/org/clulab/numeric/HolidayNormalizer.scala
@@ -1,7 +1,5 @@
 package org.clulab.numeric
 
-import de.jollyday.config.Configuration
-import de.jollyday.impl.DefaultHolidayManager
 import de.jollyday.parameter.UrlManagerParameter
 import de.jollyday.{HolidayCalendar, HolidayManager, ManagerParameters}
 
@@ -11,37 +9,20 @@ import java.time.LocalDate
 import java.util.Properties
 import scala.jdk.CollectionConverters._
 
-class CluXmlManager extends DefaultHolidayManager {
-  println("I got here!")
-
-  // This exposes the otherwise protected configuration.
-  def getConfiguration(): Configuration = configuration
-}
-
 object HolidayNormalizer {
   protected val normMapper: Map[String, NormAndUnitClass] = UnitNormalizer.readNormsFromResource("/org/clulab/numeric/HOLIDAY.tsv")
-  protected val holidayManager =
-      if (true) {
-        val managerProps = {
-          val properties = new Properties()
+  protected val holidayManager = {
+    val xmlPath = "/home/kwa/Projects/clulab/processors-project/processors/main/src/main/resources/org/clulab/numeric/CluHolidays_sutime.xml"
+    if (!new File(xmlPath).exists)
+      println("It is wrong!")
 
-          properties.setProperty("manager.impl", "org.clulab.numeric.CluXmlManager")
-          properties
-        }
-        val xmlPath = "/home/kwa/Projects/clulab/processors-project/processors/main/src/main/resources/CluHolidays_sutime.xml"
-        if (!new File(xmlPath).exists)
-          println("It is wrong!")
-        val holidayXmlUrl = new URL("file://" + xmlPath)
-        val urlManagerParameter = new UrlManagerParameter(holidayXmlUrl, managerProps)
-        val holidayManager = HolidayManager.getInstance(urlManagerParameter)
+    // Get the resource as url, check the protocol or something
+    val holidayXmlUrl = new URL("file://" + xmlPath)
+    val urlManagerParameter = new UrlManagerParameter(holidayXmlUrl, new Properties())
+    val holidayManager = HolidayManager.getInstance(urlManagerParameter)
 
-        holidayManager
-      }
-      else {
-        val holidayManager = HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.UNITED_STATES))
-
-        holidayManager
-      }
+    holidayManager
+  }
 
   /** Retrieves date (day and month) for a holiday */
   def norm(holidaySeq: Seq[String], yearOpt: Option [Seq[String]]): Option[(String, String)] = {
@@ -56,7 +37,8 @@ object HolidayNormalizer {
       val allHolidays = holidayManager
           .getHolidays(year).asScala
       val holidayOpt = allHolidays
-          .filter(_.getPropertiesKey == holidayCanonical).headOption
+          .filter(_.getPropertiesKey == holidayCanonical)
+          .headOption
       val normOpt = holidayOpt.map { holiday =>
         val date = holiday.getDate
 
