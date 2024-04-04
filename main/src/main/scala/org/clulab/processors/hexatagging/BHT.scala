e26554891829289e098af6de6293285de989faf4
Mihai Surdeanu <surdeanu@gmail.com>
2024-04-03 18:06:09 -0700

    More hexa tags

--- main/src/main/scala/org/clulab/processors/hexatagging/BHT.scala
+++ main/src/main/scala/org/clulab/processors/hexatagging/BHT.scala
@@ -3,6 +3,9 @@ package org.clulab.processors.hexatagging
 trait BHT {
   def isTerminal(): Boolean
 
+  def rightMostLeft:Int
+  def leftMostRight:Int
+
   protected def addOffset(offset: Int): String = {
     val sb = new StringBuilder()
     for(i <- 0 until offset) sb.append(' ')
@@ -17,13 +20,21 @@ trait BHT {
 class TerminalBHT(node: Int, label: String) extends BHT {
   override def isTerminal(): Boolean = true
   override def toString(offset: Int): String = s"${addOffset(offset)}($node, $label)\n"
+
+  override def leftMostRight:Int = node
+  override def rightMostLeft: Int = node
 }
 
 class NonTerminalBHT(label: String, left: BHT, right: BHT) extends BHT {
+
+  override def rightMostLeft: Int = left.leftMostRight
+
+  override def leftMostRight: Int = right.rightMostLeft
+
   override def isTerminal(): Boolean = false
   override def toString(offset: Int): String = {
     val sb = new StringBuilder()
-    sb.append(s"${addOffset(offset)}$label\n")
+    sb.append(s"${addOffset(offset)}$label ($rightMostLeft, $leftMostRight)\n")
     sb.append(left.toString(offset + 2))
     sb.append(right.toString(offset + 2))
     sb.toString()
--- main/src/main/scala/org/clulab/processors/hexatagging/DepsToTags.scala
+++ main/src/main/scala/org/clulab/processors/hexatagging/DepsToTags.scala
@@ -2,20 +2,39 @@ package org.clulab.processors.hexatagging
 
 import org.clulab.sequences.ColumnReader
 import scala.collection.mutable.Stack
+import org.clulab.sequences.Row
 
 object DepsToTags {
+  def generateHexaTags(sent: Array[Row]): Unit = {
+    val depTree = DependencyTree.toTree(sent)
+    val stack = new Stack[BHT]
+    depTree.toBHT(stack, "root")
+    val bht = stack.pop()
+    val (termTags, nonTermTags) = assignHexaTags(bht, sent.length)
+    
+    println(depTree)
+    println(bht)
+    println("termTags = " + termTags.mkString(", "))
+    println("nonTermTags = " + nonTermTags.mkString(", "))
+  }
+
+  def assignHexaTags(bht: BHT, length: Int): (Array[String], Array[String]) = {
+    val termTags = new Array[String](length)
+    val nonTermTags = new Array[String](length)
+    nonTermTags(length - 1) = "eos"
+
+    // TODO!
+
+    (termTags, nonTermTags)
+  }
+
   def main(args: Array[String]): Unit = {
     val depsFileName = "dynet/en/deps/universal/wsj/train.labels"
     val sents = ColumnReader.readColumns(depsFileName)
     println(s"Read ${sents.length} sentences.")
 
     for(sent <- sents) {
-      val depTree = DependencyTree.toTree(sent)
-      val stack = new Stack[BHT]
-      depTree.toBHT(stack, "root")
-      val bht = stack.pop()
-      println(depTree)
-      println(bht)
+      generateHexaTags(sent)
       System.exit(1)
     }
   }
