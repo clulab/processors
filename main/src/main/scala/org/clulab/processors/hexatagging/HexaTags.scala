e8809538ffa15183a7523c690b02e022f82bc1c0
Mihai Surdeanu <surdeanu@gmail.com>
2024-05-15 18:28:53 +0300

    added missing file

---
+++ main/src/main/scala/org/clulab/processors/hexatagging/HexaTags.scala
@@ -0,0 +1,8 @@
+package org.clulab.processors.hexatagging
+
+object HexaTags {
+  val TERMINAL_RIGHT_CHILD = "tr"
+  val TERMINAL_LEFT_CHILD = "tl"
+  val NONTERM_RIGHT_CHILD = "nr"
+  val NONTERM_LEFT_CHILD = "nl"
+}
