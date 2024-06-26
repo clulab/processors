070639d3ba1ed27e59a654dc0d86249356ea1e0c
Mihai Surdeanu <surdeanu@gmail.com>
2024-06-26 15:35:07 +0300

    top k decoding still broken. reverting to a simpler one for debugging.

--- main/src/main/scala/org/clulab/processors/hexatagging/TopKPaths.scala
+++ main/src/main/scala/org/clulab/processors/hexatagging/TopKPaths.scala
@@ -13,6 +13,10 @@ class Path[L](val score: Float, val sequence: Seq[L]) extends HeapElement {
     val newSeq = sequence :+ elem
     new Path[L](newScore, newSeq)
   }
+
+  override def toString(): String = {
+    s"Path($score): [${sequence.mkString(", ")}]"
+  }
 }
 
 object Path {
@@ -28,9 +32,18 @@ object TopKPaths {
     * @return a sequence of paths sorted in descending order of scores
     */
   def findTopK(tags: Array[Array[(String, Float)]], k: Int): Seq[Path[String]] = {
+    //println(s"Inside findTopK ${tags.length} x ${tags(0).length}, k = $k")
     var pathHeap = new MinHeap(k)
     pathHeap.insert(Path[String]())
 
+    for(i <- tags.indices) {
+      val p = pathHeap.extractMin().get.asInstanceOf[Path[String]]
+      val newP = p.append(tags(i)(0)._1, tags(i)(0)._2)
+      //println(s"newP = $newP")
+      pathHeap.insert(newP)
+    }
+
+    /*
     for(i <- tags.indices) {
       val newHeap = new MinHeap(k)
 
@@ -39,8 +52,9 @@ object TopKPaths {
         val p: Path[String] = it.next()
         var stillWorking = true
         // each column in the lattice must be sorted in descending order of scores
-        for(j <- tags(i).indices if stillWorking) {
+        for(j <- 0 until 1 if stillWorking) {
           val newP = p.append(tags(i)(j)._1, tags(i)(j)._2)
+          println(s"newP = $newP")
           // if insert fails it means the old path + the new elem < the min score already in the heap
           // since columns are sorted in descending order of scores, we can then stop
           stillWorking = newHeap.insert(newP)
@@ -49,7 +63,11 @@ object TopKPaths {
 
       pathHeap = newHeap
     }
+    */
 
-    pathHeap.toSortedSeq.map(_.asInstanceOf[Path[String]])
+    val paths = pathHeap.toSortedSeq.map(_.asInstanceOf[Path[String]])
+    //println(paths.head)
+    //System.exit(1)
+    paths
   }
 }
