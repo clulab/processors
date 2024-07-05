c6b97b4c4be706d0a92099367afbb919dc716e65
Mihai Surdeanu <MihaiSurdeanu@users.noreply.github.com>
2024-07-05 12:09:59 +0300

    removed embeddings (#798)
    * removed embeddings
    
    * fixed trailing comma in a list

--- main/src/main/scala/org/clulab/odin/impl/OdinResource.scala
+++ main/src/main/scala/org/clulab/odin/impl/OdinResource.scala
@@ -2,19 +2,17 @@ package org.clulab.odin.impl
 
 import java.io.InputStream
 
-import org.clulab.embeddings.SanitizedWordEmbeddingMap
 import org.clulab.scala.WrappedArray._
 
 
 trait OdinResource
 
 // for distributional similarity comparisons
-// Uses Word2Vec class as its backend
-@annotation.nowarn("cat=deprecation")
-class EmbeddingsResource(is: InputStream) extends SanitizedWordEmbeddingMap(is, None, false) with OdinResource {
-  override def similarity(w1: String, w2: String): Double = {
-    val score = super.similarity(w1, w2)
-      //println(s"score is $score")
-      score
+class EmbeddingsResource(/* is: InputStream */) extends OdinResource {
+  def similarity(w1: String, w2: String): Double = {
+    // embedding similarity no longer supported starting with v10 
+    // TODO: if we decide to add it back to Odin, this is the place
+    if(w1.equalsIgnoreCase(w2)) 1.0
+    else 0.0
   }
 }
