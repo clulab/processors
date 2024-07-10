c954b7dcae8045bc26e31b23d8850b16d88882d9
Mihai Surdeanu <surdeanu@gmail.com>
2024-07-10 16:15:26 +0300

    improved PostProcessor

--- library/src/main/scala/org/clulab/processors/clu/PostProcessor.scala
+++ library/src/main/scala/org/clulab/processors/clu/PostProcessor.scala
@@ -13,17 +13,26 @@ object PostProcessor {
 
   /** POS tag corrections, in place */
   def postprocessPartOfSpeechTags(words: Array[String], tags: Array[String]): Array[String] = {
+
+    // unigram patterns
     words.indices.foreach { index =>
-      if (tags(index) != "CC" &&
-          VERSUS_PATTERN.matcher(words(index)).matches)
+      if (tags(index) != "CC" && VERSUS_PATTERN.matcher(words(index)).matches) {
         tags(index) = "CC" // "versus" seems like a CC to me. but maybe not...
+      }
     }
+
+    // bigram patterns
     words.indices.dropRight(1).foreach { curr =>
       val next = curr + 1
       // "due" in "due to" must be a preposition
-      if (words(curr).equalsIgnoreCase("due") &&
-          words(next).equalsIgnoreCase("to"))
+      if (words(curr).equalsIgnoreCase("due") && words(next).equalsIgnoreCase("to")) {
         tags(curr) = "IN"
+      }
+
+      // "fall" followed by a CD must be NN
+      else if(words(curr).equalsIgnoreCase("fall") && tags(next).equals("CD")) {
+        tags(curr) = "NN"
+      }
     }
 
     tags
