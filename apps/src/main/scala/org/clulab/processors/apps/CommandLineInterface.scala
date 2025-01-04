6a3a165d3524a36e075a893befd740fc62a0a08e
Mihai Surdeanu <surdeanu@gmail.com>
2025-01-04 11:30:16 -0700

    Added CoNLLU serializer; removed apps superseeded by CommandLineInterface

--- apps/src/main/scala/org/clulab/processors/apps/CommandLineInterface.scala
+++ apps/src/main/scala/org/clulab/processors/apps/CommandLineInterface.scala
@@ -1,5 +1,48 @@
 package org.clulab.processors.apps
 
+import org.clulab.processors.Document
+import org.clulab.processors.clu.BalaurProcessor
+import org.clulab.serialization.CoNLLUSerializer
+import org.clulab.utils.{FileUtils, StringUtils}
+
+import java.io.PrintWriter
+
 object CommandLineInterface extends App {
-  println("hello")
+  // specifies the input file
+  val INPUT = "input"
+  // specifies the output file; if not provided uses stdout
+  val OUTPUT = "output"
+
+  // input formats:
+  val RAW = "raw"
+  val SENTS = "sents"
+  val TOKENS = "tokens"
+
+  val props = StringUtils.argsToProperties(args)
+  println(props)
+
+  if (!props.containsKey(INPUT)) {
+    // run in interactive mode because no input file was provided
+    new ProcessorsShell().shell()
+  } else {
+    // process a file in batch mode
+    val proc = new BalaurProcessor()
+
+    val doc: Document =
+      if (props.containsKey(RAW)) {
+        val rawText = FileUtils.getTextFromFile(props.getProperty(INPUT))
+        proc.annotate(rawText)
+      } else {
+        throw new RuntimeException(s"ERROR: unknown input file format. Please use one of: $RAW|$SENTS|$TOKENS.")
+      }
+
+    val pw = mkOutput()
+    CoNLLUSerializer.saveCoNLLUExtended(pw, doc)
+    pw.close()
+  }
+
+  def mkOutput(): PrintWriter = {
+    if(props.containsKey(OUTPUT)) FileUtils.printWriterFromFile(props.getProperty(OUTPUT))
+    else new PrintWriter(System.out)
+  }
 }
---
+++ library/src/main/scala/org/clulab/serialization/CoNLLUSerializer.scala
@@ -0,0 +1,51 @@
+package org.clulab.serialization
+
+import org.clulab.processors.Document
+
+import java.io.PrintWriter
+
+object CoNLLUSerializer {
+  val UNDEF = "_"
+  val ROOT = "root"
+
+  /**
+   * Saves a doc in a format inspired by CoNLL-U
+   * See the processors documentation page for details
+   */
+  def saveCoNLLUExtended(pw: PrintWriter, doc: Document): Unit = {
+    for(sent <- doc.sentences) {
+      for(i <- 0 until sent.size) {
+        val id = i + 1 // offsets start at 1 in CoNLL-U
+        val form = sent.words(i)
+        val lemma = if(sent.lemmas.isDefined) sent.lemmas.get(i) else UNDEF
+        val upos = if(sent.tags.isDefined) sent.tags.get(i) else UNDEF
+        val xpos = UNDEF
+        val feats = UNDEF
+
+        val (head, depRel) =
+          if(sent.universalEnhancedDependencies.isDefined) {
+            val deps = sent.universalEnhancedDependencies.get
+            val edges = deps.getIncomingEdges(i)
+            if(edges.length > 0) {
+              ((edges.head._1 + 1).toString, edges.head._2)
+            } else {
+              (0, ROOT)
+            }
+          } else {
+            (UNDEF, UNDEF)
+          }
+
+        // extra info specific to us
+        val startOffset = sent.startOffsets(i)
+        val endOffset = sent.endOffsets(i)
+        val ent = if(sent.entities.isDefined) sent.entities.get(i) else UNDEF
+        val entNorm = if(sent.norms.isDefined && sent.norms.get(i).nonEmpty) sent.norms.get(i) else UNDEF
+        val chunk = if(sent.chunks.isDefined) sent.chunks.get(i) else UNDEF
+
+        pw.println(s"$id\t$form\t$lemma\t$upos\t$xpos\t$feats\t$head\t$depRel\t$startOffset\t$endOffset\t$ent\t$entNorm\t$chunk")
+      }
+
+      pw.println()
+    }
+  }
+}
