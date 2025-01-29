914cdc560826ed86a59b90366a6706fbccf195e7
Keith Alcock <github@keithalcock.com>
2025-01-29 10:47:21 -0700

    Fill in the other extractors

--- debugger/src/main/scala/org/clulab/odin/debugger/debugging/DebuggingCrossSentenceExtractor.scala
+++ debugger/src/main/scala/org/clulab/odin/debugger/debugging/DebuggingCrossSentenceExtractor.scala
@@ -1,8 +1,9 @@
 package org.clulab.odin.debugger.debugging
 
-import org.clulab.odin.Action
+import org.clulab.odin.{Action, Mention, State}
 import org.clulab.odin.debugger.Debugger
 import org.clulab.odin.impl.{CrossSentenceExtractor, Priority, TokenExtractor}
+import org.clulab.processors.Document
 
 class DebuggingCrossSentenceExtractor(
   val debugger: Debugger,
@@ -21,6 +22,14 @@ class DebuggingCrossSentenceExtractor(
 ) extends CrossSentenceExtractor(name, labels, priority, keep, action, leftWindow, rightWindow, anchorPattern,
     neighborPattern, anchorRole, neighborRole) {
 
+  // This comes indirectly through Extractor.
+  override def findAllIn(doc: Document, state: State): Seq[Mention] = debugger.debugExtractor(crossSentenceExtractor) {
+    super.findAllIn(doc, state)
+  }
+
+  override def findAllIn(sent: Int, doc: Document, state: State): Seq[Mention] = debugger.debugSentence(sent, doc.sentences(sent)) {
+    super.findAllIn(sent, doc, state) // TODO
+  }
 }
 
 object DebuggingCrossSentenceExtractor {
@@ -36,8 +45,8 @@ object DebuggingCrossSentenceExtractor {
       crossSentenceExtractor.action,
       crossSentenceExtractor.leftWindow,
       crossSentenceExtractor.rightWindow,
-      crossSentenceExtractor.anchorPattern,
-      crossSentenceExtractor.neighborPattern,
+      DebuggingTokenExtractor(debugger, crossSentenceExtractor.anchorPattern),
+      DebuggingTokenExtractor(debugger, crossSentenceExtractor.neighborPattern),
       crossSentenceExtractor.anchorRole,
       crossSentenceExtractor.neighborRole
     )
--- debugger/src/main/scala/org/clulab/odin/debugger/debugging/DebuggingGraphExtractor.scala
+++ debugger/src/main/scala/org/clulab/odin/debugger/debugging/DebuggingGraphExtractor.scala
@@ -2,8 +2,112 @@ package org.clulab.odin.debugger.debugging
 
 import org.clulab.odin.debugger.Debugger
 import org.clulab.odin.{Action, Mention, State}
-import org.clulab.odin.impl.{GraphExtractor, GraphPattern, OdinConfig, Priority}
+import org.clulab.odin.impl.{ArgumentPattern, GraphExtractor, GraphPattern, OdinConfig, Priority, RelationGraphPattern, TokenPattern, TriggerMentionGraphPattern, TriggerPatternGraphPattern}
 import org.clulab.processors.Document
+
+class DebuggingTriggerPatternGraphPattern(
+  val debugger: Debugger,
+  trigger: TokenPattern,
+  arguments: Seq[ArgumentPattern], // TODO
+  config: OdinConfig
+) extends TriggerPatternGraphPattern(trigger, arguments, config) {
+
+  override def getMentions(
+    sent: Int,
+    doc: Document,
+    state: State,
+    labels: Seq[String],
+    keep: Boolean,
+    ruleName: String
+  ): Seq[Mention] = {
+    super.getMentions(sent, doc, state, labels, keep, ruleName)
+    // TODO: record should be made automatically
+  }
+}
+
+object DebuggingTriggerPatternGraphPattern {
+
+  def apply(debugger: Debugger, triggerPatternGraphPattern: TriggerPatternGraphPattern): DebuggingTriggerPatternGraphPattern = {
+    new DebuggingTriggerPatternGraphPattern(
+      debugger,
+      DebuggingTokenPattern(debugger, triggerPatternGraphPattern.trigger),
+      triggerPatternGraphPattern.arguments,
+      triggerPatternGraphPattern.config
+    )
+  }
+}
+
+class DebuggingTriggerMentionGraphPattern(
+  val debugger: Debugger,
+  triggerLabel: String,
+  arguments: Seq[ArgumentPattern], // TODO
+  config: OdinConfig
+) extends TriggerMentionGraphPattern(triggerLabel, arguments, config) {
+
+  override def getMentions(
+    sent: Int,
+    doc: Document,
+    state: State,
+    labels: Seq[String],
+    keep: Boolean,
+    ruleName: String
+  ): Seq[Mention] = {
+    super.getMentions(sent, doc, state, labels, keep, ruleName)
+    // TODO: record each mention and whether matched or not
+    // debugger.debugMention
+    // debugger.matches
+  }
+}
+
+object DebuggingTriggerMentionGraphPattern {
+
+  def apply(debugger: Debugger, triggerMentionGraphPattern: TriggerMentionGraphPattern) = {
+    new DebuggingTriggerMentionGraphPattern(
+      debugger,
+      triggerMentionGraphPattern.triggerLabel,
+      triggerMentionGraphPattern.arguments,
+      triggerMentionGraphPattern.config
+    )
+  }
+}
+
+class DebuggingRelationGraphPattern(
+  val debugger: Debugger,
+  anchorName: String,
+  anchorLabel: String,
+  arguments: Seq[ArgumentPattern],  // TODO
+  config: OdinConfig
+) extends RelationGraphPattern(anchorName, anchorLabel, arguments, config) {
+
+    override def getMentions(
+    sent: Int,
+    doc: Document,
+    state: State,
+    labels: Seq[String],
+    keep: Boolean,
+    ruleName: String
+  ): Seq[Mention] = {
+    super.getMentions(sent, doc, state, labels, keep, ruleName)
+    // TODO: record each mention and whether matched or not
+    // debugger.debugMention
+    // debugger.matches
+  }
+}
+
+object DebuggingRelationGraphPattern {
+
+  def apply(debugger: Debugger, relationGraphPattern: RelationGraphPattern): DebuggingRelationGraphPattern = {
+    new DebuggingRelationGraphPattern(
+      debugger,
+      relationGraphPattern.anchorName,
+      relationGraphPattern.anchorLabel,
+      relationGraphPattern.arguments, // TODO, debugging version?
+      relationGraphPattern.config
+    )
+  }
+}
+
+
 
 class DebuggingGraphExtractor(
   val debugger: Debugger,
